e081a1eb1985445aafae5dc69bdc2134714b2ef3
Enache Ionut <Enache-Ionut@users.noreply.github.com>
2017-09-20 14:02:23 +0300

    include UseUserSettings property in Tidy Category

--- ClangPowerTools/ClangPowerTools/TidyOptions.cs
+++ ClangPowerTools/ClangPowerTools/TidyOptions.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.VisualStudio.Shell;
+using System;
 using System.ComponentModel;
+using System.Runtime.InteropServices;
 
 namespace ClangPowerTools
 {
+  [ClassInterface(ClassInterfaceType.AutoDual)]
   public class TidyOptions : DialogPage
   {
     #region Members
@@ -11,6 +14,8 @@ namespace ClangPowerTools
 
     #endregion
 
+    #region Methods
+
     public override void LoadSettingsFromStorage()
     {
       base.LoadSettingsFromStorage();
@@ -18,13 +23,9 @@ namespace ClangPowerTools
       UseUserSettings = true;
     }
 
-    #region Properties
+    #endregion
 
-    [Category(" Tidy")]
-    [DisplayName("Use User Settings")]
-    [Description("If not empty clang-tidy will be called with given flags, instead of clang++. The tidy operation is applied to whole translation units, meaning all directory headers included in the CPP will be tidied up too. Changes will not be applied, only simulated.")]
-    [Browsable(false)]
-    public bool UseUserSettings { get; set; }
+    #region Properties
 
     #region Tidy Category
 
@@ -42,6 +43,12 @@ namespace ClangPowerTools
     [DisplayName("Fix")]
     [Description("If not empty clang-tidy will be called with given flags, instead of clang++. The tidy operation is applied to whole translation units, meaning all directory headers included in the CPP will be tidied up too. Changes will be applied to the file(s).")]
     public bool Fix { get; set; }
+
+    [Category(" Tidy")]
+    [DisplayName("Use User Settings")]
+    [Description("If not empty clang-tidy will be called with given flags, instead of clang++. The tidy operation is applied to whole translation units, meaning all directory headers included in the CPP will be tidied up too. Changes will not be applied, only simulated.")]
+    [Browsable(false)]
+    public bool UseUserSettings { get; set; }
 
     #endregion
 
