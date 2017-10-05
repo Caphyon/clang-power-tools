2c4544dfdc4b5455da6989defc949fefec0c57e7
Enache Ionut <Enache-Ionut@users.noreply.github.com>
2017-10-05 15:25:16 +0300

    Open modified files

---
+++ ClangPowerTools/ClangPowerTools/FileOpener.cs
@@ -0,0 +1,20 @@
+﻿using EnvDTE80;
+
+namespace ClangPowerTools
+{
+  public class FileOpener
+  {
+    #region Members
+
+    private string kOpenCommand = "File.OpenFile";
+
+    #endregion
+
+    #region Public methods
+
+    public void Open(DTE2 aDte, string aFilePath) => 
+      aDte.ExecuteCommand(kOpenCommand, aFilePath);
+
+    #endregion
+  }
+}
