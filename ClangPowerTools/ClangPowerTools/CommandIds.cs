4ad77d86d924f128acf0419272f3bda10d1aae52
Enache Ionut <Enache-Ionut@users.noreply.github.com>
2017-11-10 12:44:48 +0200

    added command ids in separate helper class and fixed clear error list

---
+++ ClangPowerTools/ClangPowerTools/CommandIds.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClangPowerTools
+{
+  public class CommandIds
+  {
+    public static int kTidyId = 0x0101;
+    public static int kCompileId = 0x0102;
+    public static int kSettingsId = 0x0103;
+  }
+}
