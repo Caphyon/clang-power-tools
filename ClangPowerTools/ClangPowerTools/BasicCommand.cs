4b024a27f3baf0dbc67a9cea0adc01ca6087a769
Enache Ionut <Enache-Ionut@users.noreply.github.com>
2017-11-10 12:40:29 +0200

    added  the base classes for new structure

---
+++ ClangPowerTools/ClangPowerTools/BasicCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.Shell;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClangPowerTools
+{
+  {
+    #region Members
+
+    #endregion
+
+    #region Properties
+
+    protected int Id { get; set; }
+    protected Guid CommandSet { get; set; }
+    protected Package Package { get; set; }
+    protected IServiceProvider ServiceProvider => Package;
+
+    #endregion
+
+    #region Constructor
+
+    {
+      Package = aPackage ?? throw new ArgumentNullException("package");
+      CommandSet = aGuid;
+      Id = aId;
+    }
+    
+    #endregion
+
+  }
+}
