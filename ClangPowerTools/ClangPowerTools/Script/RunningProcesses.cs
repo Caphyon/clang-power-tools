b00196e0506fab052249e02da0d5090cff7657c7
Enache Ionut <Enache-Ionut@users.noreply.github.com>
2017-12-06 12:49:53 +0200

    kill all launched powershell processes

--- ClangPowerTools/ClangPowerTools/Script/RunningProcesses.cs
+++ ClangPowerTools/ClangPowerTools/Script/RunningProcesses.cs
@@ -8,6 +8,7 @@ namespace ClangPowerTools
   {
     #region Members
 
+    private const string processeName = "powershell";
     private List<Process> mProcesses = new List<Process>();
     
     #endregion
@@ -31,13 +32,11 @@ namespace ClangPowerTools
           process.Kill();
 
       List<Process> processes = new List<Process>();
-      processes.AddRange(Process.GetProcessesByName("clang++.exe"));
-      processes.AddRange(Process.GetProcessesByName("clang-tidy.exe"));
-
+      processes.AddRange(Process.GetProcessesByName(processeName));
+      
       foreach (var process in processes)
         if(!process.HasExited)
           process.Kill();
-
       mProcesses.Clear();
     }
 
