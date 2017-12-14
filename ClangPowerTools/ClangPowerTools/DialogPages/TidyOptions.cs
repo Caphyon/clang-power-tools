7668c8cce193082a3e9a8d28600c5b4d5c39bef2
Enache Ionut <Enache-Ionut@users.noreply.github.com>
2017-12-14 14:45:29 +0200

    changed the "Operation Mode" option name to "Use checks from"

--- ClangPowerTools/ClangPowerTools/DialogPages/TidyOptions.cs
+++ ClangPowerTools/ClangPowerTools/DialogPages/TidyOptions.cs
@@ -36,7 +36,7 @@ namespace ClangPowerTools
     public bool Fix { get; set; }
 
     [Category(" Tidy")]
-    [DisplayName("Operation mode")]
+    [DisplayName("Use checks from")]
     [Description("Tidy checks: switch between explicitly specified checks (predefined or custom) and using checks from .clang-tidy configuration files.\nOther options are always loaded from .clang-tidy files.")]
     [TypeConverter(typeof(TidyModeConvertor))]
     public string TidyMode { get; set; }
