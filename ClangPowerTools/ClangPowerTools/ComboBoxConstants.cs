486e82a363e64ea006679cd96f4bfed4a4c1d989
Enache Ionut <enachestoianionut@gmail.com>
2017-12-20 23:35:28 +0200

    create combobox convertor base class

--- ClangPowerTools/ClangPowerTools/ComboBoxConstants.cs
+++ ClangPowerTools/ClangPowerTools/ComboBoxConstants.cs
@@ -1,12 +1,15 @@
 ﻿namespace ClangPowerTools
 {
-  public class TidyModeConstants
+  public sealed class ComboBoxConstants
   {
     #region Constants
 
     public const string kCustomChecks = "custom checks";
     public const string kPredefinedChecks = "predefined checks";
     public const string kTidyFile = ".clang-tidy config file";
+
+    public const string kDefaultHeaderFilter = ".*";
+    public const string kCorrespondingHeader = "Corresponding Header";
 
     #endregion
 
