978360d16537c9c5722056036c48007bdd04d4ba
Enache Ionut <enachestoianionut@gmail.com>
2018-02-13 17:11:16 +0200

    collect all errors into a HashSet to be sure that there will not be duplicates errors

--- ClangPowerTools/ClangPowerTools/Error/TaskError.cs
+++ ClangPowerTools/ClangPowerTools/Error/TaskError.cs
@@ -10,20 +10,36 @@ namespace ClangPowerTools
     public string FullMessage { get; set; }
     public string FilePath { get; set; }
     public int Line { get; set; }
-    public TaskErrorCategory Category { get; set; } 
+    public TaskErrorCategory Category { get; set; }
 
-  #endregion
+    #endregion
 
-  #region Constructor
+    #region Constructor
 
-  public TaskError(string aFilePath, string aFullMessage, 
-    string aMessage, int aLine, TaskErrorCategory aCategory)
+    public TaskError(string aFilePath, string aFullMessage,
+      string aMessage, int aLine, TaskErrorCategory aCategory)
     {
       FilePath = aFilePath;
       FullMessage = aFullMessage;
       Message = aMessage;
       Line = aLine;
       Category = aCategory;
+    }
+
+    public override bool Equals(object obj)
+    {
+      var otherObj = obj as TaskError;
+      if (null == otherObj)
+        return false;
+
+      return Line == otherObj.Line &&
+        FilePath == otherObj.FilePath &&
+        FullMessage == otherObj.FullMessage;
+    }
+
+    public override int GetHashCode()
+    {
+      return $"{Line.ToString()}{FilePath}{FullMessage}".GetHashCode();
     }
 
     #endregion
