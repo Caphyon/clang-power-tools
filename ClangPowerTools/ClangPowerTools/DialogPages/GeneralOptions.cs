7ea2f698ed5560d89e04ac8ca48299c596b74611
Enache Ionut <enachestoianionut@gmail.com>
2018-02-15 14:36:06 +0200

    changed the name and description for dedicated UI option

--- ClangPowerTools/ClangPowerTools/DialogPages/GeneralOptions.cs
+++ ClangPowerTools/ClangPowerTools/DialogPages/GeneralOptions.cs
@@ -62,10 +62,9 @@ namespace ClangPowerTools
     [TypeConverter(typeof(AdditionalIncludesConvertor))]
     public string AdditionalIncludes { get; set; }
 
-
     [Category("General")]
-    [DisplayName("Clang compile after VS compile")]
-    [Description("Run clang compile after VS compile succeeded.")]
+    [DisplayName("Clang compile after MSVC compile")]
+    [Description("Always run Clang compile on the current source file, after successful MSVC compilation.")]
     public bool ClangCompileAfterVsCompile { get; set; }
 
     [Browsable(false)]
