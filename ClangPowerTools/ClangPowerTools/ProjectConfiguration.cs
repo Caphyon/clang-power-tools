479417fc49d5406a9ba0037b4426df2be6bc28c4
Enache Ionut <enachestoianionut@gmail.com>
2018-02-21 17:56:36 +0200

    refactored the getters of platform name and configuration name

--- ClangPowerTools/ClangPowerTools/ProjectConfiguration.cs
+++ ClangPowerTools/ClangPowerTools/ProjectConfiguration.cs
@@ -1,31 +1,51 @@
 ﻿using EnvDTE;
-using System;
 
 namespace ClangPowerTools
 {
-  public static class ProjectConfiguration
+  public class ProjectConfiguration
   {
-    public static string GetPlatform(Project aProjectItem)
+    #region Public Methods
+
+    public static string GetPlatform(Project aProject)
     {
-      try
-      {
-        return aProjectItem.ConfigurationManager.ActiveConfiguration.PlatformName;
-      }
-      catch (Exception)
-      {
-      }
-      return string.Empty;
+      var succes = GetActiveConfiguration(aProject, out Configuration configuration);
+      if (false == succes)
+        return string.Empty;
+
+      return configuration.PlatformName;
     }
-    public static string GetConfiguration(Project aProjectItem)
+
+    public static string GetConfiguration(Project aProject)
     {
-      try
-      {
-        return aProjectItem.ConfigurationManager.ActiveConfiguration.ConfigurationName;
-      }
-      catch (Exception)
-      {
-      }
-      return string.Empty;
+      var succes = GetActiveConfiguration(aProject, out Configuration configuration);
+      if (false == succes)
+        return string.Empty;
+
+      return configuration.ConfigurationName;
     }
+
+    #endregion
+
+    #region Private Methods
+
+    private static bool GetActiveConfiguration(Project aProject, out Configuration aConfiguration)
+    {
+      aConfiguration = null;
+
+      var configurationManager = aProject.ConfigurationManager;
+      if (null == configurationManager)
+        return false;
+
+      aConfiguration = configurationManager.ActiveConfiguration;
+      if (null == aConfiguration)
+        return false;
+
+      return true;
+    }
+
+    #endregion
+
+
+
   }
 }
