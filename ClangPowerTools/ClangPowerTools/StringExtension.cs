cb7bca815b138f297ef34b4d8a881bbf14fbcfb0
Enache Ionut <enachestoianionut@gmail.com>
2018-03-16 11:03:11 +0200

    created an extension class for strings with the scop of finding the text before and after a certain string

---
+++ ClangPowerTools/ClangPowerTools/StringExtension.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+
+namespace ClangPowerTools
+{
+  public static class StringExtension
+  {
+    #region Public Methods
+
+    public static string SubstringAfter(this string source, string value)
+    {
+      if (string.IsNullOrEmpty(value))
+        return source;
+
+      CompareInfo compareInfo = CultureInfo.InvariantCulture.CompareInfo;
+      int index = compareInfo.IndexOf(source, value, CompareOptions.Ordinal);
+
+      if (index < 0)
+        return string.Empty; //No such substring
+
+      return source.Substring(index + value.Length);
+    }
+
+    public static string SubstringBefore(this string source, string value)
+    {
+      if (string.IsNullOrEmpty(value))
+        return value;
+
+      CompareInfo compareInfo = CultureInfo.InvariantCulture.CompareInfo;
+      int index = compareInfo.IndexOf(source, value, CompareOptions.Ordinal);
+
+      if (index < 0)
+        return string.Empty; //No such substring
+
+      return source.Substring(0, index);
+    }
+
+    #endregion
+
+  }
+}
