72078726a0f85ba23dd5ffd17057936533a23ee9
Enache Ionut <enachestoianionut@gmail.com>
2018-03-22 18:45:25 +0200

    chagne default values

--- ClangPowerTools/ClangPowerTools/DialogOptionsModel/DefaultOptions.cs
+++ ClangPowerTools/ClangPowerTools/DialogOptionsModel/DefaultOptions.cs
@@ -4,13 +4,12 @@
   {
     public static readonly string[] kClangFlags = new string[]
     {
+      "-Werror",
       "-Wall",
       "-fms-compatibility-version=19.10",
-      "-fms-compatibility",
       "-Wmicrosoft",
       "-Wno-invalid-token-paste",
       "-Wno-unknown-pragmas",
-      "-Wno-unused-variable",
       "-Wno-unused-value"
     };
 
