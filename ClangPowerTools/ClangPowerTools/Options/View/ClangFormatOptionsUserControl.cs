969f44e8abe1b0b9cc8f7a942abbf1844ceb4df6
Enache Ionut <enachestoianionut@gmail.com>
2018-04-18 18:03:39 +0300

    changed the win form user controle name

--- ClangPowerTools/ClangPowerTools/Options/View/ClangFormatOptionsUserControl.cs
+++ ClangPowerTools/ClangPowerTools/Options/View/ClangFormatOptionsUserControl.cs
@@ -1,21 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 using ClangPowerTools.DialogPages;
 
 namespace ClangPowerTools.Options.ViewModel
 {
-  public partial class MyUserControl : UserControl
+  public partial class ClangFormatOptionsUserControl : UserControl
   {
     private ClangFormatOptionsView page;
 
-    public MyUserControl(ClangFormatOptionsView page)
+    public ClangFormatOptionsUserControl(ClangFormatOptionsView page)
     {
       this.page = page;
       InitializeComponent();
