bb372be189a496b7e4f22c7764d72841ea3d2490
Enache Ionut <enachestoianionut@gmail.com>
2018-04-18 12:00:21 +0300

    added convertors for clang format style and fallback-style

---
+++ ClangPowerTools/ClangPowerTools/Convertors/ClangFormatFallbackStyleConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ClangPowerTools.Convertors
+{
+  class ClangFormatFallbackStyleConverter : IValueConverter
+  {
+
+    public static Dictionary<ClangFormatFallbackStyle?, string> StyleDisplay = new Dictionary<ClangFormatFallbackStyle?, string>()
+    {
+      { ClangFormatFallbackStyle.none, "none" },
+      { ClangFormatFallbackStyle.file, "file" },
+      { ClangFormatFallbackStyle.Chromium, "Chromium" },
+      { ClangFormatFallbackStyle.Google, "Google" },
+      { ClangFormatFallbackStyle.LLVM, "LLVM" },
+      { ClangFormatFallbackStyle.Mozilla, "Mozilla" },
+      { ClangFormatFallbackStyle.WebKit, "WebKit" },
+    };
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      ClangFormatFallbackStyle fallbackStyle = (ClangFormatFallbackStyle)value;
+      return StyleDisplay[fallbackStyle];
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      throw new NotImplementedException();
+    }
+
+  }
+}
