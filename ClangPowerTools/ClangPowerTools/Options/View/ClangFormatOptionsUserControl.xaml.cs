d24664b80fde0aa3edc3a29fadc5b3f42453680c
Enache Ionut <enachestoianionut@gmail.com>
2018-04-24 14:30:08 +0300

    enable for editing all the UI elements

--- ClangPowerTools/ClangPowerTools/Options/View/ClangFormatOptionsUserControl.xaml.cs
+++ ClangPowerTools/ClangPowerTools/Options/View/ClangFormatOptionsUserControl.xaml.cs
@@ -10,13 +10,6 @@ namespace ClangPowerTools.Options.ViewModel
   /// </summary>
   public partial class ClangFormatOptionsUserControl : UserControl
   {
-    private const UInt32 DLGC_WANTARROWS = 0x0001;
-    private const UInt32 DLGC_WANTTAB = 0x0002;
-    private const UInt32 DLGC_WANTALLKEYS = 0x0004;
-    private const UInt32 DLGC_HASSETSEL = 0x0008;
-    private const UInt32 DLGC_WANTCHARS = 0x0080;
-    private const UInt32 WM_GETDLGCODE = 0x0087;
-
     public ClangFormatOptionsUserControl(ClangFormatOptionsView clangFormat)
     {
       InitializeComponent();
@@ -24,21 +17,9 @@ namespace ClangPowerTools.Options.ViewModel
       wpfPropGrid.SelectedObject = clangFormat;
 
       Loaded += delegate
-      {
-        HwndSource s = HwndSource.FromVisual(this) as HwndSource;
-        if (s != null)
-          s.AddHook(new HwndSourceHook(ChildHwndSourceHook));
-      };
-    }
-
-    IntPtr ChildHwndSourceHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
-    {
-      if (msg == WM_GETDLGCODE)
-      {
-        handled = true;
-        return new IntPtr(DLGC_WANTCHARS | DLGC_WANTARROWS | DLGC_HASSETSEL);
-      }
-      return IntPtr.Zero;
+        {
+          UIElementsActivator.Activate(HwndSource.FromVisual(this) as HwndSource);
+        };
     }
 
   }
