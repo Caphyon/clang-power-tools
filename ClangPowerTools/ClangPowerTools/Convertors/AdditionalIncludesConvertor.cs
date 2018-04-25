de2c3541909a26c8035d78988d6020abf54418c9
Enache Ionut <enachestoianionut@gmail.com>
2018-04-25 18:43:03 +0300

    implement explicit convertor for additional includ enum

--- ClangPowerTools/ClangPowerTools/Convertors/AdditionalIncludesConvertor.cs
+++ ClangPowerTools/ClangPowerTools/Convertors/AdditionalIncludesConvertor.cs
@@ -1,17 +1,52 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows.Data;
 
 namespace ClangPowerTools.Convertors
 {
-  class AdditionalIncludesConvertor : ComboBoxConvertor
+  public class AdditionalIncludesConvertor : IValueConverter
   {
-    public AdditionalIncludesConvertor() :
-     base(
-       new ArrayList(new string[]
-       {
-          ComboBoxConstants.kIncludeDirectories,
-          ComboBoxConstants.kSystemIncludeDirectories
-       }))
-    { }
+    #region Members
+
+    private static readonly Dictionary<ClangGeneralAdditionalIncludes?, string> mAdditionalIncludesEnumToString =
+      new Dictionary<ClangGeneralAdditionalIncludes?, string>
+      {
+        {ClangGeneralAdditionalIncludes.IncludeDirectories, ComboBoxConstants.kIncludeDirectories},
+        {ClangGeneralAdditionalIncludes.SystemIncludeDirectories, ComboBoxConstants.kSystemIncludeDirectories }
+      };
+
+    private static readonly Dictionary<string, ClangGeneralAdditionalIncludes?> mAdditionalIncludesStringToEnum =
+      new Dictionary<string, ClangGeneralAdditionalIncludes?>
+      {
+        { ComboBoxConstants.kIncludeDirectories, ClangGeneralAdditionalIncludes.IncludeDirectories },
+        { ComboBoxConstants.kSystemIncludeDirectories, ClangGeneralAdditionalIncludes.SystemIncludeDirectories }
+      };
+
+    #endregion
+
+    #region Public Methods
+
+    public static string ToString(ClangGeneralAdditionalIncludes? aAdditionalIncludes) 
+      => mAdditionalIncludesEnumToString[aAdditionalIncludes];
+
+    public static ClangGeneralAdditionalIncludes? FromString(string aAdditionalIncludes)
+      => mAdditionalIncludesStringToEnum[aAdditionalIncludes];
+
+
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      ClangGeneralAdditionalIncludes additionalIncludes = (ClangGeneralAdditionalIncludes)value;
+      return mAdditionalIncludesEnumToString[additionalIncludes];
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      throw new NotImplementedException();
+    }
+
+    #endregion
 
   }
 }
