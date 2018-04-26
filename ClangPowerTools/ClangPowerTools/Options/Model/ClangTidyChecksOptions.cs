cac5f28f2886277779b87feb0bd8be506771eef9
Enache Ionut <enachestoianionut@gmail.com>
2018-04-26 16:10:51 +0300

    added new tidy checks and sort the properties after name

--- ClangPowerTools/ClangPowerTools/Options/Model/ClangTidyChecksOptions.cs
+++ ClangPowerTools/ClangPowerTools/Options/Model/ClangTidyChecksOptions.cs
@@ -4,124 +4,6 @@
   {
     #region Properties
 
-    public bool ClangAnalyzerCoreCallAndMessage { get; set; }
-
-    public bool ClangAnalyzerCoreDivideZero { get; set; }
-
-    public bool ClangAnalyzerCoreNonNullParamChecker { get; set; }
-
-    public bool ClangAnalyzerCoreNullDereference { get; set; }
-
-    public bool ClangAnalyzerCoreStackAddressEscape { get; set; }
-
-    public bool ClangAnalyzerCoreUndefinedBinaryOperatorResult { get; set; }
-
-    public bool ClangAnalyzerCoreVLASize { get; set; }
-
-    public bool ClangAnalyzerCoreuninitializedArraySubscript { get; set; }
-
-    public bool ClangAnalyzerCoreuninitializedAssign { get; set; }
-
-    public bool ClangAnalyzerCoreuninitializedBranch { get; set; }
-
-    public bool ClangAnalyzerCoreuninitializedCapturedBlockVariable { get; set; }
-
-    public bool ClangAnalyzerCoreuninitializedUndefReturn { get; set; }
-
-    public bool ClangAnalyzerCplusplusNewDelete { get; set; }
-
-    public bool ClangAnalyzerCplusplusNewDeleteLeaks { get; set; }
-
-    public bool ClangAnalyzerDeadcodeDeadStores { get; set; }
-
-    public bool ClangAnalyzerNullabilityNullPassedToNonnull { get; set; }
-
-    public bool ClangAnalyzerNullabilityNullReturnedFromNonnull { get; set; }
-
-    public bool ClangAnalyzerNullabilityNullableDereferenced { get; set; }
-
-    public bool ClangAnalyzerNullabilityNullablePassedToNonnull { get; set; }
-
-    public bool ClangAnalyzerOptinmpiMPIChecker { get; set; }
-
-    public bool ClangAnalyzerOptinosxcocoalocalizabilityEmptyLocalizationContextChecker { get; set; }
-
-    public bool ClangAnalyzerOptinosxcocoalocalizabilityNonLocalizedStringChecker { get; set; }
-
-    public bool ClangAnalyzerOsxAPI { get; set; }
-
-    public bool ClangAnalyzerOsxNumberObjectConversion { get; set; }
-
-    public bool ClangAnalyzerOsxSecKeychainAPI { get; set; }
-
-    public bool ClangAnalyzerOsxcocoaAtSync { get; set; }
-
-    public bool ClangAnalyzerOsxcocoaClassRelease { get; set; }
-
-    public bool ClangAnalyzerOsxcocoaDealloc { get; set; }
-
-    public bool ClangAnalyzerOsxcocoaIncompatibleMethodTypes { get; set; }
-
-    public bool ClangAnalyzerOsxcocoaNSAutoreleasePool { get; set; }
-
-    public bool ClangAnalyzerOsxcocoaNSError { get; set; }
-
-    public bool ClangAnalyzerOsxcocoaNilArg { get; set; }
-
-    public bool ClangAnalyzerOsxcocoaObjCGenerics { get; set; }
-
-    public bool ClangAnalyzerOsxcocoaRetainCount { get; set; }
-
-    public bool ClangAnalyzerOsxcocoaSelfInit { get; set; }
-
-    public bool ClangAnalyzerOsxcocoaSuperDealloc { get; set; }
-
-    public bool ClangAnalyzerOsxcocoaUnusedIvars { get; set; }
-
-    public bool ClangAnalyzerOsxcocoaVariadicMethodTypes { get; set; }
-
-    public bool ClangAnalyzerOsxcoreFoundationCFError { get; set; }
-
-    public bool ClangAnalyzerOsxcoreFoundationCFNumber { get; set; }
-
-    public bool ClangAnalyzerOsxcoreFoundationCFRetainRelease { get; set; }
-
-    public bool ClangAnalyzerOsxcoreFoundationcontainersOutOfBounds { get; set; }
-
-    public bool ClangAnalyzerOsxcoreFoundationcontainersPointerSizedValues { get; set; }
-
-    public bool ClangAnalyzerSecurityFloatLoopCounter { get; set; }
-
-    public bool ClangAnalyzerSecurityinsecureAPIUncheckedReturn { get; set; }
-
-    public bool ClangAnalyzerSecurityinsecureAPIgetpw { get; set; }
-
-    public bool ClangAnalyzerSecurityinsecureAPIgets { get; set; }
-
-    public bool ClangAnalyzerSecurityinsecureAPImkstemp { get; set; }
-
-    public bool ClangAnalyzerSecurityinsecureAPImktemp { get; set; }
-
-    public bool ClangAnalyzerSecurityinsecureAPIrand { get; set; }
-
-    public bool ClangAnalyzerSecurityinsecureAPIstrcpy { get; set; }
-
-    public bool ClangAnalyzerSecurityinsecureAPIvfork { get; set; }
-
-    public bool ClangAnalyzerUnixAPI { get; set; }
-
-    public bool ClangAnalyzerUnixMalloc { get; set; }
-
-    public bool ClangAnalyzerUnixMallocSizeof { get; set; }
-
-    public bool ClangAnalyzerUnixMismatchedDeallocator { get; set; }
-
-    public bool ClangAnalyzerUnixVfork { get; set; }
-
-    public bool ClangAnalyzerUnixcstringBadSizeArg { get; set; }
-
-    public bool ClangAnalyzerUnixcstringNullArg { get; set; }
-
     public bool AndroidCloexecCreat { get; set; }
 
     public bool AndroidCloexecFopen { get; set; }
@@ -172,6 +54,160 @@
 
     public bool CertOop11Cpp { get; set; }
 
+    public bool ClangAnalyzerApimodelinggoogleGTest { get; set; }
+
+    public bool ClangAnalyzerCorebuiltinBuiltinFunctions { get; set; }
+
+    public bool ClangAnalyzerCorebuiltinNoReturnFunctions { get; set; }
+
+    public bool ClangAnalyzerCoreCallAndMessage { get; set; }
+
+    public bool ClangAnalyzerCoreDivideZero { get; set; }
+
+    public bool ClangAnalyzerCoreDynamicTypePropagation { get; set; }
+
+    public bool ClangAnalyzerCoreNonNullParamChecker { get; set; }
+
+    public bool ClangAnalyzerCoreNullDereference { get; set; }
+
+    public bool ClangAnalyzerCoreStackAddressEscape { get; set; }
+
+    public bool ClangAnalyzerCoreUndefinedBinaryOperatorResult { get; set; }
+
+    public bool ClangAnalyzerCoreuninitializedArraySubscript { get; set; }
+
+    public bool ClangAnalyzerCoreuninitializedAssign { get; set; }
+
+    public bool ClangAnalyzerCoreuninitializedBranch { get; set; }
+
+    public bool ClangAnalyzerCoreuninitializedCapturedBlockVariable { get; set; }
+
+    public bool ClangAnalyzerCoreuninitializedUndefReturn { get; set; }
+
+    public bool ClangAnalyzerCoreVLASize { get; set; }
+
+    public bool ClangAnalyzerCplusplusNewDelete { get; set; }
+
+    public bool ClangAnalyzerCplusplusNewDeleteLeaks { get; set; }
+
+    public bool ClangAnalyzerCplusplusSelfAssignment { get; set; }
+
+    public bool ClangAnalyzerDeadcodeDeadStores { get; set; }
+
+    public bool ClangAnalyzerLlvmConventions { get; set; }
+
+    public bool ClangAnalyzerNullabilityNullableDereferenced { get; set; }
+
+    public bool ClangAnalyzerNullabilityNullablePassedToNonnull { get; set; }
+
+    public bool ClangAnalyzerNullabilityNullableReturnedFromNonnull { get; set; }
+
+    public bool ClangAnalyzerNullabilityNullPassedToNonnull { get; set; }
+
+    public bool ClangAnalyzerNullabilityNullReturnedFromNonnull { get; set; }
+
+    public bool ClangAnalyzerOptincplusplusVirtualCall { get; set; }
+
+    public bool ClangAnalyzerOptinmpiMPIChecker { get; set; }
+
+    public bool ClangAnalyzerOptinosxcocoalocalizabilityEmptyLocalizationContextChecker { get; set; }
+
+    public bool ClangAnalyzerOptinosxcocoalocalizabilityNonLocalizedStringChecker { get; set; }
+
+    public bool ClangAnalyzerOptinperformancePadding { get; set; }
+
+    public bool ClangAnalyzerOptinportabilityUnixAPI { get; set; }
+
+    public bool ClangAnalyzerOsxAPI { get; set; }
+
+    public bool ClangAnalyzerOsxcocoaAtSync { get; set; }
+
+    public bool ClangAnalyzerOsxcocoaClassRelease { get; set; }
+
+    public bool ClangAnalyzerOsxcocoaDealloc { get; set; }
+
+    public bool ClangAnalyzerOsxcocoaIncompatibleMethodTypes { get; set; }
+
+    public bool ClangAnalyzerOsxcocoaLoops { get; set; }
+
+    public bool ClangAnalyzerOsxcocoaMissingSuperCall { get; set; }
+
+    public bool ClangAnalyzerOsxcocoaNilArg { get; set; }
+
+    public bool ClangAnalyzerOsxcocoaNonNilReturnValue { get; set; }
+
+    public bool ClangAnalyzerOsxcocoaNSAutoreleasePool { get; set; }
+
+    public bool ClangAnalyzerOsxcocoaNSError { get; set; }
+
+    public bool ClangAnalyzerOsxcocoaObjCGenerics { get; set; }
+
+    public bool ClangAnalyzerOsxcocoaRetainCount { get; set; }
+
+    public bool ClangAnalyzerOsxcocoaSelfInit { get; set; }
+
+    public bool ClangAnalyzerOsxcocoaSuperDealloc { get; set; }
+
+    public bool ClangAnalyzerOsxcocoaUnusedIvars { get; set; }
+
+    public bool ClangAnalyzerOsxcocoaVariadicMethodTypes { get; set; }
+
+    public bool ClangAnalyzerOsxcoreFoundationCFError { get; set; }
+
+    public bool ClangAnalyzerOsxcoreFoundationCFNumber { get; set; }
+
+    public bool ClangAnalyzerOsxcoreFoundationCFRetainRelease { get; set; }
+
+    public bool ClangAnalyzerOsxcoreFoundationcontainersOutOfBounds { get; set; }
+
+    public bool ClangAnalyzerOsxcoreFoundationcontainersPointerSizedValues { get; set; }
+
+    public bool ClangAnalyzerOsxNumberObjectConversion { get; set; }
+
+    public bool ClangAnalyzerOsxObjCProperty { get; set; }
+
+    public bool ClangAnalyzerOsxSecKeychainAPI { get; set; }
+
+    public bool ClangAnalyzerSecurityFloatLoopCounter { get; set; }
+
+    public bool ClangAnalyzerSecurityinsecureAPIgetpw { get; set; }
+
+    public bool ClangAnalyzerSecurityinsecureAPIgets { get; set; }
+
+    public bool ClangAnalyzerSecurityinsecureAPImkstemp { get; set; }
+
+    public bool ClangAnalyzerSecurityinsecureAPImktemp { get; set; }
+
+    public bool ClangAnalyzerSecurityinsecureAPIrand { get; set; }
+
+    public bool ClangAnalyzerSecurityinsecureAPIstrcpy { get; set; }
+
+    public bool ClangAnalyzerSecurityinsecureAPIUncheckedReturn { get; set; }
+
+    public bool ClangAnalyzerSecurityinsecureAPIvfork { get; set; }
+
+    public bool ClangAnalyzerUnixAPI { get; set; }
+
+    public bool ClangAnalyzerUnixcstringBadSizeArg { get; set; }
+
+    public bool ClangAnalyzerUnixcstringNullArg { get; set; }
+
+    public bool ClangAnalyzerUnixMalloc { get; set; }
+
+    public bool ClangAnalyzerUnixMallocSizeof { get; set; }
+
+    public bool ClangAnalyzerUnixMismatchedDeallocator { get; set; }
+
+    public bool ClangAnalyzerUnixStdCLibraryFunctions { get; set; }
+
+    public bool ClangAnalyzerUnixVfork { get; set; }
+
+    public bool ClangAnalyzerValistCopyToSelf { get; set; }
+
+    public bool ClangAnalyzerValistUninitialized { get; set; }
+
+    public bool ClangAnalyzerValistUnterminated { get; set; }
+
     public bool CppcoreguidelinesCCopyAssignmentSignature { get; set; }
 
     public bool CppcoreguidelinesInterfacesGlobalInit { get; set; }
@@ -214,17 +250,17 @@
 
     public bool GoogleGlobalNamesInHeaders { get; set; }
 
-    public bool GoogleReadabilityBracesAroundStatements { get; set; } = true;
+    public bool GoogleReadabilityBracesAroundStatements { get; set; }
 
-    public bool GoogleReadabilityCasting { get; set; } = true;
+    public bool GoogleReadabilityCasting { get; set; }
 
-    public bool GoogleReadabilityFunctionSize { get; set; } = true;
+    public bool GoogleReadabilityFunctionSize { get; set; }
 
-    public bool GoogleReadabilityNamespaceComments { get; set; } = true;
+    public bool GoogleReadabilityNamespaceComments { get; set; }
 
-    public bool GoogleReadabilityRedundantSmartptrGet { get; set; } = true;
+    public bool GoogleReadabilityRedundantSmartptrGet { get; set; }
 
-    public bool GoogleReadabilityTodo { get; set; } = true;
+    public bool GoogleReadabilityTodo { get; set; }
 
     public bool GoogleRuntimeInt { get; set; }
 
@@ -360,53 +396,53 @@
 
     public bool MiscVirtualNearMiss { get; set; }
 
-    public bool ModernizeAvoidBind { get; set; } = true;
+    public bool ModernizeAvoidBind { get; set; }
 
-    public bool ModernizeDeprecatedHeaders { get; set; } = true;
+    public bool ModernizeDeprecatedHeaders { get; set; }
 
-    public bool ModernizeLoopConvert { get; set; } = true;
+    public bool ModernizeLoopConvert { get; set; }
 
-    public bool ModernizeMakeShared { get; set; } = true;
+    public bool ModernizeMakeShared { get; set; }
 
-    public bool ModernizeMakeUnique { get; set; } = true;
+    public bool ModernizeMakeUnique { get; set; }
 
-    public bool ModernizePassByValue { get; set; } = true;
+    public bool ModernizePassByValue { get; set; }
 
-    public bool ModernizeRawStringLiteral { get; set; } = true;
+    public bool ModernizeRawStringLiteral { get; set; }
 
-    public bool ModernizeRedundantVoidArg { get; set; } = true;
+    public bool ModernizeRedundantVoidArg { get; set; }
 
-    public bool ModernizeReplaceAutoPtr { get; set; } = true;
+    public bool ModernizeReplaceAutoPtr { get; set; }
 
-    public bool ModernizeReplaceRandomShuffle { get; set; } = true;
+    public bool ModernizeReplaceRandomShuffle { get; set; }
 
-    public bool ModernizeReturnBracedInitList { get; set; } = true;
+    public bool ModernizeReturnBracedInitList { get; set; }
 
-    public bool ModernizeShrinkToFit { get; set; } = true;
+    public bool ModernizeShrinkToFit { get; set; }
 
-    public bool ModernizeUnaryStaticAssert { get; set; } = true;
+    public bool ModernizeUnaryStaticAssert { get; set; }
 
-    public bool ModernizeUseAuto { get; set; } = true;
+    public bool ModernizeUseAuto { get; set; }
 
-    public bool ModernizeUseBoolLiterals { get; set; } = true;
+    public bool ModernizeUseBoolLiterals { get; set; }
 
-    public bool ModernizeUseDefaultMemberInit { get; set; } = true;
+    public bool ModernizeUseDefaultMemberInit { get; set; }
 
-    public bool ModernizeUseEmplace { get; set; } = true;
+    public bool ModernizeUseEmplace { get; set; }
 
-    public bool ModernizeUseEqualsDefault { get; set; } = true;
+    public bool ModernizeUseEqualsDefault { get; set; }
 
-    public bool ModernizeUseEqualsDelete { get; set; } = true;
+    public bool ModernizeUseEqualsDelete { get; set; }
 
-    public bool ModernizeUseNoexcept { get; set; } = true;
+    public bool ModernizeUseNoexcept { get; set; }
 
-    public bool ModernizeUseNullptr { get; set; } = true;
+    public bool ModernizeUseNullptr { get; set; }
 
-    public bool ModernizeUseOverride { get; set; } = true;
+    public bool ModernizeUseOverride { get; set; }
 
-    public bool ModernizeUseTransparentFunctors { get; set; } = true;
+    public bool ModernizeUseTransparentFunctors { get; set; }
 
-    public bool ModernizeUseUsing { get; set; } = true;
+    public bool ModernizeUseUsing { get; set; }
 
     public bool MpiBufferDeref { get; set; }
 
@@ -415,6 +451,8 @@
     public bool PerformanceFasterStringFind { get; set; }
 
     public bool PerformanceForRangeCopy { get; set; }
+
+    public bool PerformanceImplicitCastInLoop { get; set; }
 
     public bool PerformanceInefficientStringConcatenation { get; set; }
 
@@ -426,91 +464,53 @@
 
     public bool PerformanceUnnecessaryValueParam { get; set; }
 
-    public bool ReadabilityAvoidConstParamsInDecls { get; set; } = true;
+    public bool ReadabilityAvoidConstParamsInDecls { get; set; }
 
-    public bool ReadabilityBracesAroundStatements { get; set; } = true;
+    public bool ReadabilityBracesAroundStatements { get; set; }
 
-    public bool ReadabilityContainerSizeEmpty { get; set; } = true;
+    public bool ReadabilityContainerSizeEmpty { get; set; }
 
-    public bool ReadabilityDeleteNullPointer { get; set; } = true;
+    public bool ReadabilityDeletedDefault { get; set; }
 
-    public bool ReadabilityDeletedDefault { get; set; } = true;
+    public bool ReadabilityDeleteNullPointer { get; set; }
 
-    public bool ReadabilityElseAfterReturn { get; set; } = true;
+    public bool ReadabilityElseAfterReturn { get; set; }
 
-    public bool ReadabilityFunctionSize { get; set; } = true;
+    public bool ReadabilityFunctionSize { get; set; }
 
-    public bool ReadabilityIdentifierNaming { get; set; } = true;
+    public bool ReadabilityIdentifierNaming { get; set; }
 
-    public bool ReadabilityInconsistentDeclarationParameterName { get; set; } = true;
+    public bool ReadabilityImplicitBoolCast { get; set; }
 
-    public bool ReadabilityMisleadingIndentation { get; set; } = true;
+    public bool ReadabilityInconsistentDeclarationParameterName { get; set; }
 
-    public bool ReadabilityMisplacedArrayIndex { get; set; } = true;
+    public bool ReadabilityMisleadingIndentation { get; set; }
 
-    public bool ReadabilityNamedParameter { get; set; } = true;
+    public bool ReadabilityMisplacedArrayIndex { get; set; }
 
-    public bool ReadabilityNonConstParameter { get; set; } = true;
+    public bool ReadabilityNamedParameter { get; set; }
 
-    public bool ReadabilityRedundantControlFlow { get; set; } = true;
+    public bool ReadabilityNonConstParameter { get; set; }
 
-    public bool ReadabilityRedundantDeclaration { get; set; } = true;
+    public bool ReadabilityRedundantControlFlow { get; set; }
 
-    public bool ReadabilityRedundantFunctionPtrDereference { get; set; } = true;
+    public bool ReadabilityRedundantDeclaration { get; set; }
 
-    public bool ReadabilityRedundantMemberInit { get; set; } = true;
+    public bool ReadabilityRedundantFunctionPtrDereference { get; set; }
 
-    public bool ReadabilityRedundantSmartptrGet { get; set; } = true;
+    public bool ReadabilityRedundantMemberInit { get; set; }
 
-    public bool ReadabilityRedundantStringCstr { get; set; } = true;
+    public bool ReadabilityRedundantSmartptrGet { get; set; }
 
-    public bool ReadabilityRedundantStringInit { get; set; } = true;
+    public bool ReadabilityRedundantStringCstr { get; set; }
 
-    public bool ReadabilitySimplifyBooleanExpr { get; set; } = true;
+    public bool ReadabilityRedundantStringInit { get; set; }
 
-    public bool ReadabilityStaticDefinitionInAnonymousNamespace { get; set; } = true;
+    public bool ReadabilitySimplifyBooleanExpr { get; set; }
 
-    public bool ReadabilityUniqueptrDeleteRelease { get; set; } = true;
+    public bool ReadabilityStaticDefinitionInAnonymousNamespace { get; set; }
 
-    public bool ClangAnalyzerApimodelinggoogleGTest { get; set; }
-
-    public bool ClangAnalyzerCoreDynamicTypePropagation { get; set; }
-
-    public bool ClangAnalyzerCorebuiltinBuiltinFunctions { get; set; }
-
-    public bool ClangAnalyzerCorebuiltinNoReturnFunctions { get; set; }
-
-    public bool ClangAnalyzerCplusplusSelfAssignment { get; set; }
-
-    public bool ClangAnalyzerLlvmConventions { get; set; }
-
-    public bool ClangAnalyzerNullabilityNullableReturnedFromNonnull { get; set; }
-
-    public bool ClangAnalyzerOptincplusplusVirtualCall { get; set; }
-
-    public bool ClangAnalyzerOptinperformancePadding { get; set; }
-
-    public bool ClangAnalyzerOptinportabilityUnixAPI { get; set; }
-
-    public bool ClangAnalyzerOsxObjCProperty { get; set; }
-
-    public bool ClangAnalyzerOsxcocoaLoops { get; set; }
-
-    public bool ClangAnalyzerOsxcocoaMissingSuperCall { get; set; }
-
-    public bool ClangAnalyzerOsxcocoaNonNilReturnValue { get; set; }
-
-    public bool ClangAnalyzerUnixStdCLibraryFunctions { get; set; }
-
-    public bool ClangAnalyzerValistCopyToSelf { get; set; }
-
-    public bool ClangAnalyzerValistUninitialized { get; set; }
-
-    public bool ClangAnalyzerValistUnterminated { get; set; }
-
-    public bool PerformanceImplicitCastInLoop { get; set; }
-
-    public bool ReadabilityImplicitBoolCast { get; set; } = true;
+    public bool ReadabilityUniqueptrDeleteRelease { get; set; }
 
     #endregion
 
