3f902ab29ea20f44c2b40c7343d96d3df1508a0b
Enache Ionut <enachestoianionut@gmail.com>
2018-04-26 11:45:32 +0300

    implemented new user control for clang tidy custom checks page

---
+++ ClangPowerTools/ClangPowerTools/Options/View/ClangTidyCustomChecksUserControl.xaml.cs
@@ -0,0 +1,25 @@
+﻿using ClangPowerTools.DialogPages;
+using System.Windows.Controls;
+using System.Windows.Interop;
+
+namespace ClangPowerTools.Options.View
+{
+  /// <summary>
+  /// Interaction logic for ClangTidyCustomChecksUserControl.xaml
+  /// </summary>
+  public partial class ClangTidyCustomChecksUserControl : UserControl
+  {
+    public ClangTidyCustomChecksUserControl(ClangTidyCustomChecksOptionsView clangTidyCustomChecksOption)
+    {
+      InitializeComponent();
+
+      wpfPropGrid.SelectedObject = clangTidyCustomChecksOption;
+
+      Loaded += delegate
+      {
+        UIElementsActivator.Activate(HwndSource.FromVisual(this) as HwndSource);
+      };
+
+    }
+  }
+}
