39551c0445d60e816cf69052beca295989e05b80
Enache Ionut <enachestoianionut@gmail.com>
2018-04-27 12:09:02 +0300

    added clang tidy options user control

---
+++ ClangPowerTools/ClangPowerTools/Options/View/ClangTidyOptionsUserControl.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Controls;
+using System.Windows.Interop;
+
+namespace ClangPowerTools.Options.View
+{
+  /// <summary>
+  /// Interaction logic for ClangTidyOptionsUserControl.xaml
+  /// </summary>
+  public partial class ClangTidyOptionsUserControl : UserControl
+  {
+    public ClangTidyOptionsUserControl(ClangTidyOptionsView clangTidyOptions)
+    {
+      InitializeComponent();
+
+      wpfPropGrid.SelectedObject = clangTidyOptions;
+
+      Loaded += delegate
+      {
+        UIElementsActivator.Activate(HwndSource.FromVisual(this) as HwndSource);
+      };
+    }
+  }
+}
