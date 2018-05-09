334d7072c0a18f0210f9847441c8d92248ae7536
Enache Ionut <enachestoianionut@gmail.com>
2018-05-09 10:50:54 +0300

    extend the textbox base class and add the browse mechanism for selecting the path for an exe file

---
+++ ClangPowerTools/ClangPowerTools/Options/View/WpfPropertyGrid/BorderedTextBoxBrowse.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Documents;
+
+namespace ClangPowerTools.Options.View.WpfPropertyGrid
+{
+  /// <summary>
+  /// Interaction logic for BorderedTextBoxBrowse.xaml
+  /// </summary>
+  public partial class BorderedTextBoxBrowse : TextBoxNotifaiableUserControl
+  {
+    public BorderedTextBoxBrowse()
+    {
+      InitializeComponent();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+      // Create OpenFileDialog
+      Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+      // Set filter for file extension and default file extension
+      dlg.DefaultExt = ".exe";
+      dlg.Filter = "Text documents (.exe)|*.exe";
+
+      // Display OpenFileDialog by calling ShowDialog method
+      Nullable<bool> result = dlg.ShowDialog();
+
+      // Get the selected file name and display in a TextBox
+      if (result == true)
+      {
+        // Open document
+        string filename = dlg.FileName;
+        TextEditor.Text = filename;
+
+        Paragraph paragraph = new Paragraph();
+        paragraph.Inlines.Add(System.IO.File.ReadAllText(filename));
+        //FlowDocument document = new FlowDocument(paragraph);
+        //FlowDocReader.Document = document;
+      }
+    }
+  }
+}
