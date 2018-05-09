5cf3e689e35c5d7aeaed949dc3be4d216ba8559f
Enache Ionut <enachestoianionut@gmail.com>
2018-05-09 10:53:57 +0300

    implemented custom attribute to identify the clang-format path property in UI

---
+++ ClangPowerTools/ClangPowerTools/TextBoxAndBrowseAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClangPowerTools
+{
+  [AttributeUsage(AttributeTargets.Property)]
+  public class TextBoxAndBrowseAttribute : Attribute
+  {
+    public bool Activate { get; set; }
+
+    public TextBoxAndBrowseAttribute(bool aActivate)
+    {
+      Activate = aActivate;
+    }
+  }
+}
