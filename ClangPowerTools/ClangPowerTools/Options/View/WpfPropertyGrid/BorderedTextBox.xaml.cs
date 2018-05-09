fcc9f1266c3d88694456bd06204c8788ae75b5fb
Enache Ionut <enachestoianionut@gmail.com>
2018-05-09 10:48:19 +0300

    extend the textbox base class and add the String Collection Editor button

--- ClangPowerTools/ClangPowerTools/Options/View/WpfPropertyGrid/BorderedTextBox.xaml.cs
+++ ClangPowerTools/ClangPowerTools/Options/View/WpfPropertyGrid/BorderedTextBox.xaml.cs
@@ -1,8 +1,5 @@
-﻿using System.ComponentModel;
-using System.Reflection;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Input;
+﻿using System.Windows;
+using ClangPowerTools.Options.View;
 using ClangPowerTools.Options.View.WpfPropertyGrid;
 
 namespace Caphyon.AdvInstVSIntegration.ProjectEditor.View.WpfPropertyGrid
@@ -10,46 +7,13 @@ namespace Caphyon.AdvInstVSIntegration.ProjectEditor.View.WpfPropertyGrid
   /// <summary>
   /// Interaction logic for BorderedTextBox.xaml
   /// </summary>
-  public partial class BorderedTextBox : UserControl, INotifyPropertyChanged
+  public partial class BorderedTextBox : TextBoxNotifaiableUserControl
   {
     public BorderedTextBox()
     {
       InitializeComponent();
     }
-
-    private void TextBoxPreviewMouseUp(object sender, MouseButtonEventArgs e)
-    {
-      MouseUpDownEventForwarding(sender, e, UIElement.MouseUpEvent);
-    }
-
-    private void MouseUpDownEventForwarding(object sender, MouseButtonEventArgs e, RoutedEvent evFwd)
-    {
-      TextBox tb = sender as TextBox;
-      if (null != tb)
-      {
-        Border b = tb.Parent as Border;
-        if (null != b)
-        {
-          MouseButtonEventArgs newEvArgs = new MouseButtonEventArgs(e.MouseDevice, e.Timestamp, e.ChangedButton);
-          newEvArgs.RoutedEvent = evFwd;
-          b.RaiseEvent(newEvArgs);
-        }
-      }
-    }
-
-    private void TextBoxPreviewMouseDown(object sender, MouseButtonEventArgs e)
-    {
-      MouseUpDownEventForwarding(sender, e, UIElement.MouseDownEvent);
-    }
-
-    public event PropertyChangedEventHandler PropertyChanged;
-
-    public void NotifyPropertyChanged(string propName)
-    {
-      if (PropertyChanged != null)
-        PropertyChanged(this, new PropertyChangedEventArgs(propName));
-    }
-
+   
     private void Button_Click(object sender, RoutedEventArgs e)
     {
       PropertyData propData = (PropertyData)DataContext;
