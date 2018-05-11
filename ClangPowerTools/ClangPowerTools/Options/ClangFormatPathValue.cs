7978005ffd9c57714f607648b42edbb1e51a7448
Enache Ionut <enachestoianionut@gmail.com>
2018-05-11 13:04:02 +0300

    implemented clang format path class. this class will contain the checkbox and text values

---
+++ ClangPowerTools/ClangPowerTools/Options/ClangFormatPathValue.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace ClangPowerTools.Options
+{
+  public class ClangFormatPathValue : INotifyPropertyChanged
+  {
+    #region Members 
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public String mPath;
+
+    #endregion
+
+    #region Properties
+
+    public bool Enable { get; set; } = false;
+
+    public string Path
+    {
+      get
+      {
+        return mPath;
+      }
+      set
+      {
+        mPath = value;
+        if (PropertyChanged != null)
+          PropertyChanged(this, new PropertyChangedEventArgs("Path"));
+      }
+    }
+
+    #endregion
+
+  }
+}
