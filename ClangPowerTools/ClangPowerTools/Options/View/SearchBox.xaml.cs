9650c6203c1472d2bb00f59336982ddd08f6832d
Enache Ionut <enachestoianionut@gmail.com>
2018-06-11 11:04:03 +0300

    commented the SearchBox fuctionality

--- ClangPowerTools/ClangPowerTools/Options/View/SearchBox.xaml.cs
+++ ClangPowerTools/ClangPowerTools/Options/View/SearchBox.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System.ComponentModel;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 
@@ -8,41 +7,40 @@ namespace ClangPowerTools.Options.View
   /// <summary>
   /// Interaction logic for SearchBox.xaml
   /// </summary>
-  public partial class SearchBox : UserControl, INotifyPropertyChanged
+  public partial class SearchBox : UserControl //, INotifyPropertyChanged
   {
+    #region Cosntructor
 
-    private string mSearchtext = string.Empty;
-
-    public event PropertyChangedEventHandler PropertyChanged;
-
-    public string SearchText
-    {
-      get { return mSearchtext; }
-      set
-      {
-        mSearchtext = value;
-        OnPropertyChanged("SearchText");
-      }
-    }
-
+    /// <summary>
+    /// Default constructor
+    /// </summary>
     public SearchBox()
     {
       InitializeComponent();
     }
 
-    private void OnPropertyChanged(string aPropertyName)
-    {
-      if (PropertyChanged != null)
-        PropertyChanged(this, new PropertyChangedEventArgs(aPropertyName));
-    }
+    #endregion
 
 
+    #region Private Methods
+
+    /// <summary>
+    /// When the SearchBox got focus select the TextBox to make it's border blue
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     private void SearchBox_GotFocus(object sender, RoutedEventArgs e)
     {
       var t = (TextBox)sender;
       t.SelectAll();
     }
 
+
+    /// <summary>
+    /// When the SearchBox got mouse capture select the TextBox to make it's border blue
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     private void SearchTextBox_GotMouseCapture(object sender, MouseEventArgs e)
     {
       var t = (TextBox)sender;
@@ -50,13 +48,23 @@ namespace ClangPowerTools.Options.View
     }
 
 
+    /// <summary>
+    /// Focus the SearchBox or clear all the text from it
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     private void Search_Click(object sender, RoutedEventArgs e)
     {
+      // If the SearchBox is not empty then the Clear Search option was pressed 
+      // Make the SearchBox empty
       if (false == string.IsNullOrWhiteSpace(SearchTextBox.Text))
         SearchTextBox.Text = string.Empty;
 
+      // Put the mouse cursor inside the SearchBox by focus it
       SearchTextBox.Focus();
     }
+
+    #endregion
 
   }
 }
