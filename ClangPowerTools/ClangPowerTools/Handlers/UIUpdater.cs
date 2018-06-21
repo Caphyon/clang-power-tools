835f3c50a37e872c2468b004d03bad705446e402
Enache Ionut <enachestoianionut@gmail.com>
2018-06-21 14:43:43 +0300

    implement a handler which will execute the UI actions on the UI thread

---
+++ ClangPowerTools/ClangPowerTools/Handlers/UIUpdater.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.Shell;
+using System;
+
+namespace ClangPowerTools.Handlers
+{
+  public class UIUpdater
+  {
+    #region Public Methods
+
+    public async static void BeginInvoke(Action aAction)
+    {
+      await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+      aAction.BeginInvoke(aAction.EndInvoke, null);
+    }
+
+    public async static void Invoke(Action aAction)
+    {
+      await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+      aAction.Invoke();
+    }
+
+    #endregion
+
+  }
+}
