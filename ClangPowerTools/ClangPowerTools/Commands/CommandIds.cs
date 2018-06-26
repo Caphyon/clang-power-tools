3b0c9a7b1e74ec25808c4774c1588c085a038c49
Enache Ionut <enachestoianionut@gmail.com>
2018-06-26 18:22:16 +0300

    register the new command

--- ClangPowerTools/ClangPowerTools/Commands/CommandIds.cs
+++ ClangPowerTools/ClangPowerTools/Commands/CommandIds.cs
@@ -4,11 +4,12 @@
   {
     #region Constants
 
-    public const int kTidyId = 0x0101;
-    public const int kCompileId = 0x0102;
-    public const int kSettingsId = 0x0103;
-    public const int kStopClang = 0x0104;
-    public const int kClangFormat = 0x0105;
+    public const int kCompileId     = 0x0102;
+    public const int kTidyId        = 0x0101;
+    public const int kTidyFixId     = 0x0106;
+    public const int kClangFormat   = 0x0105;
+    public const int kStopClang     = 0x0104;
+    public const int kSettingsId    = 0x0103;
 
     #endregion
 
