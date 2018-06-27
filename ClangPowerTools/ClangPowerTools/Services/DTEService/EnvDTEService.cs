8b34ecb957b40adfe809404a793eaee1b4e8a166
Enache Ionut <enachestoianionut@gmail.com>
2018-06-27 19:29:27 +0300

    create custom services to get async the necessary components

---
+++ ClangPowerTools/ClangPowerTools/Services/DTEService/EnvDTEService.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading;
+using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio.Shell;
+
+namespace ClangPowerTools.Services
+{
+  public class EnvDTEService : SEnvDTEService, IEnvDTEService
+  {
+    #region Members
+
+    private IAsyncServiceProvider mServiceProvider;
+
+    #endregion
+
+
+    #region Constructor 
+
+    public EnvDTEService(IAsyncServiceProvider aAsyncServiceProvider)
+      => mServiceProvider = aAsyncServiceProvider;
+
+    #endregion
+
+
+    #region IEnvDTEService implementation
+
+    public async System.Threading.Tasks.Task<DTE2> GetDTE2Async(IAsyncServiceProvider provider, CancellationToken cancellationToken)
+    {
+      return await provider.GetServiceAsync(typeof(DTE)) as DTE2;
+    }
+
+    #endregion
+
+  }
+}
---
+++ ClangPowerTools/ClangPowerTools/Services/DTEService/IEnvDTEService.cs
@@ -0,0 +1,11 @@
+﻿using EnvDTE80;
+using Microsoft.VisualStudio.Shell;
+using System.Threading;
+
+namespace ClangPowerTools.Services
+{
+  public interface IEnvDTEService
+  {
+    System.Threading.Tasks.Task<DTE2> GetDTE2Async(IAsyncServiceProvider provider, CancellationToken cancellationToken);
+  }
+}
