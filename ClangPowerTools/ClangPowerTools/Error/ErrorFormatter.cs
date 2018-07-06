341f318a99da6e0e0f4f29e24dbfd4dc31362114
Enache Ionut <enachestoianionut@gmail.com>
2018-07-06 15:50:42 +0300

    implement text formater interface to edit the errors from the stcript output

---
+++ ClangPowerTools/ClangPowerTools/Error/ErrorFormatter.cs
@@ -0,0 +1,20 @@
+﻿using ClangPowerTools.TextOperationsInterfaces;
+using System.Text.RegularExpressions;
+
+namespace ClangPowerTools.Error
+{
+  class ErrorFormatter : ITextFormatter
+  {
+
+    #region ITextFormater Implementation
+
+    public string Format(string aText, string aReplacement)
+    {
+      Regex regex = new Regex(ErrorParserConstants.kErrorMessageRegex);
+      return regex.Replace(aText, aReplacement, 1);
+    }
+
+    #endregion
+
+  }
+}
---
+++ ClangPowerTools/ClangPowerTools/TextOperationsInterfaces/ITextFormatter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClangPowerTools.TextOperationsInterfaces
+{
+  public interface ITextFormatter
+  {
+    string Format(string aText, string aReplacement);
+
+
+  }
+}
