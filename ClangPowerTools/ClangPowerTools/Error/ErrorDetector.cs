8d70ae6304285aa49b0f2c4301e57dba154361f4
Enache Ionut <enachestoianionut@gmail.com>
2018-07-06 15:47:22 +0300

    detect if any type of errors exist in the a specific text

---
+++ ClangPowerTools/ClangPowerTools/Error/ErrorDetector.cs
@@ -0,0 +1,35 @@
+﻿using ClangPowerTools.TextOperationsInterfaces;
+using System.Text.RegularExpressions;
+
+namespace ClangPowerTools
+{
+  public class ErrorDetector : IDetector
+  {
+
+    #region IDetector Implementation
+
+
+    public bool Detect(string aText, out Match aMatchResult)
+    {
+      Regex regex = new Regex(ErrorParserConstants.kErrorMessageRegex);
+      aMatchResult = regex.Match(aText);
+      return aMatchResult.Success;
+    }
+
+
+    #endregion
+
+
+    #region Public Methods
+
+
+    public bool LlvmIsMissing(string aMessages)
+    {
+      return aMessages.Contains(ErrorParserConstants.kCompileClangMissingFromPath) ||
+        aMessages.Contains(ErrorParserConstants.kTidyClangMissingFromPath);
+    }
+
+    #endregion
+
+  }
+}
