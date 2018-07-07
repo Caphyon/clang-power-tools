d3acb14a3bdd46439dc664f25a15a5aef37d22f0
Enache Ionut <enachestoianionut@gmail.com>
2018-07-07 20:54:27 +0300

    implement all error window functionalities  in one wrapper class

--- ClangPowerTools/ClangPowerTools/Error/ErrorWindow.cs
+++ ClangPowerTools/ClangPowerTools/Error/ErrorWindow.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using ClangPowerTools.Handlers;
+using EnvDTE;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
 
@@ -8,68 +11,53 @@ namespace ClangPowerTools
   {
     #region Members
 
-    private static ErrorListProvider mErrorProvider = null;
+    private static ErrorListProvider mErrorListProvider = null;
+
+    #endregion
+
+    #region Constructor
+
+    /// <summary>
+    /// Instance Constructor
+    /// </summary>
+    /// <param name="aServiceProvider"></param>
+    public ErrorWindow(IServiceProvider aServiceProvider)
+    {
+      mErrorListProvider = new ErrorListProvider(aServiceProvider);
+    }
 
     #endregion
 
 
     #region Public Methods
 
-    public void Initialize(IServiceProvider aServiceProvider)
+
+    public void AddErrors(IEnumerable<TaskErrorModel> aErrors)
     {
-      if (null == mErrorProvider)
-        mErrorProvider = new ErrorListProvider(aServiceProvider);
-    }
-
-    public void Show() => mErrorProvider.Show();
-
-    public void Clear() => mErrorProvider.Tasks.Clear();
-
-    public bool HasErrors() => 0 != mErrorProvider.Tasks.Count;
-
-    public void SuspendRefresh() => mErrorProvider.SuspendRefresh();
-
-    public void ResumeRefresh() => mErrorProvider.ResumeRefresh();
-
-    public void AddError(TaskErrorModel aError) => AddTask(aError);
-
-    public void RemoveErrors(IVsHierarchy aHierarchy) => RemoveTasks(aHierarchy);
-
-    #endregion
-
-
-    #region Private Methods
-
-    private void AddTask(TaskErrorModel aError)
-    {
-      ErrorTask errorTask = new ErrorTask
+      UIUpdater.Invoke(() =>
       {
-        ErrorCategory = aError.Category,
-        Document = aError.FilePath,
-        Text = aError.Description,
-        Line = aError.Line - 1,
-        Column = aError.Column,
-        Category = TaskCategory.BuildCompile,
-        Priority = TaskPriority.High,
-        HierarchyItem = aError.HierarchyItem
-      };
-      errorTask.Navigate += ErrorTaskNavigate;
-      mErrorProvider.Tasks.Add(errorTask);
-    }
+        mErrorListProvider.SuspendRefresh();
 
-    private void RemoveTasks(IVsHierarchy aHierarchy)
-    {
-      for (int i = mErrorProvider.Tasks.Count - 1; i >= 0; --i)
-      {
-        var errorTask = mErrorProvider.Tasks[i] as ErrorTask;
-        aHierarchy.GetCanonicalName(Microsoft.VisualStudio.VSConstants.VSITEMID_ROOT, out string nameInHierarchy);
-        errorTask.HierarchyItem.GetCanonicalName(Microsoft.VisualStudio.VSConstants.VSITEMID_ROOT, out string nameErrorTaskHierarchy);
-        if (nameInHierarchy == nameErrorTaskHierarchy)
+        foreach (TaskErrorModel error in aErrors)
         {
-          errorTask.Navigate -= ErrorTaskNavigate;
-          mErrorProvider.Tasks.Remove(errorTask);
+          ErrorTask errorTask = new ErrorTask
+          {
+            ErrorCategory = error.Category,
+            Document = error.FilePath,
+            Text = error.Description,
+            Line = error.Line - 1,
+            Column = error.Column,
+            Category = TaskCategory.BuildCompile,
+            Priority = TaskPriority.High,
+            HierarchyItem = error.HierarchyItem
+          };
+          errorTask.Navigate += ErrorTaskNavigate;
+          mErrorListProvider.Tasks.Add(errorTask);
         }
-      }
+
+        mErrorListProvider.BringToFront();
+        mErrorListProvider.ResumeRefresh();
+      });
     }
 
 
@@ -77,11 +65,48 @@ namespace ClangPowerTools
     {
       ErrorTask objErrorTask = (ErrorTask)sender;
       objErrorTask.Line += 1;
-      bool bResult = mErrorProvider.Navigate(objErrorTask, new Guid(EnvDTE.Constants.vsViewKindCode));
+      bool bResult = mErrorListProvider.Navigate(objErrorTask, new Guid(EnvDTE.Constants.vsViewKindCode));
       objErrorTask.Line -= 1;
     }
 
-    #endregion
 
+    public void RemoveErrors(IVsHierarchy aHierarchy)
+    {
+      UIUpdater.Invoke(() =>
+      {
+        mErrorListProvider.SuspendRefresh();
+
+        for (int i = mErrorListProvider.Tasks.Count - 1; i >= 0; --i)
+        {
+          var errorTask = mErrorListProvider.Tasks[i] as ErrorTask;
+          aHierarchy.GetCanonicalName(Microsoft.VisualStudio.VSConstants.VSITEMID_ROOT, out string nameInHierarchy);
+          errorTask.HierarchyItem.GetCanonicalName(Microsoft.VisualStudio.VSConstants.VSITEMID_ROOT, out string nameErrorTaskHierarchy);
+          if (nameInHierarchy == nameErrorTaskHierarchy)
+          {
+            errorTask.Navigate -= ErrorTaskNavigate;
+            mErrorListProvider.Tasks.Remove(errorTask);
+          }
+        }
+
+        mErrorListProvider.ResumeRefresh();
+      });
+    }
+
+
+    public void Clear()
+    {
+      UIUpdater.Invoke(() =>
+      {
+        mErrorListProvider.Tasks.Clear();
+      });
+    }
+
+
+    public void OnBuildBegin(vsBuildScope Scope, vsBuildAction Action)
+    {
+      Clear();
+    }
+
+    #endregion
   }
 }
