49e9bd5b07d574645b6795ab8155ebff193df503
Enache Ionut <enachestoianionut@gmail.com>
2018-07-12 17:57:47 +0300

    commented unnecessary classes

--- ClangPowerTools/ClangPowerTools/Output/OutputManager.cs
+++ ClangPowerTools/ClangPowerTools/Output/OutputManager.cs
@@ -1,203 +1,203 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Text;
-using System.Windows.Threading;
-using EnvDTE80;
-using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.Shell.Interop;
-using ClangPowerTools.Handlers;
-using System.Text.RegularExpressions;
-using ClangPowerTools.Error;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Diagnostics;
+//using System.Text;
+//using System.Windows.Threading;
+//using EnvDTE80;
+//using Microsoft.VisualStudio.Shell;
+//using Microsoft.VisualStudio.Shell.Interop;
+//using ClangPowerTools.Handlers;
+//using System.Text.RegularExpressions;
+//using ClangPowerTools.Error;
 
-namespace ClangPowerTools
-{
-  public class OutputManager
-  {
-    #region Members
+//namespace ClangPowerTools
+//{
+//  public class OutputWindowManager
+//  {
+//    #region Members
 
-    private DTE2 mDte = null;
-    private OutputWindow mOutputWindow = null;
+//    private DTE2 mDte = null;
+//    private OutputWindow mOutputWindow = null;
 
-    private int kBufferSize = 5;
-    private List<string> mMessagesBuffer = new List<string>();
+//    private int kBufferSize = 5;
+//    private List<string> mMessagesBuffer = new List<string>();
 
-    private ErrorDetector mErrorParser = new ErrorDetector();
+//    private ErrorDetector mErrorParser = new ErrorDetector();
 
-    private bool mMissingLlvm = false;
-    private HashSet<TaskErrorModel> mErrors = new HashSet<TaskErrorModel>();
+//    private bool mMissingLlvm = false;
+//    private HashSet<TaskErrorModel> mErrors = new HashSet<TaskErrorModel>();
 
-    private List<string> mPCHPaths = new List<string>();
+//    private List<string> mPCHPaths = new List<string>();
 
-    #endregion
+//    #endregion
 
-    #region Properties
+//    #region Properties
 
-    public bool MissingLlvm => mMissingLlvm;
+//    public bool MissingLlvm => mMissingLlvm;
 
-    public List<string> Buffer => mMessagesBuffer;
+//    public List<string> Buffer => mMessagesBuffer;
 
-    public bool EmptyBuffer => mMessagesBuffer.Count == 0;
+//    public bool EmptyBuffer => mMessagesBuffer.Count == 0;
 
-    public HashSet<TaskErrorModel> Errors => mErrors;
+//    public HashSet<TaskErrorModel> Errors => mErrors;
 
-    public bool HasErrors => 0 != mErrors.Count;
+//    public bool HasErrors => 0 != mErrors.Count;
 
-    public List<string> PCHPaths => mPCHPaths;
+//    public List<string> PCHPaths => mPCHPaths;
 
-    public IVsHierarchy Hierarchy { get; set; }
-    #endregion
+//    public IVsHierarchy Hierarchy { get; set; }
+//    #endregion
 
-    #region Constructor
+//    #region Constructor
 
-    public OutputManager(DTE2 aDte)
-    {
-      mDte = aDte;
-      mOutputWindow = new OutputWindow(mDte);
-    }
+//    public OutputWindowManager(DTE2 aDte)
+//    {
+//      mDte = aDte;
+//      mOutputWindow = new OutputWindow(mDte);
+//    }
 
-    #endregion
+//    #endregion
 
-    #region Public Methods
+//    #region Public Methods
 
-    public void Clear()
-    {
-      UIUpdater.Invoke(() =>
-      {
-        mOutputWindow.Clear();
-      });
-    }
+//    public void Clear()
+//    {
+//      UIUpdater.Invoke(() =>
+//      {
+//        mOutputWindow.Clear();
+//      });
+//    }
 
-    public void Show()
-    {
-      UIUpdater.Invoke(() =>
-      {
-        mOutputWindow.Show(mDte);
-      });
-    }
+//    public void Show()
+//    {
+//      UIUpdater.Invoke(() =>
+//      {
+//        mOutputWindow.Show(mDte);
+//      });
+//    }
 
-    public void AddMessage(string aMessage)
-    {
-      UIUpdater.Invoke(() =>
-      {
-        if (String.IsNullOrWhiteSpace(aMessage))
-          return;
-        mOutputWindow.Write(aMessage);
-      });
-    }
+//    public void AddMessage(string aMessage)
+//    {
+//      UIUpdater.Invoke(() =>
+//      {
+//        if (String.IsNullOrWhiteSpace(aMessage))
+//          return;
+//        mOutputWindow.Write(aMessage);
+//      });
+//    }
 
-    private void ProcessOutput(string aMessage)
-    {
-      try
-      {
-        if (mErrorParser.LlvmIsMissing(aMessage))
-        {
-          mMissingLlvm = true;
-        }
-        else if (!mMissingLlvm)
-        {
-          string text = String.Join("\n", mMessagesBuffer) + "\n";
+//    private void ProcessOutput(string aMessage)
+//    {
+//      try
+//      {
+//        if (mErrorParser.LlvmIsMissing(aMessage))
+//        {
+//          mMissingLlvm = true;
+//        }
+//        else if (!mMissingLlvm)
+//        {
+//          string text = String.Join("\n", mMessagesBuffer) + "\n";
 
-          // Find error messages from powershell script output
-          // replace them with an error message format that VS output window knows to interpret
-          if (mErrorParser.Detect(text, out Match aMarchResult))
-          {
-            IBuilder<TaskErrorModel> errorBuilder = new TaskErrorModelBuilder(aMarchResult);
-            errorBuilder.Build();
-            TaskErrorModel error = errorBuilder.GetResult();
+//          // Find error messages from powershell script output
+//          // replace them with an error message format that VS output window knows to interpret
+//          if (mErrorParser.Detect(text, out Match aMarchResult))
+//          {
+//            IBuilder<TaskErrorModel> errorBuilder = new TaskErrorModelBuilder(aMarchResult);
+//            errorBuilder.Build();
+//            TaskErrorModel error = errorBuilder.GetResult();
 
-            error.HierarchyItem = Hierarchy;
-            List<TaskErrorModel> errors = new List<TaskErrorModel>();
-            errors.Add(error);
+//            error.HierarchyItem = Hierarchy;
+//            List<TaskErrorModel> errors = new List<TaskErrorModel>();
+//            errors.Add(error);
 
-            StringBuilder output = new StringBuilder(
-              GetOutput(ref text, error.FullMessage));
+//            StringBuilder output = new StringBuilder(
+//              GetOutput(ref text, error.FullMessage));
 
-            while (mErrorParser.Detect(text, out aMarchResult))
-            {
-              errorBuilder = new TaskErrorModelBuilder(aMarchResult);
-              errorBuilder.Build();
-              error = errorBuilder.GetResult();
+//            while (mErrorParser.Detect(text, out aMarchResult))
+//            {
+//              errorBuilder = new TaskErrorModelBuilder(aMarchResult);
+//              errorBuilder.Build();
+//              error = errorBuilder.GetResult();
 
-              error.HierarchyItem = Hierarchy;
-              errors.Add(error);
-              output.Append(GetOutput(ref text, error.FullMessage));
-            }
+//              error.HierarchyItem = Hierarchy;
+//              errors.Add(error);
+//              output.Append(GetOutput(ref text, error.FullMessage));
+//            }
 
-            AddMessage(output.ToString());
-            output.Clear();
+//            AddMessage(output.ToString());
+//            output.Clear();
 
-            if (0 != mMessagesBuffer.Count)
-              mMessagesBuffer.Clear();
+//            if (0 != mMessagesBuffer.Count)
+//              mMessagesBuffer.Clear();
 
-            SaveErrorsMessages(errors);
-          }
-          else if (kBufferSize <= mMessagesBuffer.Count)
-          {
-            AddMessage(mMessagesBuffer[0]);
-            mMessagesBuffer.RemoveAt(0);
-          }
-        }
-      }
-      catch (Exception)
-      {
+//            SaveErrorsMessages(errors);
+//          }
+//          else if (kBufferSize <= mMessagesBuffer.Count)
+//          {
+//            AddMessage(mMessagesBuffer[0]);
+//            mMessagesBuffer.RemoveAt(0);
+//          }
+//        }
+//      }
+//      catch (Exception)
+//      {
 
-      }
+//      }
 
-    }
+//    }
 
-    public void OutputDataReceived(object sender, DataReceivedEventArgs e)
-    {
-      if (null == e.Data)
-        return;
-      mMessagesBuffer.Add(e.Data);
-      ProcessOutput(e.Data);
-    }
+//    public void OutputDataReceived(object sender, DataReceivedEventArgs e)
+//    {
+//      if (null == e.Data)
+//        return;
+//      mMessagesBuffer.Add(e.Data);
+//      ProcessOutput(e.Data);
+//    }
 
-    public void OutputDataErrorReceived(object sender, DataReceivedEventArgs e)
-    {
-      if (null == e.Data)
-        return;
-      mMessagesBuffer.Add(e.Data);
-      ProcessOutput(e.Data);
-    }
+//    public void OutputDataErrorReceived(object sender, DataReceivedEventArgs e)
+//    {
+//      if (null == e.Data)
+//        return;
+//      mMessagesBuffer.Add(e.Data);
+//      ProcessOutput(e.Data);
+//    }
 
-    #endregion
+//    #endregion
 
-    #region Private Methods
+//    #region Private Methods
 
-    private void SaveErrorsMessages(List<TaskErrorModel> aErrorCollection)
-    {
-      if (0 == aErrorCollection.Count)
-        return;
+//    private void SaveErrorsMessages(List<TaskErrorModel> aErrorCollection)
+//    {
+//      if (0 == aErrorCollection.Count)
+//        return;
 
-      foreach (var newError in aErrorCollection)
-      {
-        if (null == newError)
-          continue;
-        mErrors.Add(newError);
-      }
-    }
+//      foreach (var newError in aErrorCollection)
+//      {
+//        if (null == newError)
+//          continue;
+//        mErrors.Add(newError);
+//      }
+//    }
 
-    private string GetOutput(ref string aText, string aSearchedSubstring)
-    {
-      ErrorFormatter errorFormatter = new ErrorFormatter();
-      aText = errorFormatter.Format(aText, aSearchedSubstring);
+//    private string GetOutput(ref string aText, string aSearchedSubstring)
+//    {
+//      ErrorFormatter errorFormatter = new ErrorFormatter();
+//      aText = errorFormatter.Format(aText, aSearchedSubstring);
 
-      GetBeforeAndAfterSubstrings(aText, aSearchedSubstring,
-        out string substringBefore, out string substringAfter);
+//      GetBeforeAndAfterSubstrings(aText, aSearchedSubstring,
+//        out string substringBefore, out string substringAfter);
 
-      aText = substringAfter;
-      return substringBefore + aSearchedSubstring;
-    }
+//      aText = substringAfter;
+//      return substringBefore + aSearchedSubstring;
+//    }
 
-    private void GetBeforeAndAfterSubstrings(string aText, string aSearchedSubstring, out string aTextBefore, out string aTextAfter)
-    {
-      aTextBefore = StringExtension.SubstringBefore(aText, aSearchedSubstring);
-      aTextAfter = StringExtension.SubstringAfter(aText, aSearchedSubstring);
-    }
+//    private void GetBeforeAndAfterSubstrings(string aText, string aSearchedSubstring, out string aTextBefore, out string aTextAfter)
+//    {
+//      aTextBefore = StringExtension.SubstringBefore(aText, aSearchedSubstring);
+//      aTextAfter = StringExtension.SubstringAfter(aText, aSearchedSubstring);
+//    }
 
-    #endregion
+//    #endregion
 
-  }
-}
+//  }
+//}
--- ClangPowerTools/ClangPowerTools/Output/OutputWindow.cs
+++ ClangPowerTools/ClangPowerTools/Output/OutputWindow.cs
@@ -1,69 +1,69 @@
-﻿using EnvDTE80;
-using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.Shell.Interop;
-using System;
-using System.IO;
-using System.Text;
+﻿//using EnvDTE80;
+//using Microsoft.VisualStudio.Shell;
+//using Microsoft.VisualStudio.Shell.Interop;
+//using System;
+//using System.IO;
+//using System.Text;
 
-namespace ClangPowerTools
-{
-  public class OutputWindow : TextWriter
-  {
-    #region Members
+//namespace ClangPowerTools
+//{
+//  public class OutputWindow : TextWriter
+//  {
+//    #region Members
 
-    private static readonly Guid mPaneGuid = new Guid("AB9F45E4-2001-4197-BAF5-4B165222AF29");
-    private static IVsOutputWindow mOutputWindow = null;
-    private static IVsOutputWindowPane mOutputPane = null;
+//    private static readonly Guid mPaneGuid = new Guid("AB9F45E4-2001-4197-BAF5-4B165222AF29");
+//    private static IVsOutputWindow mOutputWindow = null;
+//    private static IVsOutputWindowPane mOutputPane = null;
 
-    #endregion
+//    #endregion
 
-    #region Constructor
+//    #region Constructor
 
-    public OutputWindow(DTE2 aDte)
-    {
-      if( null == mOutputWindow )
-      {
-        IServiceProvider serviceProvider = 
-          new ServiceProvider(aDte as Microsoft.VisualStudio.OLE.Interop.IServiceProvider);
-        mOutputWindow = serviceProvider.GetService(typeof(SVsOutputWindow)) as IVsOutputWindow;
-      }
+//    public OutputWindow(DTE2 aDte)
+//    {
+//      if( null == mOutputWindow )
+//      {
+//        IServiceProvider serviceProvider = 
+//          new ServiceProvider(aDte as Microsoft.VisualStudio.OLE.Interop.IServiceProvider);
+//        mOutputWindow = serviceProvider.GetService(typeof(SVsOutputWindow)) as IVsOutputWindow;
+//      }
 
-      if (null == mOutputPane)
-      {
-        Guid generalPaneGuid = mPaneGuid;
-        mOutputWindow.GetPane(ref generalPaneGuid, out IVsOutputWindowPane pane);
+//      if (null == mOutputPane)
+//      {
+//        Guid generalPaneGuid = mPaneGuid;
+//        mOutputWindow.GetPane(ref generalPaneGuid, out IVsOutputWindowPane pane);
         
-        if ( null == pane)
-        {
-          mOutputWindow.CreatePane(ref generalPaneGuid, OutputWindowConstants.kPaneName, 0, 1);
-          mOutputWindow.GetPane(ref generalPaneGuid, out pane);
-        }
-        mOutputPane = pane;
-      }
-    }
+//        if ( null == pane)
+//        {
+//          mOutputWindow.CreatePane(ref generalPaneGuid, OutputWindowConstants.kPaneName, 0, 1);
+//          mOutputWindow.GetPane(ref generalPaneGuid, out pane);
+//        }
+//        mOutputPane = pane;
+//      }
+//    }
 
-    #endregion
+//    #endregion
 
-    #region Properties
+//    #region Properties
 
-    public override Encoding Encoding => System.Text.Encoding.Default;
+//    public override Encoding Encoding => System.Text.Encoding.Default;
 
-    #endregion
+//    #endregion
 
-    #region Public Methods
+//    #region Public Methods
 
-    public override void Write(string aMessage) => mOutputPane.OutputString($"{aMessage}\n");
+//    public override void Write(string aMessage) => mOutputPane.OutputString($"{aMessage}\n");
 
-    public override void Write(char aCharacter) => mOutputPane.OutputString(aCharacter.ToString());
+//    public override void Write(char aCharacter) => mOutputPane.OutputString(aCharacter.ToString());
 
-    public void Show(DTE2 aDte)
-    {
-      mOutputPane.Activate();
-      aDte.ExecuteCommand("View.Output", string.Empty);
-    }
+//    public void Show(DTE2 aDte)
+//    {
+//      mOutputPane.Activate();
+//      aDte.ExecuteCommand("View.Output", string.Empty);
+//    }
 
-    public void Clear() => mOutputPane.Clear();
+//    public void Clear() => mOutputPane.Clear();
 
-    #endregion
-  }
-}
+//    #endregion
+//  }
+//}
