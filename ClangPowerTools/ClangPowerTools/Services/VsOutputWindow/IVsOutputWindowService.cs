160122f5dd7121ce459cc4bf104eb9f5450cc9bc
Enache Ionut <enachestoianionut@gmail.com>
2018-07-12 17:55:06 +0300

    implement new service wrapper for getting the VS output window

---
+++ ClangPowerTools/ClangPowerTools/Services/VsOutputWindow/IVsOutputWindowService.cs
@@ -0,0 +1,12 @@
+﻿using EnvDTE80;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System.Threading;
+
+namespace ClangPowerTools.Services
+{
+  public interface IVsOutputWindowService
+  {
+    System.Threading.Tasks.Task<IVsOutputWindow> GetOutputWindowAsync(Microsoft.VisualStudio.Shell.IAsyncServiceProvider provider, CancellationToken cancellationToken);
+  }
+}
---
+++ ClangPowerTools/ClangPowerTools/Services/VsOutputWindow/VsOutputWindowService.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace ClangPowerTools.Services
+{
+  public class VsOutputWindowService : SVsOutputWindowService, IVsOutputWindowService, IService
+  {
+    #region Members
+
+    private Microsoft.VisualStudio.Shell.IAsyncServiceProvider mServiceProvider;
+
+    #endregion
+
+
+    #region Constructor 
+
+    public VsOutputWindowService(Microsoft.VisualStudio.Shell.IAsyncServiceProvider aAsyncServiceProvider)
+      => mServiceProvider = aAsyncServiceProvider;
+
+    #endregion
+
+
+    #region IEnvDTEService implementation
+
+    public async Task<IVsOutputWindow> GetOutputWindowAsync(
+      Microsoft.VisualStudio.Shell.IAsyncServiceProvider provider, CancellationToken cancellationToken)
+    {
+      return await provider.GetServiceAsync(typeof(SVsOutputWindow)) as IVsOutputWindow;
+    }
+
+    #endregion
+
+  }
+}
