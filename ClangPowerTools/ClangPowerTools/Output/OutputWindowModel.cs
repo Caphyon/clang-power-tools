ebe617f29d17f7f55120b0fc679af42c41d280b3
Enache Ionut <enachestoianionut@gmail.com>
2018-07-12 17:56:19 +0300

    implement output window model

---
+++ ClangPowerTools/ClangPowerTools/Output/OutputWindowModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.Shell.Interop;
+using System;
+
+namespace ClangPowerTools.Output
+{
+  /// <summary>
+  /// Contains all the necessary VS Output Window elements
+  /// 
+  public class OutputWindowModel
+  {
+    /// <summary>
+    /// Output window instance
+    /// </summary>
+    public IVsOutputWindow VsOutputWindow { get; set; }
+
+    /// <summary>
+    /// The output window pane to display messages
+    /// </summary>
+    public IVsOutputWindowPane Pane { get; set; }
+
+    /// <summary>
+    /// Standard guid for the output window pane 
+    /// </summary>
+    public Guid PaneGuid { get; } = new Guid("AB9F45E4-2001-4197-BAF5-4B165222AF29");
+  }
+}
