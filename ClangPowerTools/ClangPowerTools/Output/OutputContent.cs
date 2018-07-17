37ffc1f72e1d4d6d5efb929124255d67a68afc52
Enache Ionut <enachestoianionut@gmail.com>
2018-07-17 15:03:06 +0300

    implement a output content model

---
+++ ClangPowerTools/ClangPowerTools/Output/OutputContent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClangPowerTools.Output
+{
+  public class OutputContent
+  {
+    #region Properties
+
+    public HashSet<TaskErrorModel> Errors { get; set; } = new HashSet<TaskErrorModel>();
+    public List<string> Buffer { get; set; } = new List<string>();
+    public string Message { get; set; }
+    public bool MissingLLVM { get; set; }
+
+
+    #endregion
+
+  }
+}
