8ba1122bad175ff34d1a86398d7488361bb7a4bb
Enache Ionut <enachestoianionut@gmail.com>
2018-07-26 17:15:54 +0300

    implement OleMenuCommandService wrapper to get the service async

---
+++ ClangPowerTools/ClangPowerTools/Services/OleMenuCommandCustomService/OleMenuCommandServiceImpl.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.Design;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Shell;
+
+namespace ClangPowerTools.Services.OleMenuCommandCustomService
+{
+  class OleMenuCommandServiceImpl : SOleMenuCommandService, IOleMenuCommandService, IService
+  {
+    #region Members
+
+    private IAsyncServiceProvider mServiceProvider;
+
+    #endregion
+
+
+    #region Constructor 
+
+    public OleMenuCommandServiceImpl(IAsyncServiceProvider aAsyncServiceProvider)
+      => mServiceProvider = aAsyncServiceProvider;
+
+    #endregion
+
+
+    #region IEnvDTEService implementation
+
+    public async Task<OleMenuCommandService> GetVsFileChangeAsync(IAsyncServiceProvider provider, CancellationToken cancellationToken)
+    {
+      return await provider.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+    }
+
+    #endregion
+
+  }
+}
