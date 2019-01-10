af15115483a59bfdb102d428bdb8947ac844c07a
Horatiu Prica <horatiu.prica@caphyon.com>
2019-01-10 17:23:39 +0200

    Reformat page

--- ClangPowerTools/ClangPowerTools/Commands/IgnoreCommand.cs
+++ ClangPowerTools/ClangPowerTools/Commands/IgnoreCommand.cs
@@ -16,117 +16,121 @@ using System.Collections.Generic;
 
 namespace ClangPowerTools.Commands
 {
+  /// <summary>
+  /// Command handler
+  /// </summary>
+  internal sealed class IgnoreCommand : BasicCommand
+  {
+
+
+    #region Properties
+
     /// <summary>
-    /// Command handler
+    /// Gets the instance of the command.
     /// </summary>
-    internal sealed class IgnoreCommand : BasicCommand
+    public static IgnoreCommand Instance
     {
-
-
-        #region Properties
-
-        /// <summary>
-        /// Gets the instance of the command.
-        /// </summary>
-        public static IgnoreCommand Instance
-        {
-            get;
-            private set;
-        }
-        #endregion
-
-
-        #region Constructor
-        /// <summary>
-        /// Initializes a new instance of the <see cref="IgnoreCommand"/> class.
-        /// Adds our command handlers for menu (commands must exist in the command table file)
-        /// </summary>
-        /// <param name="package">Owner package, not null.</param>
-        /// <param name="commandService">Command service to add command to, not null.</param>
-        private IgnoreCommand(CommandsController aCommandsController, OleMenuCommandService aCommandService, AsyncPackage aPackage, Guid aGuid, int aId)
-          : base(aPackage, aGuid, aId)
-        {
-            if (null != aCommandService)
-            {
-                var menuCommandID = new CommandID(CommandSet, Id);
-                var menuItem = new OleMenuCommand(aCommandsController.Execute, menuCommandID);
-                aCommandService.AddCommand(menuItem);
-            }
-        }
-
-        #endregion
-
-
-        #region Public Methods
-
-        /// <summary>
-        /// Initializes the singleton instance of the command.
-        /// </summary>
-        /// <param name="package">Owner package, not null.</param>
-        public static async System.Threading.Tasks.Task InitializeAsync(CommandsController aCommandsController,
-          ErrorWindowController aErrorWindow, OutputWindowController aOutputWindow, AsyncPackage aPackage, Guid aGuid, int aId)
-        {
-            // Switch to the main thread - the call to AddCommand in StopClang's constructor requires
-            // the UI thread.
-            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(aPackage.DisposalToken);
-
-            OleMenuCommandService commandService = await aPackage.GetServiceAsync((typeof(IMenuCommandService))) as OleMenuCommandService;
-            Instance = new IgnoreCommand(aCommandsController, commandService, aPackage, aGuid, aId);
-        }
-
-        /// <summary>
-        /// This function is the callback used to execute the command when the menu item is clicked.
-        /// See the constructor to see how the menu item is associated with this function using
-        /// OleMenuCommandService service and MenuCommand class.
-        /// </summary>
-        /// <param name="sender">Event sender.</param>
-        /// <param name="e">Event args.</param>
-        public void RunIgnoreCommand(int aId)
-        {
-            var task = System.Threading.Tasks.Task.Run(() =>
-            {
-                try
-                {
-                    List<string> documentsToIgnore = DocumentsHandler.GetDocumentsToIgnore();
-                    AddIgnoreFilesToSettings(documentsToIgnore);
-
-                }
-                catch (Exception exception)
-                {
-
-                    //throw;
-                }
-            });
-
-
-      
-        }
-
-        #endregion
-
-
-        #region Private Methods
-        /// <summary>
-        /// Create string for the settings page
-        /// </summary>
-        /// <param name="documentsToIgnore"></param>
-        /// <returns></returns>
-        private void AddIgnoreFilesToSettings(List<string> documentsToIgnore)
-        {
-            var settings = SettingsProvider.GetSettingsPage(typeof(ClangFormatOptionsView)) as ClangFormatOptionsView;
-            settings.SkipFiles += ";";
-
-            StringBuilder stringBuilder = new StringBuilder();
-            foreach (var item in documentsToIgnore)
-            {
-                stringBuilder.Append(item).Append(";");
-            }
-
-            settings.SkipFiles += stringBuilder.ToString();
-            settings.SaveSettingsToStorage();
-
-            MessageBox.Show(stringBuilder.ToString());
-        }
-        #endregion
+      get;
+      private set;
     }
+    #endregion
+
+
+    #region Constructor
+    /// <summary>
+    /// Initializes a new instance of the <see cref="IgnoreCommand"/> class.
+    /// Adds our command handlers for menu (commands must exist in the command table file)
+    /// </summary>
+    /// <param name="package">Owner package, not null.</param>
+    /// <param name="commandService">Command service to add command to, not null.</param>
+    private IgnoreCommand(CommandsController aCommandsController, OleMenuCommandService aCommandService, AsyncPackage aPackage, Guid aGuid, int aId)
+      : base(aPackage, aGuid, aId)
+    {
+      if (null != aCommandService)
+      {
+        var menuCommandID = new CommandID(CommandSet, Id);
+        var menuItem = new OleMenuCommand(aCommandsController.Execute, menuCommandID);
+        aCommandService.AddCommand(menuItem);
+      }
+    }
+
+    #endregion
+
+
+    #region Public Methods
+
+    /// <summary>
+    /// Initializes the singleton instance of the command.
+    /// </summary>
+    /// <param name="package">Owner package, not null.</param>
+    public static async System.Threading.Tasks.Task InitializeAsync(CommandsController aCommandsController,
+      ErrorWindowController aErrorWindow, OutputWindowController aOutputWindow, AsyncPackage aPackage, Guid aGuid, int aId)
+    {
+      // Switch to the main thread - the call to AddCommand in StopClang's constructor requires
+      // the UI thread.
+      await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(aPackage.DisposalToken);
+
+      OleMenuCommandService commandService = await aPackage.GetServiceAsync((typeof(IMenuCommandService))) as OleMenuCommandService;
+      Instance = new IgnoreCommand(aCommandsController, commandService, aPackage, aGuid, aId);
+    }
+
+    /// <summary>
+    /// This function is the callback used to execute the command when the menu item is clicked.
+    /// See the constructor to see how the menu item is associated with this function using
+    /// OleMenuCommandService service and MenuCommand class.
+    /// </summary>
+    /// <param name="sender">Event sender.</param>
+    /// <param name="e">Event args.</param>
+    public void RunIgnoreCommand(int aId)
+    {
+      var task = System.Threading.Tasks.Task.Run(() =>
+      {
+        try
+        {
+          List<string> documentsToIgnore = DocumentsHandler.GetDocumentsToIgnore();
+          AddIgnoreFilesToSettings(documentsToIgnore);
+
+        }
+        catch (Exception exception)
+        {
+
+                //throw;
+              }
+      });
+
+
+
+    }
+
+    #endregion
+
+
+    #region Private Methods
+    /// <summary>
+    /// Create string for the settings page
+    /// </summary>
+    /// <param name="documentsToIgnore"></param>
+    /// <returns></returns>
+    private void AddIgnoreFilesToSettings(List<string> documentsToIgnore)
+    {
+      var settings = SettingsProvider.GetSettingsPage(typeof(ClangFormatOptionsView)) as ClangFormatOptionsView;
+      settings.SkipFiles += ";";
+
+      StringBuilder stringBuilder = new StringBuilder();
+      foreach (var item in documentsToIgnore)
+      {
+        stringBuilder.Append(item).Append(";");
+      }
+
+      settings.SkipFiles += stringBuilder.ToString();
+      settings.SaveSettingsToStorage();
+
+      MessageBox.Show(stringBuilder.ToString());
+    }
+    #endregion
+
+
+    #region Private Methods
+    #endregion
+  }
 }
