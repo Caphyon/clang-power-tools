d43051a9c18351ea0353150377c4b9176b8b8cac
Enache Ionut <enachestoianionut@gmail.com>
2019-01-17 11:38:23 +0200

    remove the delimitator for the default values in option page

--- ClangPowerTools/ClangPowerTools/Options/Model/DefaultOptions.cs
+++ ClangPowerTools/ClangPowerTools/Options/Model/DefaultOptions.cs
@@ -4,7 +4,7 @@
   {
     public const string kClangFlags = "-Wall;-fms-compatibility-version=19.10;-Wmicrosoft;-Wno-invalid-token-paste;-Wno-unknown-pragmas;-Wno-unused-value";
     public const string kHeaderFilter = ".*";
-    public const string kFileExtensions = ".c;.cpp;.cxx;.cc;.tli;.tlh;.h;.hh;.hpp;.hxx;.hh;";
-    public const string kSkipFiles = "resource.h;";
+    public const string kFileExtensions = ".c;.cpp;.cxx;.cc;.tli;.tlh;.h;.hh;.hpp;.hxx;.hh";
+    public const string kSkipFiles = "resource.h";
   }
 }
