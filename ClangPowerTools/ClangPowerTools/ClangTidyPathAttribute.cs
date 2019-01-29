300127c1016b13adb546162c050945545cdc135d
Horatiu Prica <horatiu.prica@caphyon.com>
2019-01-29 14:37:03 +0200

    Add ClangTidyPathAttribute and BorderedTextBoxBrowseTidy

---
+++ ClangPowerTools/ClangPowerTools/ClangTidyPathAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClangPowerTools
+{
+  [AttributeUsage(AttributeTargets.Property)]
+  public class ClangTidyPathAttribute : Attribute
+  {
+    public bool Activate { get; set; }
+
+    public ClangTidyPathAttribute(bool aActivate)
+    {
+      Activate = aActivate;
+    }
+  }
+}
