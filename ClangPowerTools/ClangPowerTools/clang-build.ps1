0abdcf0fdbbc3ac6abc8740a0f2b10ea09fb790b
Gabriel Diaconita <gabriel.diaconita@caphyon.com>
2019-01-31 11:51:26 +0200

    Support custom clang-tidy builds using the CLANG_TIDY_PATH environment variable

--- ClangPowerTools/ClangPowerTools/clang-build.ps1
+++ ClangPowerTools/ClangPowerTools/clang-build.ps1
@@ -203,6 +203,12 @@ Set-Variable -name kExtensionSolution        -value ".sln"              -option 
 Set-Variable -name kExtensionClangPch        -value ".clang.pch"        -option Constant
 Set-Variable -name kExtensionC               -value ".c"                -option Constant
 
+
+# ------------------------------------------------------------------------------------------------
+# Envinroment Variables for controlling logic
+
+Set-Variable -name kVarEnvClangTidyPath     -value "env:CLANG_TIDY_PATH"-option Constant
+
 # ------------------------------------------------------------------------------------------------
 # Clang-Related Constants
 
@@ -220,8 +226,20 @@ Set-Variable -name kClangFlagFileIsCPP      -value "-x c++"             -option 
 Set-Variable -name kClangFlagFileIsC        -value "-x c"               -option Constant
 Set-Variable -name kClangFlagForceInclude   -value "-include"           -option Constant
 
-Set-Variable -name kClangCompiler             -value "clang++.exe"      -option Constant
-Set-Variable -name kClangTidy                 -value "clang-tidy.exe"   -option Constant
+Set-Variable -name kClangCompiler           -value "clang++.exe"        -option Constant
+
+# we may have a custom path for Clang-Tidy. Use it if that's the case.
+if (Test-Path $kVarEnvClangTidyPath)
+{
+  [string] $customTidyPath = (Get-Item $kVarEnvClangTidyPath).Value
+  Set-Variable -name kClangTidy             -value $customTidyPath      -option Constant
+}
+else
+{
+  Set-Variable -name kClangTidy             -value "clang-tidy.exe"     -option Constant
+}
+
+
 Set-Variable -name kClangTidyFlags            -value @("-quiet"
                                                       ,"--")            -option Constant
 Set-Variable -name kClangTidyFixFlags         -value @("-quiet"
