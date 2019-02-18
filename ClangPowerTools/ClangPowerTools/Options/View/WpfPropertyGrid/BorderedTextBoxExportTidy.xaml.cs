4c9375654c44b92a2bd89a96fc5afd6d9f81878c
Horatiu Prica <horatiu.prica@caphyon.com>
2019-02-18 15:47:07 +0200

    Export config button

---
+++ ClangPowerTools/ClangPowerTools/Options/View/WpfPropertyGrid/BorderedTextBoxExportTidy.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Documents;
+
+namespace ClangPowerTools.Options.View.WpfPropertyGrid
+{
+  /// <summary>
+  /// Interaction logic for BorderedTextBoxExportTidy.xaml
+  /// </summary>
+  public partial class BorderedTextBoxExportTidy : TextBoxNotifaiableUserControl
+  {
+    public BorderedTextBoxExportTidy()
+    {
+      InitializeComponent();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+  }
+}
