ccd731b0abf7cdf41dce6ce952825a8dcb8c604b
Horatiu Prica <horatiu.prica@caphyon.com>
2019-02-19 16:31:34 +0200

    Export button functionality

--- ClangPowerTools/ClangPowerTools/Options/View/WpfPropertyGrid/BorderedTextBoxExportTidy.xaml.cs
+++ ClangPowerTools/ClangPowerTools/Options/View/WpfPropertyGrid/BorderedTextBoxExportTidy.xaml.cs
@@ -16,7 +16,20 @@ namespace ClangPowerTools.Options.View.WpfPropertyGrid
 
     private void Button_Click(object sender, RoutedEventArgs e)
     {
+      // Create dialog window
+      Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
 
+      // Set the default file extension
+      dlg.DefaultExt = ".clang-tidy";
+      dlg.Filter = "clang-tidy";
+
+      //Display the dialog window
+      bool? result = dlg.ShowDialog();
+
+      if(result == true)
+      {
+        string filename = dlg.FileName;
+      }
     }
   }
 }
