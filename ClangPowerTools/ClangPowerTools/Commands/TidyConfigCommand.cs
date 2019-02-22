4a3ae0009625657df7b1ede5e88431fd42fd5529
Horatiu Prica <horatiu.prica@caphyon.com>
2019-02-22 14:06:04 +0200

    New TidyConfigCommand

---
+++ ClangPowerTools/ClangPowerTools/Commands/TidyConfigCommand.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Task = System.Threading.Tasks.Task;
+
+namespace ClangPowerTools.Commands
+{
+  /// <summary>
+  /// Command handler
+  /// </summary>
+  internal sealed class TidyConfigCommand
+  {
+    /// <summary>
+    /// Command ID.
+    /// </summary>
+    public const int CommandId = 256;
+
+    /// <summary>
+    /// Command menu group (command set GUID).
+    /// </summary>
+    public static readonly Guid CommandSet = new Guid("a95ae60d-773b-4090-833f-745d0f846100");
+
+    /// <summary>
+    /// VS Package that provides this command, not null.
+    /// </summary>
+    private readonly AsyncPackage package;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="TidyConfigCommand"/> class.
+    /// Adds our command handlers for menu (commands must exist in the command table file)
+    /// </summary>
+    /// <param name="package">Owner package, not null.</param>
+    /// <param name="commandService">Command service to add command to, not null.</param>
+    private TidyConfigCommand(AsyncPackage package, OleMenuCommandService commandService)
+    {
+      this.package = package ?? throw new ArgumentNullException(nameof(package));
+      commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+      var menuCommandID = new CommandID(CommandSet, CommandId);
+      var menuItem = new MenuCommand(this.Execute, menuCommandID);
+      commandService.AddCommand(menuItem);
+    }
+
+    /// <summary>
+    /// Gets the instance of the command.
+    /// </summary>
+    public static TidyConfigCommand Instance
+    {
+      get;
+      private set;
+    }
+
+    /// <summary>
+    /// Gets the service provider from the owner package.
+    /// </summary>
+    private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+    {
+      get
+      {
+        return this.package;
+      }
+    }
+
+    /// <summary>
+    /// Initializes the singleton instance of the command.
+    /// </summary>
+    /// <param name="package">Owner package, not null.</param>
+    public static async Task InitializeAsync(AsyncPackage package)
+    {
+      // Switch to the main thread - the call to AddCommand in TidyConfigCommand's constructor requires
+      // the UI thread.
+      await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+      OleMenuCommandService commandService = await package.GetServiceAsync((typeof(IMenuCommandService))) as OleMenuCommandService;
+      Instance = new TidyConfigCommand(package, commandService);
+    }
+
+    /// <summary>
+    /// This function is the callback used to execute the command when the menu item is clicked.
+    /// See the constructor to see how the menu item is associated with this function using
+    /// OleMenuCommandService service and MenuCommand class.
+    /// </summary>
+    /// <param name="sender">Event sender.</param>
+    /// <param name="e">Event args.</param>
+    private void Execute(object sender, EventArgs e)
+    {
+      // Create dialog window
+      Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+
+      // Set the default file extension
+      dlg.DefaultExt = ".clang-tidy";
+      dlg.Filter = "Configuration files (.clang-tidy)|*.clang-tidy";
+
+      //Display the dialog window
+      bool? result = dlg.ShowDialog();
+
+      if (result == true)
+      {
+        string filename = dlg.FileName;
+      }
+    }
+  }
+}
