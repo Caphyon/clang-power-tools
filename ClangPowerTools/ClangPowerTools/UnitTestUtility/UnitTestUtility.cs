11225b9009b14aab9ee7813632ecc3948f66a044
Horatiu Prica <horatiu.prica@caphyon.com>
2019-04-01 18:20:20 +0300

    Integrate the VsixTesting plugin

---
+++ ClangPowerTools/ClangPowerTools/UnitTestUtility/UnitTestUtility.cs
@@ -0,0 +1,16 @@
+﻿using EnvDTE;
+using Microsoft;
+using Microsoft.VisualStudio.Shell;
+
+namespace ClangPowerTools.Tests
+{
+  public class UnitTestUtility
+  {
+    public static string GetVsVersion()
+    {
+      ThreadHelper.ThrowIfNotOnUIThread();
+      var dte = (DTE)ServiceProvider.GlobalProvider.GetService(typeof(DTE));
+      return dte.Version;
+    }
+  }
+}
---
+++ ClangPowerTools/ClangPowerToolsUnitTests/VSVersionTests.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+
+namespace ClangPowerTools.Tests
+{
+  [VsTestSettings(UIThread = true)]
+  public class VSVersionTests
+  {
+    [VsFact(Version = "2012")]
+    void TestMethod11()
+    {
+      Assert.Equal("11.0", UnitTestUtility.GetVsVersion());
+    }
+
+    [VsFact(Version = "2013")]
+    void TestMethod12()
+    {
+      //  Assert.Equal("12.0", DteUtilities.GetVsVersion());
+    }
+
+    [VsFact(Version = "2015")]
+    void TestMethod14()
+    {
+      //Assert.Equal("14.0", DteUtilities.GetVsVersion());
+    }
+
+    [VsFact(Version = "2017")]
+    void TestMethod15()
+    {
+      Assert.Equal("15.0", UnitTestUtility.GetVsVersion());
+    }
+  }
+}
