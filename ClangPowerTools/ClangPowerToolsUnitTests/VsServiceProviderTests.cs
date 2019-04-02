d17e4e1dca180c8cab060ad63e790eab245717c9
Enache Ionut <ionut.enache@caphyon.com>
2019-04-02 17:39:13 +0300

    try do do something good

---
+++ ClangPowerTools/ClangPowerToolsUnitTests/VsServiceProviderTests.cs
@@ -0,0 +1,36 @@
+﻿using ClangPowerTools.Services;
+using EnvDTE;
+using EnvDTE80;
+using Xunit;
+using ClangPowerTools;
+using Microsoft.VisualStudio.Shell;
+
+namespace ClangPowerTools.Tests
+{
+  [VsTestSettings(UIThread = true)]
+  public class VsServiceProviderTests
+  {
+    [VsFact()]
+    public void DteServiceWasRegistered_Test()
+    {
+      ThreadHelper.ThrowIfNotOnUIThread();
+
+      System.Threading.Thread.Sleep(5000);
+
+      DTE dte = VsServiceProvider.GetService(typeof(DTE)) as DTE;
+      Assert.NotNull(dte);
+    }
+
+
+    //[VsFact()]
+    //public void DTEServiceIsConvertibleToDTE2_Test()
+    //{
+    //  DTE2 dte2 = null;
+    //  if (VsServiceProvider.TryGetService(typeof(DTE), out object dteObj))
+    //  {
+    //    dte2 = dteObj as DTE2;
+    //  }
+    //  Assert.NotNull(dte2);
+    //}
+  }
+}
