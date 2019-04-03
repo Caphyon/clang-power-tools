96b39f22f86e9ceee84b5db513a84ffa7b60da81
Horatiu Prica <horatiu.prica@caphyon.com>
2019-04-03 15:35:04 +0300

    Get command with GlobalProvider

--- ClangPowerTools/ClangPowerToolsUnitTests/VSPackageTests.cs
+++ ClangPowerTools/ClangPowerToolsUnitTests/VSPackageTests.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft;
+﻿using EnvDTE;
+using EnvDTE80;
+using Microsoft;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
 using System;
@@ -9,13 +11,22 @@ namespace ClangPowerTools.Tests
 {
   public class AsyncPackageTests
   {
-    [VsTheory(Version = "2015-")]
+    [VsTheory(Version = "2019")]
     [InlineData(RunClangPowerToolsPackage.PackageGuidString, true)]
     [InlineData("11111111-2222-3333-4444-555555555555", false)]
     async Task LoadTestAsync(string guidString, bool expectedSuccess)
     {
       await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
       var shell = (IVsShell7)ServiceProvider.GlobalProvider.GetService(typeof(SVsShell));
+      var dte = (DTE2)ServiceProvider.GlobalProvider.GetService(typeof(DTE));
+
+      var commands = dte.Commands as Commands2;
+
+      Command cmd;
+      if (null != commands)
+        cmd = commands.Item("498fdff5-5217-4da9-88d2-edad44ba3874", 0x0102);
+
+
       Assumes.Present(shell);
 
       var guid = Guid.Parse(guidString);
