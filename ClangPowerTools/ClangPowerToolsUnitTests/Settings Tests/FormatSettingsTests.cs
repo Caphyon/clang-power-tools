9d07b4370bf7cdcd11c316755164a07c66121413
Horatiu Prica <horatiu.prica@caphyon.com>
2019-04-17 18:05:00 +0300

    Rename test class

--- ClangPowerTools/ClangPowerToolsUnitTests/Settings Tests/FormatSettingsTests.cs
+++ ClangPowerTools/ClangPowerToolsUnitTests/Settings Tests/FormatSettingsTests.cs
@@ -4,7 +4,7 @@ using Task = System.Threading.Tasks.Task;
 
 namespace ClangPowerTools.Tests.Settings
 {
-  public class ClangFormatSettingsTests
+  public class FormatSettingsTests
   {
     [VsFact(Version = "2019")]
     public async Task ClangFormatOptionsView_NotNullAsync()
