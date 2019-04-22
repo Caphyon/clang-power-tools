d898e3aeaa5a9830aec79345b9468e09f1ce7ef8
Enache Ionut <ionut.enache@caphyon.com>
2019-04-22 12:29:19 +0300

    remove the version hash keeper from the basic clang command class

--- ClangPowerTools/ClangPowerTools/Commands/BasicCommand.cs
+++ ClangPowerTools/ClangPowerTools/Commands/BasicCommand.cs
@@ -6,19 +6,6 @@ namespace ClangPowerTools
 {
   public abstract class BasicCommand
   {
-    #region Members
-
-    private readonly Dictionary<string, string> mVsVersions = new Dictionary<string, string>
-    {
-      {"11.0", "2010"},
-      {"12.0", "2012"},
-      {"13.0", "2013"},
-      {"14.0", "2015"},
-      {"15.0", "2017"}
-    };
-
-    #endregion
-
     #region Properties
 
     protected int Id { get; set; }
