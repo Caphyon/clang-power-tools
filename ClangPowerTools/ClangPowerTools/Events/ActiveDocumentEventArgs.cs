2603a94631d8fa38c72d32edec40a50a5169e4eb
Horatiu Prica <horatiu.prica@caphyon.com>
2019-05-09 15:17:20 +0300

    New ActiveDoumentEventArgs

---
+++ ClangPowerTools/ClangPowerTools/Events/ActiveDocumentEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClangPowerTools.Events
+{
+  public class ActiveDocumentEventArgs
+  {
+    public bool IsActiveDocument { get; set; }
+
+    public ActiveDocumentEventArgs(bool isActiveDocument)
+    {
+      IsActiveDocument = isActiveDocument;
+    }
+  }
+}
