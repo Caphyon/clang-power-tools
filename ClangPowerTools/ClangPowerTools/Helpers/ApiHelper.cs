c2a591c369219f426cee691208bd5dbe7a3c8b5a
Horatiu Prica <horatiu.prica@caphyon.com>
2019-06-14 11:50:51 +0300

    Add ApiHelper

---
+++ ClangPowerTools/ClangPowerTools/Helpers/ApiHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace ClangPowerTools
+{
+  public static class ApiHelper
+  {
+    public static HttpClient ApiClient { get; set; }
+
+    public static void InitializeApiClient()
+    {
+      ApiClient = new HttpClient();
+      ApiClient.DefaultRequestHeaders.Accept.Clear();
+      ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+    }
+  }
+}
