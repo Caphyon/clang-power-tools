08e5b6d40c373cb3f2c631bba9032ce0645f24fd
Horatiu Prica <horatiu.prica@caphyon.com>
2019-06-14 17:59:12 +0300

    Add NetworkUtility

---
+++ ClangPowerTools/ClangPowerTools/Helpers/NetworkUtility.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ClangPowerTools.Account
+{
+  public class NetworkUtility
+  {
+    public static async Task CheckInternetConnectionAsync()
+    {
+      try
+      {
+        using (HttpResponseMessage result = await ApiHelper.ApiClient.GetAsync("http://www.google.com"))
+        {
+
+        }
+      }
+      catch (Exception)
+      {
+        throw;
+      }
+    }
+  }
+}
