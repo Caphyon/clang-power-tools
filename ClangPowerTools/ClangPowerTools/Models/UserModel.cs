d8c693d7be3522538fac65816f42c4eee0eaace8
Horatiu Prica <horatiu.prica@caphyon.com>
2019-06-14 16:39:01 +0300

    New constructor for UserModel

--- ClangPowerTools/ClangPowerTools/Models/UserModel.cs
+++ ClangPowerTools/ClangPowerTools/Models/UserModel.cs
@@ -4,10 +4,15 @@
   {
     public string email { get; set; }
     public string password { get; set; }
+    public bool IsActive { get; set; } = false;
     public UserModel(string email, string password)
     {
       this.email = email;
       this.password = password;
+    }
+    public UserModel()
+    {
+
     }
   }
 }
