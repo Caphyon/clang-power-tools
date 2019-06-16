ad1e3299adc78301b326e33863d7189ed48638c0
Enache-Ionut <enachestoianionut@gmail.com>
2019-06-16 11:02:15 +0300

    implement INotifyPropertyChanged interface for the LogIn ViewModel

--- ClangPowerTools/ClangPowerTools/MVVM/ViewModels/LoginViewModel.cs
+++ ClangPowerTools/ClangPowerTools/MVVM/ViewModels/LoginViewModel.cs
@@ -1,14 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ClangPowerTools
 {
-  public class LoginViewModel
+  public class LoginViewModel : INotifyPropertyChanged
   {
+    #region Members
 
+    private string email;
+    private string password;
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    #endregion
+
+    #region Properties
+
+    public string Email
+    {
+      get { return email; }
+      set
+      {
+        email = value;
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Email"));
+      }
+    }
+
+    public string Password
+    {
+      get { return password; }
+      set
+      {
+        password = value;
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Password"));
+      }
+    }
+
+    #endregion
 
   }
 }
