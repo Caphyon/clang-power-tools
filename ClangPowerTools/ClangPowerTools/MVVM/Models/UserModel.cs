26e4d58f88b870507b40103ecce0763dfb72a7f8
Enache-Ionut <enachestoianionut@gmail.com>
2019-06-16 11:00:57 +0300

    add regions for UserModel class

--- ClangPowerTools/ClangPowerTools/MVVM/Models/UserModel.cs
+++ ClangPowerTools/ClangPowerTools/MVVM/Models/UserModel.cs
@@ -2,17 +2,27 @@
 {
   public class UserModel
   {
+    #region Properties
+
     public string email { get; set; }
     public string password { get; set; }
     public bool IsActive { get; set; } = false;
+
+    #endregion
+
+    #region Constructor
+
     public UserModel(string email, string password)
     {
       this.email = email;
       this.password = password;
     }
+
     public UserModel()
     {
-
     }
+
+    #endregion
+
   }
 }
