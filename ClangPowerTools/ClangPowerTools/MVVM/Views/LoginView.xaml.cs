2dfd682f2e34f7681fcedbf8f1131eb68293ae27
Enache Ionut <ionut.enache@caphyon.com>
2019-06-20 10:53:35 +0300

    add placeholders for email and password and hover efect for buttons

--- ClangPowerTools/ClangPowerTools/MVVM/Views/LoginView.xaml.cs
+++ ClangPowerTools/ClangPowerTools/MVVM/Views/LoginView.xaml.cs
@@ -15,12 +15,10 @@ namespace ClangPowerTools.Views
 
     private void ForgotPasswordButton_Click(object sender, RoutedEventArgs e)
     {
-      Process.Start(new ProcessStartInfo("https://api.clangpowertools.com/api/5d011c6a375f6b5ed9716629/user/forgot-password"));
     }
 
     private void SingUpButton_Click(object sender, RoutedEventArgs e)
     {
-      Process.Start(new ProcessStartInfo("https://api.clangpowertools.com/api/5d011c6a375f6b5ed9716629/user/register"));
     }
 
     private void OnPasswordChanged(object sender, RoutedEventArgs e)
