8caa14e84fff8dbfb597f62a8d03bfb4bfae35e3
Horatiu Prica <horatiu.prica@caphyon.com>
2019-06-20 14:34:08 +0300

    Move WebApiUrls into a static class

---
+++ ClangPowerTools/ClangPowerTools/MVVM/WebApi/WebApiUrl.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClangPowerTools.MVVM.WebApi
+{
+  public static class WebApiUrl
+  {
+    private static readonly string appId = "5d011c6a375f6b5ed9716629";
+    private static readonly string url = @"https://account.clangpowertools.com";
+
+    public static readonly string loginUrl = string.Concat(url, "/api/", appId, "/user/", "login");
+    public static readonly string licenseUrl = string.Concat(url, "/api/", appId, "/license");
+    public static readonly string forgotPasswordUrl = string.Concat(url, "/api/", appId, "/user/", "forgot-password");
+    public static readonly string signUpUrl = string.Concat(url, "/api/", appId, "/user/", "register");
+  }
+}
