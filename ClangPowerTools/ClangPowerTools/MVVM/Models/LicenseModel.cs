668db8303f377bd21c0ea0d15db73ce64517a3c0
Horatiu Prica <horatiu.prica@caphyon.com>
2019-06-24 17:27:56 +0300

    Add the License Model

---
+++ ClangPowerTools/ClangPowerTools/MVVM/Models/LicenseModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClangPowerTools.MVVM.Models
+{
+  public class LicenseModel
+  {
+    public string _id { get; set; } = "";
+  }
+}
