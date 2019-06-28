5438a7e02ff47dd560260403750697bdf518bfd9
Horatiu Prica <horatiu.prica@caphyon.com>
2019-06-28 14:34:22 +0300

    Remove unnecessary method call

--- ClangPowerTools/ClangPowerTools/MVVM/Controllers/AccountController.cs
+++ ClangPowerTools/ClangPowerTools/MVVM/Controllers/AccountController.cs
@@ -25,7 +25,6 @@ namespace ClangPowerTools.MVVM.Controllers
     public async Task<bool> LoginAsync(UserModel userModel)
     {
       StringContent content = new StringContent(SeralizeUserModel(userModel), Encoding.UTF8, "application/json");
-      userModel.Dispose();
 
       try
       {
