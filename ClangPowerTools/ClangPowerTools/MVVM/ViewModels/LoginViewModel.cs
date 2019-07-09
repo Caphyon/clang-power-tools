929e3be8b5e6750ae2d08a60dc83aafd0fe5d370
Enache-Ionut <enachestoianionut@gmail.com>
2019-07-09 23:34:29 +0300

    apply red border for email validation

--- ClangPowerTools/ClangPowerTools/MVVM/ViewModels/LoginViewModel.cs
+++ ClangPowerTools/ClangPowerTools/MVVM/ViewModels/LoginViewModel.cs
@@ -1,8 +1,9 @@
 ﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace ClangPowerTools
 {
-  public class LoginViewModel : INotifyPropertyChanged
+  public class LoginViewModel : INotifyPropertyChanged, IDataErrorInfo
   {
     #region Members
 
@@ -31,7 +32,54 @@ namespace ClangPowerTools
         userModel.password = value;
       }
     }
+
+    public bool InvalidInput { get; set; } = false;
+
     #endregion
+
+    #region IDataErrorInfo Interface
+
+    public string Error => null;
+
+    public string this[string name]
+    {
+      get
+      {
+        string result = null;
+
+        switch(name)
+        {
+          case "Email":
+            InvalidInput = IsEmailAddressValid(out string errorMessage);
+            result = errorMessage;
+
+            break;
+        }
+        return result;
+      }
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    private bool IsEmailAddressValid(out string errorMessage )
+    {
+      errorMessage = null;
+      var validEmailAddress = new EmailAddressAttribute().IsValid(Email);
+
+      if (validEmailAddress == false)
+      {
+        errorMessage = "Email address is required";
+        return false;
+      }
+
+      return true;
+    }
+
+    #endregion
+
+
 
   }
 }
