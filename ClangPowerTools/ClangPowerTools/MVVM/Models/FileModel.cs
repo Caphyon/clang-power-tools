bb7f4c875897901fb35753763cfb5ad5e7419a91
Rodica Vasilescu <vasilescu.rodica@yahoo.com>
2019-07-17 00:22:34 +0300

    change the convert button and CheckBoxList visibility

--- ClangPowerTools/ClangPowerTools/MVVM/Models/FileModel.cs
+++ ClangPowerTools/ClangPowerTools/MVVM/Models/FileModel.cs
@@ -7,22 +7,9 @@ using System.Threading.Tasks;
 
 namespace ClangPowerTools.MVVM.Models
 {
-  class FileModel: INotifyPropertyChanged
+  class FileModel
   {
-    private bool _isChecked;
     public string FileName { get; set; }
-    public bool IsChecked
-    {
-      get { return _isChecked; }
-      set
-      {
-        if (_isChecked == value) return;
-        _isChecked = value;
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsChecked"));
-      }
-    }
-
-
-    public event PropertyChangedEventHandler PropertyChanged;
+    public bool IsChecked{ get; set; }
   }
 }
