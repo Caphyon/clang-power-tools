d976810ec4f29c0d678f17c2e475f5d80eb03116
rodicamihaela <vasilescu.rodica@yahoo.com>
2019-07-16 17:47:12 +0300

    modify relayCommand

--- ClangPowerTools/ClangPowerTools/MVVM/Command/RelayCommand.cs
+++ ClangPowerTools/ClangPowerTools/MVVM/Command/RelayCommand.cs
@@ -7,32 +7,29 @@ using System.Windows.Input;
 
 namespace ClangPowerTools.MVVM.Command
 {
-  // https://www.c-sharpcorner.com/UploadFile/20c06b/icommand-and-relaycommand-in-wpf/
   public class RelayCommand : ICommand
   {
-    private Action<object> execute;
-    private Func<object, bool> canExecute;
-
     public event EventHandler CanExecuteChanged
     {
       add { CommandManager.RequerySuggested += value; }
       remove { CommandManager.RequerySuggested -= value; }
     }
 
-    public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
-    {
-      this.execute = execute;
-      this.canExecute = canExecute;
-    }
+    private readonly Action _action;
 
+    private Func<bool> _canExecute;
+    public RelayCommand(Action action, Func<bool> canExecute = null)
+    {
+      _action = action;
+      _canExecute = canExecute;
+    }
     public bool CanExecute(object parameter)
     {
-      return this.canExecute == null || this.canExecute(parameter);
+      return _canExecute == null ? true : _canExecute.Invoke();
     }
-
     public void Execute(object parameter)
     {
-      this.execute(parameter);
+      _action.Invoke();
     }
   }
 }
