daedb48bb945fc51f5b7205cc75aa06710a7ed15
rodicamihaela <vasilescu.rodica@yahoo.com>
2019-07-16 18:30:30 +0300

    add fileModel

---
+++ ClangPowerTools/ClangPowerTools/MVVM/Models/FileModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClangPowerTools.MVVM.Models
+{
+  class FileModel
+  {
+    public string FileName { get; set; }
+    public bool IsChecked { get; set; } = true;
+  }
+}
