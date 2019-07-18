6ebf05d85860983f57e85ace7d88449f64107d76
Horatiu Prica <horatiu.prica@caphyon.com>
2019-07-18 17:23:54 +0300

    GeneralOptions model and view

---
+++ ClangPowerTools/ClangPowerTools/MVVM/Models/GeneralOptionsModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClangPowerTools.MVVM.Models
+{
+  public class GeneralOptionsModel : INotifyPropertyChanged
+  {
+    public string CompileFlags { get; set; }
+    public string FilesToIgnore { get; set; }
+
+    public string ProjectToIgnore { get; set; }
+
+    public ClangGeneralAdditionalIncludes AdditionalIncludes { get; set; }
+
+    public bool TreatWarningsAsErrors { get; set; }
+
+    public bool ContinueOnError { get; set; }
+
+    public bool ClangCompileAfterMSCVCompile { get; set; }
+
+    public bool VerboseMode { get; set; }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+  }
+}
