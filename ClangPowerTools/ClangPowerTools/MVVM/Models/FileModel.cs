41a945bedf5e2a4de90e910a005892b7948c9424
Rodica Vasilescu <vasilescu.rodica@yahoo.com>
2019-07-21 19:53:55 +0300

    remove comments and unused namespaces, add regions

--- ClangPowerTools/ClangPowerTools/MVVM/Models/FileModel.cs
+++ ClangPowerTools/ClangPowerTools/MVVM/Models/FileModel.cs
@@ -1,19 +1,9 @@
-﻿using ClangPowerTools.MVVM.Utils;
-using ClangPowerTools.Properties;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 
 namespace ClangPowerTools.MVVM.Models
 {
   class FileModel : INotifyPropertyChanged
   {
-    private bool isChecked;
-
-    //public static int numberOfCheckedFiles { get; set; } = 0;
     public string FileName { get; set; }
     public bool IsChecked
     {
@@ -22,19 +12,12 @@ namespace ClangPowerTools.MVVM.Models
       {
         if (isChecked == value) { return; }
         isChecked = value;
-        //if (value)
-        //{
-        //  numberOfCheckedFiles++;
-        //}
-        //else
-        //{
-        //  numberOfCheckedFiles--;
-        //}
-        //EventBus.Notify(numberOfCheckedFiles == 0 ? "DisableConvertButtonEvent" : "EnableConvertButtonEvent");
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsChecked"));
       }
     }
 
     public event PropertyChangedEventHandler PropertyChanged;
+
+    private bool isChecked;
   }
 }
