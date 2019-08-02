7a4700be2ccbead0344adda0e18d08578589ffd7
rodicamihaela <vasilescu.rodica@yahoo.com>
2019-08-02 16:33:15 +0300

    add cpp project check

--- ClangPowerTools/ClangPowerTools/Commands/CompileCommand.cs
+++ ClangPowerTools/ClangPowerTools/Commands/CompileCommand.cs
@@ -26,7 +26,7 @@ namespace ClangPowerTools.Commands
 
     public bool VsCompileFlag { get; set; }
 
-
+    public OleMenuCommand menuCommand { get; set; }
     #endregion
 
 
@@ -44,7 +44,7 @@ namespace ClangPowerTools.Commands
       if (null != aCommandService)
       {
         var menuCommandID = new CommandID(CommandSet, Id);
-        var menuCommand = new OleMenuCommand(aCommandController.Execute, menuCommandID);
+        menuCommand = new OleMenuCommand(aCommandController.Execute, menuCommandID);
         menuCommand.BeforeQueryStatus += aCommandController.OnBeforeClangCommand;
         menuCommand.Enabled = true;
         aCommandService.AddCommand(menuCommand);
