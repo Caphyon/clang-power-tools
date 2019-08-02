e03b4f1af3393ff0ec30782a02842d5f2683aade
rodicamihaela <vasilescu.rodica@yahoo.com>
2019-08-02 10:04:29 +0300

    select only project or files, not both of them

--- ClangPowerTools/ClangPowerTools/Commands/IgnoreCompileCommand.cs
+++ ClangPowerTools/ClangPowerTools/Commands/IgnoreCompileCommand.cs
@@ -1,9 +1,9 @@
 ﻿using Microsoft.VisualStudio.Shell;
 using System;
-using System.ComponentModel.Design;
 using System.Collections.Generic;
-using Task = System.Threading.Tasks.Task;
+using System.ComponentModel.Design;
 using System.Linq;
+using Task = System.Threading.Tasks.Task;
 
 namespace ClangPowerTools.Commands
 {
@@ -74,10 +74,13 @@ namespace ClangPowerTools.Commands
     {
       var task = Task.Run(() =>
       {
-        List<string> filesToIgnore = ItemsCollector.GetFilesToIgnore();
-        AddIgnoreFilesToSettings(filesToIgnore);
         List<string> projectsToIgnore = ItemsCollector.GetProjectsToIgnore();
         AddIgnoreProjectsToSettings(projectsToIgnore);
+        if (!projectsToIgnore.Any())
+        {
+          List<string> filesToIgnore = ItemsCollector.GetFilesToIgnore();
+          AddIgnoreFilesToSettings(filesToIgnore);
+        }
       });
     }
 
@@ -110,7 +113,7 @@ namespace ClangPowerTools.Commands
 
     public void AddIgnoreProjectsToSettings(List<string> documentsToIgnore)
     {
-      if(!documentsToIgnore.Any())
+      if (!documentsToIgnore.Any())
       {
         return;
       }
