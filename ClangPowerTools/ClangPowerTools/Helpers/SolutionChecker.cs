90f923ab7f132d91537c3704e717b7b27575c43a
Enache Ionut <ionut.enache@caphyon.com>
2019-08-08 17:51:08 +0300

    extract info about solution state and open folder mode

---
+++ ClangPowerTools/ClangPowerTools/Helpers/SolutionChecker.cs
@@ -0,0 +1,50 @@
+﻿using ClangPowerTools.Services;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace ClangPowerTools.Helpers
+{
+  public class SolutionChecker
+  {
+    #region Properties
+
+    public bool SolutionOpen { get; set; }
+
+    public bool OpenFolderModeActive { get; set; }
+
+    #endregion
+
+    #region Public Methods
+
+    /// <summary>
+    /// Returns the required solution file information.
+    /// </summary>
+    /// <param name="dir">Pointer to the solution directory</param>
+    /// <param name="file">Pointer to the solution file name</param>
+    /// <param name="ops">Pointer to the solutions options file name</param>
+    /// <returns>If the method succeeds, it returns Microsoft.VisualStudio.VSConstants.S_OK. If it fails, it returns an error code.</returns>
+    public int GetSolutionInfo(out string dir, out string file, out string optionFile)
+    {
+      var solution = (IVsSolution)VsServiceProvider.GetService(typeof(SVsSolution));
+      return solution.GetSolutionInfo(out dir, out file, out optionFile);
+    }
+
+    public bool IsSolutionOpen()
+    {
+      var solution = (IVsSolution)VsServiceProvider.GetService(typeof(SVsSolution));
+      solution.GetProperty((int)__VSPROPID.VSPROPID_IsSolutionOpen, out object open);
+      SolutionOpen = (bool)open;
+      return SolutionOpen;
+    }
+
+    public bool IsOpenFolderModeActive()
+    {
+      var solution = (IVsSolution)VsServiceProvider.GetService(typeof(SVsSolution));
+      solution.GetProperty((int)__VSPROPID7.VSPROPID_IsInOpenFolderMode, out object folderMode);
+      OpenFolderModeActive = (bool)folderMode;
+      return true;
+    }
+
+    #endregion
+
+  }
+}
