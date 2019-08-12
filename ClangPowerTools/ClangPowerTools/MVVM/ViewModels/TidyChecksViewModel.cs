685ec04aea3df1cf5b7931c288656716faba6313
Horatiu Prica <horatiu.prica@caphyon.com>
2019-08-12 18:24:43 +0300

    TidyChecks view and model binding

--- ClangPowerTools/ClangPowerTools/MVVM/ViewModels/TidyChecksViewModel.cs
+++ ClangPowerTools/ClangPowerTools/MVVM/ViewModels/TidyChecksViewModel.cs
@@ -11,6 +11,8 @@ namespace ClangPowerTools
     #region Members
     public event PropertyChangedEventHandler PropertyChanged;
 
+    private string checkDescription = string.Empty;
+    private TidyCheckModel selectedCheck = new TidyCheckModel();
     #endregion
 
     public List<TidyCheckModel> TidyChecksList
@@ -23,6 +25,33 @@ namespace ClangPowerTools
       {
         TidyChecks.checks = value;
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("TidyChecksList"));
+      }
+    }
+
+    public string CheckDescription
+    {
+      get
+      {
+        return checkDescription;
+      }
+      set
+      {
+        checkDescription = value;
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("CheckDescription"));
+      }
+    }
+
+    public TidyCheckModel SelectedCheck
+    {
+      get
+      {
+        return selectedCheck;
+      }
+      set
+      {
+        selectedCheck = value;
+        CheckDescription = "Description: " + selectedCheck.Description;
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("SelectedCheck"));
       }
     }
   }
