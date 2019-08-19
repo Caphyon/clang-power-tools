74d62cfe0ba1e64276c30cf1753e70cc85caf005
rodicamihaela <vasilescu.rodica@yahoo.com>
2019-08-19 15:14:04 +0300

    rename ReflectionManager

--- ClangPowerTools/ClangPowerTools/Helpers/GetSetProperty.cs
+++ ClangPowerTools/ClangPowerTools/Helpers/GetSetProperty.cs
@@ -3,11 +3,11 @@ using System.Reflection;
 
 namespace ClangPowerTools.Helpers
 {
-  public static class ReflectionManager
+  public static class GetSetProperty
   {
-    public static object GetProperty<T>(object obj, string propertyName) => typeof(T).GetProperty(propertyName).GetValue(obj);
+    public static object Get<T>(object obj, string propertyName) => typeof(T).GetProperty(propertyName).GetValue(obj);
 
-    public static void SetProperty(object obj, string propertyName, object value)
+    public static void Set(object obj, string propertyName, object value)
     {
       PropertyInfo propertyInfo = obj.GetType().GetProperty(propertyName);
       propertyInfo.SetValue(obj, Convert.ChangeType(value, propertyInfo.PropertyType), null);
