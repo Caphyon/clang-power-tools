a978605362188dfca6a79a3a8337abf83c28a1a1
Enache Ionut <ionut.enache@caphyon.com>
2019-08-22 16:37:09 +0300

    create the build dorectory for the entire CMake project

--- ClangPowerTools/ClangPowerTools/CMake/CMakeBuilder.cs
+++ ClangPowerTools/ClangPowerTools/CMake/CMakeBuilder.cs
@@ -10,6 +10,7 @@ namespace ClangPowerTools.CMake
 {
   public class CMakeBuilder
   {
+    private readonly string directoryName = "build";
 
     public bool CreateBuildDirectory(string newDirName, out string dirPath)
     {
@@ -18,17 +19,17 @@ namespace ClangPowerTools.CMake
       if (Directory.Exists(dir) == false)
         return false;
 
-      var buildDirectory = Path.Combine(dir, newDirName);
-      if (Directory.Exists(buildDirectory))
+      dirPath = Path.Combine(dir, newDirName);
+      if (Directory.Exists(dirPath))
         return true;
 
-      Directory.CreateDirectory(buildDirectory);
+      Directory.CreateDirectory(dirPath);
       return true;
     }
 
     public void Build()
     {
-      if (CreateBuildDirectory("build", out string dirPath) == false)
+      if (CreateBuildDirectory(directoryName, out string dirPath) == false)
         return;
 
       var command = "cmake -DCMAKE_CONFIGURATION_TYPES=\"Debug; Release\" -DCMAKE_GENERATOR_PLATFORM=x64 -G \"Visual Studio 16 2019\" ..";
@@ -37,16 +38,26 @@ namespace ClangPowerTools.CMake
       {
         StartInfo = new System.Diagnostics.ProcessStartInfo()
         {
+          CreateNoWindow = true,
           WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden,
           WorkingDirectory = dirPath,
           FileName = "cmd.exe",
-          Arguments = "/C " + command
+          Arguments = "/c " + command
         }
       })
       {
         process.Start();
         process.WaitForExit();
       }
+    }
+
+    public void ClearBuildCashe()
+    {
+      CreateBuildDirectory(directoryName, out string dirPath);
+      if (string.IsNullOrWhiteSpace(dirPath))
+        return;
+
+      Directory.Delete(dirPath, true);
     }
 
   }
