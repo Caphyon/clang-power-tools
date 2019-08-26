c093d4b8d6ba5b5260f09f312528f1ca73f45a62
Enache Ionut <ionut.enache@caphyon.com>
2019-08-26 14:33:26 +0300

    create a representation for selected document for CMake projects

---
+++ ClangPowerTools/ClangPowerTools/Items/CurrentDocument.cs
@@ -0,0 +1,26 @@
+﻿using EnvDTE;
+
+namespace ClangPowerTools.Items
+{
+  public class CurrentDocument : IItem
+  {
+    #region Members
+
+    private Document mDocument;
+
+    #endregion
+
+    #region IItem implementation
+
+    public string GetName() => mDocument.Name;
+
+    public object GetObject() => mDocument.Object();
+
+    public string GetPath() => mDocument.FullName;
+
+    public void Save() => mDocument.Save();
+
+    #endregion
+
+  }
+}
