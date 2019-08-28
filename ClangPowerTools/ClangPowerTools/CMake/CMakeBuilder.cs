f634e8d59d109a097f016092df4fd20ef5092422
Enache Ionut <ionut.enache@caphyon.com>
2019-08-28 16:05:01 +0300

    add documentation for CMake build class

--- ClangPowerTools/ClangPowerTools/CMake/CMakeBuilder.cs
+++ ClangPowerTools/ClangPowerTools/CMake/CMakeBuilder.cs
@@ -5,8 +5,20 @@ namespace ClangPowerTools.CMake
 {
   public class CMakeBuilder
   {
-    private readonly string directoryName = "build";
+    #region Members
 
+    private readonly string directoryName = "CPTCMakeBuild";
+
+    #endregion
+
+    #region Public Methods
+
+    /// <summary>
+    /// Create the build directory("CPTCMakeBuild") for the current CMake project
+    /// </summary>
+    /// <param name="newDirName">The name of the created directory</param>
+    /// <param name="dirPath">The full path of the created directory</param>
+    /// <returns>True if the directory is created successfully. False otherwise.</returns>
     public bool CreateBuildDirectory(string newDirName, out string dirPath)
     {
       dirPath = null;
@@ -22,6 +34,9 @@ namespace ClangPowerTools.CMake
       return true;
     }
 
+    /// <summary>
+    /// Build the current CMake project. The process will generate one .sln file and posible more .vcxproj files.
+    /// </summary>
     public void Build()
     {
       if (CreateBuildDirectory(directoryName, out string dirPath) == false)
@@ -46,6 +61,9 @@ namespace ClangPowerTools.CMake
       }
     }
 
+    /// <summary>
+    /// Delete from the disk the CMake build directory and all its content.
+    /// </summary>
     public void ClearBuildCashe()
     {
       CreateBuildDirectory(directoryName, out string dirPath);
@@ -54,6 +72,8 @@ namespace ClangPowerTools.CMake
 
       Directory.Delete(dirPath, true);
     }
+
+    #endregion
 
   }
 }
