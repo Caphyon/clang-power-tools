29b69a7127f9cfe93815fd96943e2879139d94e8
Enache Ionut <ionut.enache@caphyon.com>
2019-09-03 11:49:13 +0300

    open purchase link when you press on upgrade button

--- ClangPowerTools/ClangPowerTools/MVVM/Views/ReleaseNotesView.xaml.cs
+++ ClangPowerTools/ClangPowerTools/MVVM/Views/ReleaseNotesView.xaml.cs
@@ -1,28 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ClangPowerTools.MVVM.Views
 {
   /// <summary>
   /// Interaction logic for ReleaseNotesView.xaml
   /// </summary>
-  public partial class ReleaseNotesView : UserControl
+  public partial class ReleaseNotesView : Window
   {
     public ReleaseNotesView()
     {
       InitializeComponent();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+      Process.Start(new ProcessStartInfo("https://clangpowertools.com/download.html#pricing"));
     }
   }
 }
