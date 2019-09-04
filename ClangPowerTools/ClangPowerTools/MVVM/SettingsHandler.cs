b2788ad388f654eb03b85673f2571f38fc61b1b0
Horatiu Prica <horatiu.prica@caphyon.com>
2019-09-04 12:30:21 +0300

    Remove AdditionalIncludes from old settings import

--- ClangPowerTools/ClangPowerTools/MVVM/SettingsHandler.cs
+++ ClangPowerTools/ClangPowerTools/MVVM/SettingsHandler.cs
@@ -207,7 +207,6 @@ namespace ClangPowerTools
       compilerSettingsModel.CompileFlags = clangOptions.ClangFlagsCollection;
       compilerSettingsModel.FilesToIgnore = clangOptions.FilesToIgnore;
       compilerSettingsModel.ProjectsToIgnore = clangOptions.ProjectsToIgnore;
-      compilerSettingsModel.AdditionalIncludes = clangOptions.AdditionalIncludes;
       compilerSettingsModel.WarningsAsErrors = clangOptions.TreatWarningsAsErrors;
       compilerSettingsModel.ContinueOnError = clangOptions.Continue;
       compilerSettingsModel.ClangAfterMSVC = clangOptions.ClangCompileAfterVsCompile;
