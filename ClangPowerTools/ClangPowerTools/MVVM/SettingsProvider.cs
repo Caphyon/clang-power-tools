89e2b24c991f524f85db28bdb0b6ec9caa418ad6
Horatiu Prica <horatiu.prica@caphyon.com>
2019-09-06 18:18:12 +0300

    Get and Set methods for compilerSettingsModel

--- ClangPowerTools/ClangPowerTools/MVVM/SettingsProvider.cs
+++ ClangPowerTools/ClangPowerTools/MVVM/SettingsProvider.cs
@@ -1,11 +1,21 @@
 ﻿namespace ClangPowerTools
 {
-  public static class SettingsProvider
+  public class SettingsProvider
   {
     public static CompilerSettingsViewModel CompilerSettingsViewModel { get; set; } = new CompilerSettingsViewModel();
     public static FormatSettingsViewModel FormatSettingsViewModel { get; set; } = new FormatSettingsViewModel();
     public static TidySettingsViewModel TidySettingsViewModel { get; set; } = new TidySettingsViewModel();
     public static TidyChecksViewModel TidyChecksViewModel { get; set; } = new TidyChecksViewModel();
     public static GeneralSettingsViewModel GeneralSettingsViewModel { get; set; } = new GeneralSettingsViewModel();
+
+    public CompilerSettingsModel GetCompilerSettingsModel()
+    {
+      return CompilerSettingsViewModel.CompilerModel;
+    }
+
+    public void SetCompilerSettingsModel(CompilerSettingsModel compilerSettingsModel)
+    {
+      CompilerSettingsViewModel.CompilerModel = compilerSettingsModel;
+    }
   }
 }
