efa4b8cb8713f051ca52f77fe0011b480e932707
Enache Ionut <ionut.enache@caphyon.com>
2019-09-10 19:17:07 +0300

    start new free and check free trial status functionality

---
+++ ClangPowerTools/ClangPowerTools/MVVM/Controllers/FreeTrialController.cs
@@ -0,0 +1,39 @@
+﻿using ClangPowerTools.Helpers;
+using System;
+
+namespace ClangPowerTools.MVVM.Controllers
+{
+  public class FreeTrialController
+  {
+    #region Members
+
+    private readonly RegistryUtility registryUtility;
+
+    private readonly string registerName = @"Software\Caphyon\cpt";
+    private readonly string keyName = "trial";
+    private readonly int days = -1;
+
+    #endregion
+
+    #region Constructor
+
+    public FreeTrialController() => registryUtility = new RegistryUtility(registerName);
+
+    #endregion
+
+    #region Public Methods
+
+    public bool Start() => registryUtility.WriteRegistryKey(keyName, DateTime.Now.ToString());
+
+    public bool IsActive()
+    {
+      var freeTrialStartTimeAsString = registryUtility.ReadRegistryKey(keyName);
+      var freeTrialStartTime = DateTime.Parse(freeTrialStartTimeAsString);
+
+      return DateTime.Now.Subtract(freeTrialStartTime).Days <= days;
+    }
+
+    #endregion
+
+  }
+}
