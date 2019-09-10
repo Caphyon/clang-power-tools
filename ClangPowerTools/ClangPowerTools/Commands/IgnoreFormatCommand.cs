5e0e50dfc10a46aa1161b0306a631fb017794e60
Horatiu Prica <horatiu.prica@caphyon.com>
2019-09-10 12:18:20 +0300

    Fix command call

--- ClangPowerTools/ClangPowerTools/Commands/IgnoreFormatCommand.cs
+++ ClangPowerTools/ClangPowerTools/Commands/IgnoreFormatCommand.cs
@@ -73,18 +73,19 @@ namespace ClangPowerTools.Commands
     public void RunIgnoreFormatCommand()
     {
       _ = Task.Run(() =>
-      { 
-      string filesToIgnore = SettingsProvider.FormatSettingsViewModel.FormatModel.FilesToIgnore;
-
-      if (filesToIgnore.Length > 0)
       {
-        ItemsCollector itemsCollector = new ItemsCollector();
-        List<string> documentsToIgnore = itemsCollector.GetFilesToIgnore();
-        SettingsHandler settingsHandler = new SettingsHandler();
+        string filesToIgnore = SettingsProvider.FormatSettingsViewModel.FormatModel.FilesToIgnore;
 
-        FormatSettingsModel settings = SettingsProvider.FormatSettingsViewModel.FormatModel;
-        AddItemsToIgnore(documentsToIgnore, settings, "FilesToIgnore");
-        settingsHandler.SaveSettings();
+        if (filesToIgnore.Length > 0)
+        {
+          ItemsCollector itemsCollector = new ItemsCollector();
+          List<string> documentsToIgnore = itemsCollector.GetFilesToIgnore();
+          SettingsHandler settingsHandler = new SettingsHandler();
+
+          FormatSettingsModel settings = SettingsProvider.FormatSettingsViewModel.FormatModel;
+          AddItemsToIgnore(documentsToIgnore, settings, "FilesToIgnore");
+          settingsHandler.SaveSettings();
+        }
       });
     }
 
