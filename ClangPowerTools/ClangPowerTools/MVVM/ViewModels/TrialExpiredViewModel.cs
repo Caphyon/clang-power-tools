b7d8531ddc7f9c06f33c63937beabf7b523af0b1
Horatiu Prica <horatiu.prica@caphyon.com>
2019-09-16 15:09:41 +0300

    Add TrialExpired view and viewmodel

---
+++ ClangPowerTools/ClangPowerTools/MVVM/ViewModels/TrialExpiredViewModel.cs
@@ -0,0 +1,60 @@
+﻿using ClangPowerTools.MVVM.Commands;
+using ClangPowerTools.MVVM.Views;
+using ClangPowerTools.MVVM.WebApi;
+using System.Diagnostics;
+using System.Windows.Input;
+
+namespace ClangPowerTools
+{
+  public class TrialExpiredViewModel
+  {
+    #region Members
+
+    private ICommand createAccountCommand;
+    private ICommand signInCommand;
+
+    #endregion
+
+    #region Properties
+
+    public bool CanExecute
+    {
+      get
+      {
+        return true;
+      }
+    }
+
+    #endregion
+
+    #region Commands
+
+    public ICommand CreateAccount
+    {
+      get => createAccountCommand ?? (createAccountCommand = new RelayCommand(() => CreatAccount(), () => CanExecute));
+    }
+
+    public ICommand SignIn
+    {
+      get => signInCommand ?? (signInCommand = new RelayCommand(() => SignIn(), () => CanExecute));
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    private void CreatAccount()
+    {
+      Process.Start(new ProcessStartInfo(WebApiUrl.signUpUrl));
+    }
+
+    private void SignIn()
+    {
+      var loginView = new LoginView();
+      loginView.ShowDialog();
+    }
+
+    #endregion
+  }
+}
+;
---
+++ ClangPowerTools/ClangPowerTools/MVVM/Views/TrialExpiredView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace ClangPowerTools.MVVM.Views
+{
+  /// <summary>
+  /// Interaction logic for TrialExpiredView.xaml
+  /// </summary>
+  public partial class TrialExpiredView : Window
+  {
+    public TrialExpiredView()
+    {
+      InitializeComponent();
+      DataContext = new TrialExpiredViewModel();
+    }
+  }
+}
