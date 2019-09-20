1431bf593847331ef3b1f41962e77df0869c0c1d
Horatiu Prica <horatiu.prica@caphyon.com>
2019-09-20 16:59:00 +0300

    Working download option and binding fixes

--- ClangPowerTools/ClangPowerTools/MVVM/ViewModels/LlvmSettingsViewModel.cs
+++ ClangPowerTools/ClangPowerTools/MVVM/ViewModels/LlvmSettingsViewModel.cs
@@ -1,13 +1,16 @@
 ﻿using ClangPowerTools.MVVM.Commands;
 using System.ComponentModel;
+using System.Net;
 using System.Windows.Input;
 
 namespace ClangPowerTools
 {
-  public class LlvmSettingsViewModel
+  public class LlvmSettingsViewModel : INotifyPropertyChanged
   {
     #region Members
+    public event PropertyChangedEventHandler PropertyChanged;
 
+    private LlvmSettingsModel llvmModel = new LlvmSettingsModel();
     private ICommand dowloadCommand;
     private ICommand deleteCommand;
     private ICommand stopCommand;
@@ -15,6 +18,20 @@ namespace ClangPowerTools
     #endregion
 
     #region Properties
+    public LlvmSettingsModel LlvmModel
+    {
+      get
+      {
+        return llvmModel;
+      }
+      set
+      {
+        llvmModel = value;
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("LlvmModel"));
+      }
+    }
+
+
     public bool CanExecute
     {
       get
@@ -32,7 +49,7 @@ namespace ClangPowerTools
 
     public ICommand DeleteCommand
     {
-      get => deleteCommand ?? (deleteCommand = new RelayCommand(() => DeleteLlvmVersion(), () => CanExecute));
+      get => deleteCommand ?? (deleteCommand = new RelayCommand(() => DeleteLlvmVersion("8.0.0"), () => CanExecute));
     }
 
     public ICommand StopCommand
@@ -45,12 +62,25 @@ namespace ClangPowerTools
     #region Methods
     private void DownloadLlvmVersion()
     {
+      using (var client = new WebClient())
+      {
+        client.DownloadProgressChanged += DownloadProgressChanged;
+        SettingsPathBuilder settingsPathBuilder = new SettingsPathBuilder();
+        string settingsPath = settingsPathBuilder.GetPath("") + "\\test.exe";
 
+        client.DownloadFileAsync(new System.Uri("https://github.com/llvm/llvm-project/releases/download/llvmorg-8.0.1/LLVM-8.0.1-win32.exe"), settingsPath);
+      }
     }
 
-    private void DeleteLlvmVersion()
+    private void DeleteLlvmVersion(string version)
     {
 
+    }
+    
+    private void DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+    {
+      llvmModel.DownloadProgress = e.ProgressPercentage;
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("LlvmModel"));
     }
 
     private void StopDownload()
