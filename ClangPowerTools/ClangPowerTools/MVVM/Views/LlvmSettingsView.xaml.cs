bc6c6acb38697cafe77d1ecf5732d2d0d3c061aa
Horatiu Prica <horatiu.prica@caphyon.com>
2019-09-20 13:59:15 +0300

    Add view

---
+++ ClangPowerTools/ClangPowerTools/MVVM/Views/LlvmSettingsView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+namespace ClangPowerTools.Views
+{
+  /// <summary>
+  /// Interaction logic for LlvmSettingsView.xaml
+  /// </summary>
+  public partial class LlvmSettingsView : UserControl
+  {
+    public LlvmSettingsView()
+    {
+      InitializeComponent();
+      DataContext = new LlvmSettingsViewModel();
+    }
+  }
+}
