7215b3a4fc8386c3ff6d3336056f4e3addd4854a
Horatiu Prica <horatiu.prica@caphyon.com>
2019-09-26 17:18:32 +0300

    Create LlvmVersions

---
+++ ClangPowerTools/ClangPowerTools/MVVM/LlvmVersions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClangPowerTools
+{
+  public class LlvmVersions
+  {
+    public static List<string> Versions { get; } = new List<string>
+    {
+      "LLVM 9.0.0",
+      "LLVM 8.0.1",
+      "LLVM 8.0.0",
+      "LLVM 7.1.0",
+      "LLVM 7.0.1",
+      "LLVM 7.0.0",
+      "LLVM 6.0.1",
+      "LLVM 6.0.0",
+      "LLVM 5.0.2",
+    };
+  }
+}
