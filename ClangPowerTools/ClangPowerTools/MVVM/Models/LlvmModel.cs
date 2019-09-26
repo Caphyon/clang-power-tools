ad0f324c6db443a8dcf86cc99f4dea5144031317
Horatiu Prica <horatiu.prica@caphyon.com>
2019-09-26 17:18:13 +0300

    Create LlvmModel

---
+++ ClangPowerTools/ClangPowerTools/MVVM/Models/LlvmModel.cs
@@ -0,0 +1,17 @@
+﻿namespace ClangPowerTools
+{
+  public class LlvmModel
+  {
+    public string Name { get; set; }
+
+    public bool IsInstalled { get; set; }
+
+    public bool IsSelected { get; set; }
+
+    public int DownloadProgress { get; set; } = 0;
+
+    public int MinProgress { get; set; } = 0;
+
+    public int MaxProgress { get; set; } = 100;
+  }
+}
