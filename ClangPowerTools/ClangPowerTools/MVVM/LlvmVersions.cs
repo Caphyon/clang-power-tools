d30f0609fa4adb9ceed137b8dfbee18d8f0731a3
Horatiu Prica <horatiu.prica@caphyon.com>
2019-10-01 15:39:37 +0300

    Change the version values

--- ClangPowerTools/ClangPowerTools/MVVM/LlvmVersions.cs
+++ ClangPowerTools/ClangPowerTools/MVVM/LlvmVersions.cs
@@ -6,15 +6,15 @@ namespace ClangPowerTools
   {
     public static List<string> Versions { get; } = new List<string>
     {
-      "LLVM 9.0.0",
-      "LLVM 8.0.1",
-      "LLVM 8.0.0",
-      "LLVM 7.1.0",
-      "LLVM 7.0.1",
-      "LLVM 7.0.0",
-      "LLVM 6.0.1",
-      "LLVM 6.0.0",
-      "LLVM 5.0.2",
+      "9.0.0",
+      "8.0.1",
+      "8.0.0",
+      "7.1.0",
+      "7.0.1",
+      "7.0.0",
+      "6.0.1",
+      "6.0.0",
+      "5.0.2",
     };
   }
 }
