85192257f7073edcaf652bda549b17685d4581bf
Horatiu Prica <horatiu.prica@caphyon.com>
2019-10-04 13:20:06 +0300

    NotifyPropertyChange on LlvmModel

--- ClangPowerTools/ClangPowerTools/MVVM/Models/LlvmModel.cs
+++ ClangPowerTools/ClangPowerTools/MVVM/Models/LlvmModel.cs
@@ -1,18 +1,49 @@
-﻿namespace ClangPowerTools
+﻿using System.ComponentModel;
+
+namespace ClangPowerTools
 {
-  public class LlvmModel
+  public class LlvmModel : INotifyPropertyChanged
   {
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    private bool isDownloading = false;
+    private int downloadProgress = 0;
+
     public string Version { get; set; } = string.Empty;
 
     public bool IsInstalled { get; set; } = false;
 
-    public bool IsDownloading { get; set; } = false;
+    public bool IsDownloading
+    {
+      get
+      {
+        return isDownloading;
+      }
+
+      set
+      {
+        isDownloading = value;
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsDownloading"));
+      }
+    }
 
     public bool IsSelected { get; set; } = false;
 
     public bool Cancel { get; set; } = false;
 
-    public int DownloadProgress { get; set; } = 0;
+    public int DownloadProgress
+    {
+      get
+      {
+        return downloadProgress;
+      }
+
+      set
+      {
+        downloadProgress = value;
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("DownloadProgress"));
+      }
+    }
 
     public int MinProgress { get; set; } = 0;
 
