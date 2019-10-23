4d8e4582b482cf5bf9fe6dc513bf857fe7d297f9
Enache Ionut <ionut.enache@caphyon.com>
2019-10-23 11:44:00 +0300

    add documentation for the license validator abstract class

--- ClangPowerTools/ClangPowerTools/MVVM/Interfaces/LicenseValidator.cs
+++ ClangPowerTools/ClangPowerTools/MVVM/Interfaces/LicenseValidator.cs
@@ -1,23 +1,45 @@
 ﻿using System.IO;
+using System.Threading.Tasks;
 
 namespace ClangPowerTools.MVVM.Interfaces
 {
+  /// <summary>
+  /// Contains the logic of token retrival letting license validation logic
+  /// to be implemented in the extended classes.
+  /// </summary>
   public abstract class LicenseValidator : ILicense
   {
     #region Members
 
+    /// <summary>
+    /// License token name
+    /// </summary>
     private readonly string fileName = "ctpjwt";
 
     #endregion
 
     #region ILicense Implementation
 
-    public string GetToken()
+    /// <summary>
+    /// Get the license token path
+    /// </summary>
+    /// <returns>License token path if the file exists. Empty string otherwise.</returns>
+    public string GetTokenPath()
     {
       SettingsPathBuilder settingsPathBuilder = new SettingsPathBuilder();
       string filePath = settingsPathBuilder.GetPath(fileName);
 
-      if (File.Exists(filePath) == false)
+      return File.Exists(filePath) == true ? filePath : string.Empty;
+    }
+
+    /// <summary>
+    /// Get the content of the license token
+    /// </summary>
+    /// <returns>Content of license token</returns>
+    public string GetToken()
+    {
+      var filePath = GetTokenPath();
+      if (string.IsNullOrWhiteSpace(filePath))
         return string.Empty;
 
       using var streamReader = new StreamReader(filePath);
@@ -26,7 +48,11 @@ namespace ClangPowerTools.MVVM.Interfaces
       return jwt;
     }
 
-    public abstract bool Validate();
+    /// <summary>
+    /// Check if the user license is active
+    /// </summary>
+    /// <returns>True if the license is active. False otherwise</returns>
+    public abstract Task<bool> ValidateAsync();
 
     #endregion
 
