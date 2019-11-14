64fa1c8f04c3ff6bb477452c911ef11ec5b40223
Horatiu Prica <horatiu.prica@caphyon.com>
2019-11-14 16:27:05 +0200

    Refactor  InputDataModel to use ObservableCollections

--- ClangPowerTools/ClangPowerTools/MVVM/ViewModels/InputDataViewModel.cs
+++ ClangPowerTools/ClangPowerTools/MVVM/ViewModels/InputDataViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using ClangPowerTools.MVVM.Commands;
 using ClangPowerTools.Views;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Windows.Input;
 
@@ -11,30 +12,18 @@ namespace ClangPowerTools
     public event PropertyChangedEventHandler PropertyChanged;
 
     private InputDataView inputDataView;
-    private string textBoxInput = string.Empty;
     private ICommand okCommand;
     #endregion
 
     #region Constructor
     public InputDataViewModel(string content)
     {
-      TextBoxInput = content;
+      //TextBoxInput = content;
     }
     #endregion
 
     #region Properties
-    public string TextBoxInput
-    {
-      get
-      {
-        return textBoxInput;
-      }
-      set
-      {
-        textBoxInput = value;
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("TextBoxInput"));
-      }
-    }
+    public ObservableCollection<string> Inputs = new ObservableCollection<string>();
 
     public ICommand OkCommand
     {
