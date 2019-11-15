14f0e4dbe536224f0c11e04132696fef5c5559a5
Horatiu Prica <horatiu.prica@caphyon.com>
2019-11-15 15:45:52 +0200

    Methods in code behind for index and delete

--- ClangPowerTools/ClangPowerTools/MVVM/Views/InputDataView.xaml.cs
+++ ClangPowerTools/ClangPowerTools/MVVM/Views/InputDataView.xaml.cs
@@ -7,11 +7,26 @@ namespace ClangPowerTools.Views
   /// </summary>
   public partial class InputDataView : Window
   {
+    private InputDataViewModel inputDataViewModel = new InputDataViewModel();
     public InputDataView(InputDataViewModel inputDataViewModel)
     {
       InitializeComponent();
+      //TODO might remove inputDataViewModel paramater from constructor
+      this.inputDataViewModel = inputDataViewModel;
       DataContext = inputDataViewModel;
       Owner = SettingsProvider.SettingsView;
+    }
+
+    private void DeleteButton(object sender, RoutedEventArgs e)
+    {
+      var elementIndex = GetElementIndex(sender as FrameworkElement);
+      inputDataViewModel.DeleteInput(elementIndex);
+    }
+
+    private int GetElementIndex(FrameworkElement frameworkElement)
+    {
+      var element = frameworkElement.DataContext;
+      return InputsList.Items.IndexOf(element);
     }
   }
 }
