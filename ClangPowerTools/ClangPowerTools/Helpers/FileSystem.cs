b42628e4ce0597b487a1708aaacca67fbdc400d9
Horatiu Prica <horatiu.prica@caphyon.com>
2019-12-12 13:18:19 +0200

    Make the FileSystem helper class static

--- ClangPowerTools/ClangPowerTools/Helpers/FileSystem.cs
+++ ClangPowerTools/ClangPowerTools/Helpers/FileSystem.cs
@@ -2,19 +2,19 @@
 
 namespace ClangPowerTools.Helpers
 {
-  public class FileSystem
+  public static class FileSystem
   {
-    public void CreateDirectory(string path)
+    public static void CreateDirectory(string path)
     {
       if (Directory.Exists(path) == false) Directory.CreateDirectory(path);
     }
 
-    public void DeleteDirectory(string path)
+    public static void DeleteDirectory(string path)
     {
       if (Directory.Exists(path)) Directory.Delete(path, true);
     }
 
-    public void DeleteFile(string path)
+    public static void DeleteFile(string path)
     {
       if (File.Exists(path)) File.Delete(path);
     }
