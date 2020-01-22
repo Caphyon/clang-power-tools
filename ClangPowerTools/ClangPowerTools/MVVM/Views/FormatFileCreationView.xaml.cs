52fcb4d6c397b452c479c1619d54cecbd81d51fe
Horatiu Prica <horatiu.prica@caphyon.com>
2020-01-22 16:15:37 +0200

    Add empty view

---
+++ ClangPowerTools/ClangPowerTools/MVVM/Views/FormatFileCreationView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace ClangPowerTools.MVVM.Views
+{
+  /// <summary>
+  /// Interaction logic for FormatFileCreationView.xaml
+  /// </summary>
+  public partial class FormatFileCreationView : Window
+  {
+    public FormatFileCreationView()
+    {
+      InitializeComponent();
+    }
+  }
+}
