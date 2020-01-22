fbeca53f7af0f7a690515753ecfdf7db129d3369
Horatiu Prica <horatiu.prica@caphyon.com>
2020-01-22 18:50:39 +0200

    Add IFormatOption and FormatStyleModel

---
+++ ClangPowerTools/ClangPowerTools/MVVM/Interfaces/IFormatOption.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClangPowerTools.MVVM.Interfaces
+{
+  interface IFormatOption
+  {
+    public string Name { get; set; }
+  }
+}
---
+++ ClangPowerTools/ClangPowerTools/MVVM/Models/FormatStyleModel.cs
@@ -0,0 +1,16 @@
+﻿using ClangPowerTools.MVVM.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClangPowerTools.MVVM.Models
+{
+  public class FormatStyleModel : IFormatOption
+  {
+    public string Name { get; set; }
+
+
+  }
+}
