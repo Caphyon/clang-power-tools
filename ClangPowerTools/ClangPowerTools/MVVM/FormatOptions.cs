81fb55d32e41cb8a369b8989c102663a00bc7d4e
Horatiu Prica <horatiu.prica@caphyon.com>
2020-01-23 17:50:27 +0200

    FormatOptions default list

---
+++ ClangPowerTools/ClangPowerTools/MVVM/FormatOptions.cs
@@ -0,0 +1,15 @@
+﻿using ClangPowerTools.MVVM.Interfaces;
+using ClangPowerTools.MVVM.Models;
+using System.Collections.Generic;
+
+namespace ClangPowerTools
+{
+  public class FormatOptions
+  {
+    public static List<IFormatOption> FormatOptionsList = new List<IFormatOption>()
+    {
+      new FormatOptionModel{ Name = "test", Description = "xxxxxxxx", Input = string.Empty},
+      new FormatOptionToggleModel{Name = "toggleTest", Description = "yyyyy", IsEnabled=false}
+    };
+  }
+}
