4e8a2ce12f0276356e0fd840e9b6f50b4cf18c13
Horatiu Prica <horatiu.prica@caphyon.com>
2020-02-10 15:24:41 +0200

    Testing text highlight in data

--- ClangPowerTools/ClangPowerTools/MVVM/FormatOptionsData.cs
+++ ClangPowerTools/ClangPowerTools/MVVM/FormatOptionsData.cs
@@ -1,6 +1,8 @@
 ﻿using ClangPowerTools.MVVM.Interfaces;
 using ClangPowerTools.MVVM.Models;
 using System.Collections.Generic;
+using System.Windows.Documents;
+using System.Windows.Media;
 
 namespace ClangPowerTools
 {
@@ -9,7 +11,8 @@ namespace ClangPowerTools
     public static List<IFormatOption> FormatOptions = new List<IFormatOption>()
     {
       new FormatOptionModel{ Name = "AccessModifierOffset", Paramater = "int", Description = "The extra indent or outdent of access modifiers, e.g. public:.", Input = string.Empty},
-      new FormatOptionToggleModel{Name = "AlignEscapedNewlinesLeft", Paramater = "bool", Description = "If true, aligns escaped newlines as far left as possible. Otherwise puts them into the right-most column. This does not necessarily mean flushing lings to the left but, rather, attempting to align the current line's left margin with the previous line's left margin", IsEnabled=false}
+      new FormatOptionToggleModel{Name = "AlignEscapedNewlinesLeft", Paramater = "bool", Description = "If true, aligns escaped newlines as far left as possible. Otherwise puts them into the right-most column. This does not necessarily mean flushing lings to the left but, rather, attempting to align the current line's left margin with the previous line's left margin", IsEnabled=false},
+      new FormatOptionToggleModel{Name = "AlignTrailingComments", Paramater = "bool", Description = "If <Run Background=\"#FFCDCDCD\" Text=\"true\"/>, aligns trailing comments", IsEnabled=false}
     };
   }
 }
