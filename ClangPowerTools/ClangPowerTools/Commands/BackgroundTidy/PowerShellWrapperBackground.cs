0b9549507924f6af8ebbe0cc3507e3fab02e2f2e
Enache Ionut <enachestoianionut@gmail.com>
2020-03-02 12:39:44 +0200

    rename namespace

--- ClangPowerTools/ClangPowerTools/Commands/BackgroundTidy/PowerShellWrapperBackground.cs
+++ ClangPowerTools/ClangPowerTools/Commands/BackgroundTidy/PowerShellWrapperBackground.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ClangPowerTools.Commands.BackgroundClangTidy
+namespace ClangPowerTools.Commands.BackgroundTidy
 {
   public class PowerShellWrapperBackground
   {
