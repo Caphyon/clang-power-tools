caeb655b8b61c1753e8ccaec9db02096d81987f4
horatiu.prica <horatiu.prica@caphyon.com>
2020-03-31 15:19:08 +0300

    Change focus when interacting with flags

--- ClangPowerTools/ClangPowerTools/MVVM/Views/FormatEditorView.xaml.cs
+++ ClangPowerTools/ClangPowerTools/MVVM/Views/FormatEditorView.xaml.cs
@@ -17,24 +17,31 @@ namespace ClangPowerTools.MVVM.Views
       DataContext = formatStyleViewModel;
       CodeEditor.Text = "// --- Clang Power Tools - Format Style Editor ---\r\n//\r\n// Add your code here\r\n//\r\n// Format is run automatically on your code after \r\n// enabling any of the style options\r\n//\r\n// Check the Output tab to see your formatted code";
       CodeEditorReadOnly.Text = "// Your formatted code will be displayed here";
-    
     }
 
     private void RunFormat_TextChanged(object sender, TextChangedEventArgs e)
     {
+      object interactable = (e.OriginalSource as FrameworkElement).DataContext;
+      ChangeSelectedItem(interactable);
       formatStyleViewModel.RunFormat();
     }
 
     private void RunFormat_SelectionChanged(object sender, SelectionChangedEventArgs e)
     {
+      object interactable = (e.OriginalSource as FrameworkElement).DataContext;
+      ChangeSelectedItem(interactable);
       formatStyleViewModel.RunFormat();
     }
 
-    private void ModifyInputButton_Click(object sender, RoutedEventArgs e)
+    private void ModifyFocus(object sender, RoutedEventArgs e)
     {
-      object clicked = (e.OriginalSource as FrameworkElement).DataContext;
-      var selectedItem = FormatOptions.ItemContainerGenerator.ContainerFromItem(clicked) as ListViewItem;
-      if (selectedItem == null) return;
+      object interactable = (e.OriginalSource as FrameworkElement).DataContext;
+      ChangeSelectedItem(interactable);
+    }
+
+    private void ChangeSelectedItem(object interactable)
+    {
+      if (!(FormatOptions.ItemContainerGenerator.ContainerFromItem(interactable) is ListViewItem selectedItem)) return;
       selectedItem.IsSelected = true;
     }
   }
