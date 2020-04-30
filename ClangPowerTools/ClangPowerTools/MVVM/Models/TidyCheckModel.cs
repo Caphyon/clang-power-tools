dc22806dd268b26439385a3e40e964da42baf32e
Enache-Ionut <enachestoianionut@gmail.com>
2020-04-30 11:58:59 +0300

    activate iNotifyPropertyChange on tidy check model

--- ClangPowerTools/ClangPowerTools/MVVM/Models/TidyCheckModel.cs
+++ ClangPowerTools/ClangPowerTools/MVVM/Models/TidyCheckModel.cs
@@ -1,11 +1,37 @@
-﻿namespace ClangPowerTools
+﻿using System.ComponentModel;
+
+namespace ClangPowerTools
 {
-  public class TidyCheckModel
+  public class TidyCheckModel : INotifyPropertyChanged
   {
+    #region Members
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    private bool isChecked = false;
+
+    #endregion
+
+
+    #region Properties
+
     public string Name { get; set; } = string.Empty;
 
     public string Description { get; set; } = string.Empty;
 
-    public bool IsChecked { get; set; } = false;
+    public bool IsChecked
+    {
+      get
+      {
+        return isChecked;
+      }
+      set
+      {
+        isChecked = value;
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsChecked"));
+      }
+    }
+
+    #endregion
+
   }
 }
