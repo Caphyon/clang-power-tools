0dd46575f0219a132578188da3215d70034317d4
Enache-Ionut <enachestoianionut@gmail.com>
2020-05-05 11:46:23 +0300

    manage the current state of the list of checks

---
+++ ClangPowerTools/ClangPowerTools/Events/BoolEventArgs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClangPowerTools.Events
+{
+  public class BoolEventArgs : EventArgs
+  {
+    public bool Value { get; set; }
+
+    public BoolEventArgs(bool value) => Value = value;
+
+  }
+}
---
+++ ClangPowerTools/ClangPowerTools/MVVM/CollectionElementsCounter.cs
@@ -0,0 +1,63 @@
+﻿using ClangPowerTools.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ClangPowerTools.MVVM
+{
+  public static class CollectionElementsCounter
+  {
+    #region Members
+
+    private static int count = 0;
+    private static int length = 0;
+    private static bool full = false;
+
+    public static event EventHandler<BoolEventArgs> ButtonStateEvent;
+
+    #endregion
+
+
+    #region Constructor
+
+    public static void Initialize(IEnumerable<TidyCheckModel> collection)
+    {
+      length = collection.Count();
+      foreach (var item in collection)
+      {
+        if (item.IsChecked)
+          ++count;
+      }
+    }
+
+    #endregion
+
+
+    #region Methods
+
+    public static void Add()
+    {
+      ++count;
+      if (count == length)
+      {
+        full = true;
+        ButtonStateEvent?.Invoke(null, new BoolEventArgs(true));
+      }
+    }
+
+    public static void Remove()
+    {
+      --count;
+      if (full)
+      {
+        full = false;
+        ButtonStateEvent?.Invoke(null, new BoolEventArgs(false));
+      }
+    }
+
+    public static bool IsEmpty() => length == 0;
+
+    #endregion
+
+  }
+}
