852810629422ba60d39db57396d7ad98d1a86120
Enache-Ionut <enachestoianionut@gmail.com>
2020-06-10 16:45:50 +0300

    disable the -fallback-style when the -style option is not set on "file"

--- ClangPowerTools/ClangPowerTools/MVVM/Views/FormatSettingsView.xaml.cs
+++ ClangPowerTools/ClangPowerTools/MVVM/Views/FormatSettingsView.xaml.cs
@@ -12,5 +12,10 @@ namespace ClangPowerTools.Views
       InitializeComponent();
       DataContext = new FormatSettingsViewModel();
     }
+
+    private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+      FallbackStyleComboBox.IsEnabled = StyleComboBox.SelectedItem.ToString() == ComboBoxConstants.kFile;
+    }
   }
 }
