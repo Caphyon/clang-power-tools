9179f40475a6758b428e791156e020adbb9ca1da
horatiu.prica <horatiu.prica@caphyon.com>
2020-06-22 18:23:23 +0300

    Implement IView interface in all settings views

--- ClangPowerTools/ClangPowerTools/MVVM/Views/CompilerSettingsView.xaml.cs
+++ ClangPowerTools/ClangPowerTools/MVVM/Views/CompilerSettingsView.xaml.cs
@@ -1,15 +1,22 @@
-﻿using System.Windows.Controls;
+﻿using ClangPowerTools.MVVM.Interfaces;
+using System.Windows.Controls;
 
 namespace ClangPowerTools.Views
 {
   /// <summary>
   /// Interaction logic for CompilerSettingsView.xaml
   /// </summary>
-  public partial class CompilerSettingsView : UserControl
+  public partial class CompilerSettingsView : UserControl, IView
   {
     public CompilerSettingsView()
     {
       InitializeComponent();
+      DataContext = new CompilerSettingsViewModel();
+      SettingsHandler.ResetSettingsView += ResetView;
+    }
+
+    public void ResetView()
+    {
       DataContext = new CompilerSettingsViewModel();
     }
   }
