5e3614771a844a5ca0eeb5704598c9b04c4d5aad
horatiu.prica <horatiu.prica@caphyon.com>
2020-06-24 19:45:41 +0300

    Post and Get methods for SettingsApi

--- ClangPowerTools/ClangPowerTools/Helpers/SettingsApi.cs
+++ ClangPowerTools/ClangPowerTools/Helpers/SettingsApi.cs
@@ -3,38 +3,63 @@ using ClangPowerTools.MVVM.LicenseValidation;
 using ClangPowerTools.MVVM.WebApi;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace ClangPowerTools
 {
   public class SettingsApi
   {
-    #region Methods
+    #region Public Methods
 
     public async Task UploadSettingsAsync()
     {
       ApiUtility.InitializeApiClient();
       if (await NetworkUtility.CheckInternetConnectionAsync() == false) return;
 
-      string settingsJson = "";
-      HttpResponseMessage userAccoutHttpRestul = await GetHttpResponseAsync(settingsJson);
-
-      if (userAccoutHttpRestul.IsSuccessStatusCode)
-      {
-
-      }
-
-      //userTokenHttpResponse.
-
+      var settingsHandler = new SettingsHandler();
+      string json = settingsHandler.GetSettingsAsJson();
+      await PostSettingsAsync(json);
     }
 
-    private static async Task<HttpResponseMessage> GetHttpResponseAsync(string settingsJson)
+    public async Task DownloadSettingsAsync()
+    {
+      ApiUtility.InitializeApiClient();
+      if (await NetworkUtility.CheckInternetConnectionAsync() == false) return;
+
+
+      HttpResponseMessage httpResponseMessage = await GetSettingsAsync();
+      if (httpResponseMessage.IsSuccessStatusCode)
+      {
+        var settingsHandler = new SettingsHandler();
+        settingsHandler.SaveSettings();
+      }
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    private async Task<HttpResponseMessage> PostSettingsAsync(string settingsJson)
+    {
+      SetAuthenticationHeader();
+
+      using StringContent content = new StringContent(settingsJson, Encoding.UTF8, "application/json");
+      return await ApiUtility.ApiClient.PostAsync(WebApiUrl.settingsConfig, content);
+    }
+
+    private async Task<HttpResponseMessage> GetSettingsAsync()
+    {
+      SetAuthenticationHeader();
+
+      return await ApiUtility.ApiClient.GetAsync(WebApiUrl.settingsConfig);
+    }
+
+    private static void SetAuthenticationHeader()
     {
       var token = new Token();
       token.GetToken(out string jwt);
       ApiUtility.ApiClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", jwt);
-      string webApiUrl = string.Concat(WebApiUrl.settingsConfig, "/", settingsJson);
-      return await ApiUtility.ApiClient.GetAsync(webApiUrl);
     }
 
     #endregion
