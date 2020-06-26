59c53f52142354abc0c18040e00363ea25b5b27e
Enache-Ionut <enachestoianionut@gmail.com>
2020-06-26 15:14:31 +0300

    create the account view model basic class

---
+++ ClangPowerTools/ClangPowerTools/MVVM/ViewModels/AccountViewModel.cs
@@ -0,0 +1,49 @@
+﻿using ClangPowerTools.MVVM.Models;
+using System.ComponentModel;
+
+namespace ClangPowerTools
+{
+  public class AccountViewModel : INotifyPropertyChanged
+  {
+    #region Members
+
+    private AccountModel accountModel;
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    #endregion
+
+
+    #region Properties
+
+    public AccountModel AccountModel
+    {
+      get
+      {
+        return accountModel;
+      }
+      set
+      {
+        accountModel = value;
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("AccountModel"));
+      }
+    }
+
+    public bool CanExecute
+    {
+      get
+      {
+        return true;
+      }
+    }
+
+    #endregion
+
+
+    #region Methods
+
+
+
+
+    #endregion
+  }
+}
