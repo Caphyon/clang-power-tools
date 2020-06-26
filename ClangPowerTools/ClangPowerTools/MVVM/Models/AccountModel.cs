a32eb2c05a8b0f55800acb544a701b2863d18626
Enache-Ionut <enachestoianionut@gmail.com>
2020-06-26 15:13:47 +0300

    create the account model

---
+++ ClangPowerTools/ClangPowerTools/MVVM/Models/AccountModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ClangPowerTools.MVVM.Models
+{
+  public class AccountModel
+  {
+    #region Properties
+
+    public string UserName { get; set; }
+    public string Email { get; set; }
+
+    public string LicenseType { get; set; }
+    public DateTime LicenseExpirationDate { get; set; }
+
+    public string Version { get; set; }
+
+    #endregion
+
+  }
+}
