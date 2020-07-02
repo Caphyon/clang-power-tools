bfa2d249798e689fd4e75ccce1534358f9faaacd
Enache-Ionut <enachestoianionut@gmail.com>
2020-07-02 13:10:01 +0300

    serialize license type using custom name

--- ClangPowerTools/ClangPowerTools/MVVM/LicenseValidation/LicenseType.cs
+++ ClangPowerTools/ClangPowerTools/MVVM/LicenseValidation/LicenseType.cs
@@ -1,10 +1,24 @@
-﻿namespace ClangPowerTools.MVVM.LicenseValidation
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Runtime.Serialization;
+
+namespace ClangPowerTools.MVVM.LicenseValidation
 {
+  [Serializable]
+  [JsonConverter(typeof(StringEnumConverter))]
   public enum LicenseType
   {
+    [EnumMember(Value = "Commercial")]
     Commercial,
+
+    [EnumMember(Value = "Personal")]
     Personal,
+
+    [EnumMember(Value = "Trial")]
     Trial,
+
+    [EnumMember(Value = "NoLicense")]
     NoLicense
   }
 }
