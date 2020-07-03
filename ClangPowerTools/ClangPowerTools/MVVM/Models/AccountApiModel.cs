5645b97ba7720384e2e72bfd184b51d124ea4d46
Enache-Ionut <enachestoianionut@gmail.com>
2020-07-03 10:02:34 +0300

    fix typo in the class name

---
+++ ClangPowerTools/ClangPowerTools/MVVM/Models/AccountApiModel.cs
@@ -0,0 +1,15 @@
+﻿namespace ClangPowerTools.MVVM.Models
+{
+  public class AccountApiModel
+  {
+    #region Properties
+
+    public string firstname { get; set; } = string.Empty;
+
+    public string lastname { get; set; } = string.Empty;
+
+    public string email { get; set; } = string.Empty;
+
+    #endregion
+  }
+}
