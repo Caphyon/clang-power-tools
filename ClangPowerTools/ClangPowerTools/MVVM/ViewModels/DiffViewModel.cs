d50e0c1fb72918c5202734548cd5d8a8d46accfc
horatiu.prica <horatiu.prica@caphyon.com>
2020-07-14 22:37:04 +0300

    DiffWindow, DiffViewModel

---
+++ ClangPowerTools/ClangPowerTools/MVVM/ViewModels/DiffViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace ClangPowerTools
+{
+  public class DiffViewModel
+  {
+    #region Members
+
+    public string Html { get; set; }
+
+    #endregion
+
+    #region Constructor 
+
+    public DiffViewModel(string html)
+    {
+
+    }
+
+    #endregion
+  }
+}
