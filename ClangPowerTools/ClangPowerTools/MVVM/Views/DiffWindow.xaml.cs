fe27055622436e3a80720c8ecacb972684316cb7
horatiu.prica <horatiu.prica@caphyon.com>
2020-07-14 22:37:04 +0300

    DiffWindow, DiffViewModel

---
+++ ClangPowerTools/ClangPowerTools/MVVM/Views/DiffWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace ClangPowerTools.MVVM.Views
+{
+  /// <summary>
+  /// Interaction logic for DiffWindow.xaml
+  /// </summary>
+  public partial class DiffWindow : Window
+  {
+    public DiffWindow(string html)
+    {
+      DataContext = new DiffViewModel(html);
+      InitializeComponent();
+      MyWebBrowser.NavigateToString(html);
+    }
+
+    //Empty constructor used for XAML IntelliSense
+    public DiffWindow()
+    {
+
+    }
+  }
+}
