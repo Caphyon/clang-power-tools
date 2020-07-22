95fdadc0545395b0dac2c57a3bde73f23cbd8127
horatiu.prica <horatiu.prica@caphyon.com>
2020-07-22 19:11:43 +0300

    LoadingView window

---
+++ ClangPowerTools/ClangPowerTools/MVVM/Views/LoadingView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace ClangPowerTools.MVVM.Views
+{
+  /// <summary>
+  /// Interaction logic for LoadingView.xaml
+  /// </summary>
+  public partial class LoadingView : Window
+  {
+    public LoadingView()
+    {
+      InitializeComponent();
+    }
+  }
+}
