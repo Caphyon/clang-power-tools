a69bf9f8a8a8c768f36c38d2223a65fea3f26800
horatiu.prica <horatiu.prica@caphyon.com>
2020-08-13 17:37:11 +0300

    Multiple toggle models and view

---
+++ ClangPowerTools/ClangPowerTools/MVVM/Models/FormatOptionMultipleToggleModel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace ClangPowerTools.MVVM.Models
+{
+  public class FormatOptionMultipleToggleModel : FormatOptionModel
+  {
+    #region Properties
+
+    public List<ToggleModel> ToggleFlags { get; set; } = new List<ToggleModel>();
+
+    #endregion
+
+    #region Constructor
+
+    public FormatOptionMultipleToggleModel()
+    {
+      HasMultipleToggle = true;
+    }
+
+    #endregion
+
+    #region Methods
+
+    public string CreateFlag()
+    {
+      var sb = new StringBuilder();
+      foreach (var item in ToggleFlags)
+      {
+        sb.AppendLine(string.Concat(item.Name, ": ", item.Value.ToString()));
+      }
+      return sb.ToString();
+    }
+
+    #endregion
+
+  }
+}
---
+++ ClangPowerTools/ClangPowerTools/MVVM/Models/ToggleModel.cs
@@ -0,0 +1,22 @@
+﻿namespace ClangPowerTools.MVVM.Models
+{
+  public class ToggleModel
+  {
+    #region Properties 
+
+    public string Name { get; set; }
+    public ToggleValues Value { get; set; }
+
+    #endregion
+
+    #region Constructor
+
+    public ToggleModel(string name, ToggleValues value)
+    {
+      Name = name;
+      Value = value;
+    }
+
+    #endregion
+  }
+}
