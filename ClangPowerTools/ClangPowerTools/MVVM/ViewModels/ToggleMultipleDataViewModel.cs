8015f571388a788827b5db6e736392080d9f7f71
horatiu.prica <horatiu.prica@caphyon.com>
2020-08-13 17:37:11 +0300

    Multiple toggle models and view

---
+++ ClangPowerTools/ClangPowerTools/MVVM/ViewModels/ToggleMultipleDataViewModel.cs
@@ -0,0 +1,41 @@
+﻿using ClangPowerTools.MVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ClangPowerTools
+{
+  public class ToggleMultipleDataViewModel
+  {
+    public List<ToggleModel> Input { get; set; } = new List<ToggleModel>();
+
+    #region Properties
+
+    public IEnumerable<ToggleValues> BooleanComboboxValues
+    {
+      get
+      {
+        return Enum.GetValues(typeof(ToggleValues)).Cast<ToggleValues>();
+      }
+    }
+
+    #endregion
+
+
+    #region Constructor
+
+    public ToggleMultipleDataViewModel(List<ToggleModel> input)
+    {
+      Input = input;
+    }
+
+    // Empty constructor for 
+    public ToggleMultipleDataViewModel()
+    {
+
+    }
+
+    #endregion
+
+  }
+}
