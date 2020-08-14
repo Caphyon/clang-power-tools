97c4a3aad10db708918076ac1d23c3bacdfad326
Enache-Ionut <enachestoianionut@gmail.com>
2020-08-14 14:59:53 +0300

    component title and description dependency properties for: font size, foreground, height, width

--- ClangPowerTools/ClangPowerTools/MVVM/Views/Components/ThreePieceButton.xaml.cs
+++ ClangPowerTools/ClangPowerTools/MVVM/Views/Components/ThreePieceButton.xaml.cs
@@ -14,45 +14,7 @@ namespace ClangPowerTools.MVVM.Views.Components
       InitializeComponent();
     }
 
-    public string Image
-    {
-      get { return (string)GetValue(ImageProperty); }
-      set { SetValue(ImageProperty, value); }
-    }
-
-    public static readonly DependencyProperty ImageProperty =
-      DependencyProperty.Register("Image", typeof(string), typeof(ThreePieceButton), new PropertyMetadata(null));
-
-
-    public string Title
-    {
-      get { return (string)GetValue(TitleProperty); }
-      set { SetValue(TitleProperty, value); }
-    }
-
-    public static readonly DependencyProperty TitleProperty =
-      DependencyProperty.Register("Title", typeof(string), typeof(ThreePieceButton), new PropertyMetadata(null));
-
-
-    public string Description
-    {
-      get { return (string)GetValue(DescriptionProperty); }
-      set { SetValue(DescriptionProperty, value); }
-    }
-
-    public static readonly DependencyProperty DescriptionProperty =
-      DependencyProperty.Register("Description", typeof(string), typeof(ThreePieceButton), new PropertyMetadata(null));
-
-
-    public ICommand ButtonCommand
-    {
-      get { return (ICommand)GetValue(ButtonCommandProperty); }
-      set { SetValue(ButtonCommandProperty, value); }
-    }
-
-    public static readonly DependencyProperty ButtonCommandProperty =
-      DependencyProperty.Register("ButtonCommand", typeof(ICommand), typeof(ThreePieceButton), new PropertyMetadata(null));
-
+    #region Component
 
     public string BackgroundValue
     {
@@ -92,6 +54,130 @@ namespace ClangPowerTools.MVVM.Views.Components
 
     public static readonly DependencyProperty HeightValueProperty =
       DependencyProperty.Register("HeightValue", typeof(string), typeof(ThreePieceButton), new PropertyMetadata(null));
+
+    #endregion
+
+
+    #region ImageComponent
+
+    public string Image
+    {
+      get { return (string)GetValue(ImageProperty); }
+      set { SetValue(ImageProperty, value); }
+    }
+
+    public static readonly DependencyProperty ImageProperty =
+      DependencyProperty.Register("Image", typeof(string), typeof(ThreePieceButton), new PropertyMetadata(null));
+
+
+    public string ImageWidthValue
+    {
+      get { return (string)GetValue(ImageWidthValueProperty); }
+      set { SetValue(ImageWidthValueProperty, value); }
+    }
+
+    public static readonly DependencyProperty ImageWidthValueProperty =
+      DependencyProperty.Register("ImageWidthValue", typeof(string), typeof(ThreePieceButton), new PropertyMetadata(null));
+
+
+    public string ImageHeightValue
+    {
+      get { return (string)GetValue(ImageHeightValueProperty); }
+      set { SetValue(ImageHeightValueProperty, value); }
+    }
+
+    public static readonly DependencyProperty ImageHeightValueProperty =
+      DependencyProperty.Register("ImageHeightValue", typeof(string), typeof(ThreePieceButton), new PropertyMetadata(null));
+
+
+    #endregion
+
+
+    #region TitleComponent
+
+
+    public string Title
+    {
+      get { return (string)GetValue(TitleProperty); }
+      set { SetValue(TitleProperty, value); }
+    }
+
+    public static readonly DependencyProperty TitleProperty =
+      DependencyProperty.Register("Title", typeof(string), typeof(ThreePieceButton), new PropertyMetadata(null));
+
+
+    public string TitleFontSize
+    {
+      get { return (string)GetValue(TitleFontSizeProperty); }
+      set { SetValue(TitleFontSizeProperty, value); }
+    }
+
+    public static readonly DependencyProperty TitleFontSizeProperty =
+      DependencyProperty.Register("TitleFontSize", typeof(string), typeof(ThreePieceButton), new PropertyMetadata(null));
+
+
+    public string TitleForeground
+    {
+      get { return (string)GetValue(TitleForegroundProperty); }
+      set { SetValue(TitleForegroundProperty, value); }
+    }
+
+    public static readonly DependencyProperty TitleForegroundProperty =
+      DependencyProperty.Register("TitleForeground", typeof(string), typeof(ThreePieceButton), new PropertyMetadata(null));
+
+
+    #endregion
+
+
+    #region DescriptionComponent
+
+    public string Description
+    {
+      get { return (string)GetValue(DescriptionProperty); }
+      set { SetValue(DescriptionProperty, value); }
+    }
+
+    public static readonly DependencyProperty DescriptionProperty =
+      DependencyProperty.Register("Description", typeof(string), typeof(ThreePieceButton), new PropertyMetadata(null));
+
+
+    public string DescriptionFontSize
+    {
+      get { return (string)GetValue(DescriptionFontSizeProperty); }
+      set { SetValue(DescriptionFontSizeProperty, value); }
+    }
+
+    public static readonly DependencyProperty DescriptionFontSizeProperty =
+      DependencyProperty.Register("DescriptionFontSize", typeof(string), typeof(ThreePieceButton), new PropertyMetadata(null));
+
+
+    public string DescriptionForeground
+    {
+      get { return (string)GetValue(DescriptionForegroundProperty); }
+      set { SetValue(DescriptionForegroundProperty, value); }
+    }
+
+    public static readonly DependencyProperty DescriptionForegroundProperty =
+      DependencyProperty.Register("DescriptionForeground", typeof(string), typeof(ThreePieceButton), new PropertyMetadata(null));
+
+    #endregion
+
+
+    #region Commands
+
+    public ICommand ButtonCommand
+    {
+      get { return (ICommand)GetValue(ButtonCommandProperty); }
+      set { SetValue(ButtonCommandProperty, value); }
+    }
+
+    public static readonly DependencyProperty ButtonCommandProperty =
+      DependencyProperty.Register("ButtonCommand", typeof(ICommand), typeof(ThreePieceButton), new PropertyMetadata(null));
+
+    #endregion
+
+
+
 
   }
 }
