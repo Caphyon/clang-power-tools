1322b06d17ab3a549d83c5fe64b9ddb638d1ea47
Enache-Ionut <enachestoianionut@gmail.com>
2020-08-17 17:52:30 +0300

    basic structure for the new JSON Compilation Database command

---
+++ ClangPowerTools/ClangPowerTools/Commands/JsonCompilationDatabase.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.VisualStudio.Shell;
+using System;
+using System.ComponentModel.Design;
+using System.Windows;
+using Task = System.Threading.Tasks.Task;
+
+namespace ClangPowerTools.Commands
+{
+  /// <summary>
+  /// Command handler
+  /// </summary>
+  internal sealed class JsonCompilationDatabase : BasicCommand
+  {
+    #region Properties
+
+    /// <summary>
+    /// Gets the instance of the command.
+    /// </summary>
+    public static JsonCompilationDatabase Instance
+    {
+      get;
+      private set;
+    }
+
+    #endregion
+
+
+    #region Constructor
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="JsonCompilationDatabase"/> class.
+    /// Adds our command handlers for menu (commands must exist in the command table file)
+    /// </summary>
+    /// <param name="package">Owner package, not null.</param>
+    /// <param name="commandService">Command service to add command to, not null.</param>
+    private JsonCompilationDatabase(OleMenuCommandService aCommandService, CommandController aCommandController,
+      AsyncPackage aPackage, Guid aGuid, int aId)
+        : base(aPackage, aGuid, aId)
+    {
+      if (null != aCommandService)
+      {
+        var menuCommandID = new CommandID(CommandSet, Id);
+        var menuCommand = new OleMenuCommand(aCommandController.Execute, menuCommandID);
+        menuCommand.BeforeQueryStatus += aCommandController.OnBeforeClangCommand;
+        menuCommand.Enabled = true;
+        aCommandService.AddCommand(menuCommand);
+      }
+    }
+
+    #endregion
+
+
+    #region Method
+
+    /// <summary>
+    /// Initializes the singleton instance of the command.
+    /// </summary>
+    /// <param name="package">Owner package, not null.</param>
+    public static async Task InitializeAsync(CommandController aCommandController,
+          AsyncPackage aPackage, Guid aGuid, int aId)
+    {
+      // Switch to the main thread - the call to AddCommand in ClangFormatCommand's constructor requires
+      // the UI thread.
+      await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(aPackage.DisposalToken);
+
+      OleMenuCommandService commandService = await aPackage.GetServiceAsync((typeof(IMenuCommandService))) as OleMenuCommandService;
+      Instance = new JsonCompilationDatabase(commandService, aCommandController, aPackage, aGuid, aId);
+    }
+
+    /// <summary>
+    /// This function is the callback used to execute the command when the menu item is clicked.
+    /// See the constructor to see how the menu item is associated with this function using
+    /// OleMenuCommandService service and MenuCommand class.
+    /// </summary>
+    /// <param name="sender">Event sender.</param>
+    /// <param name="e">Event args.</param>
+    public void Export()
+    {
+      ThreadHelper.ThrowIfNotOnUIThread();
+
+      MessageBox.Show("JSON Compilation Database");
+    }
+
+    #endregion
+
+  }
+}
