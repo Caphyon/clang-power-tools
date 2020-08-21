322048c5000d3693df3857e8fc6c91b6211ae298
horatiu.prica <horatiu.prica@caphyon.com>
2020-08-21 18:14:26 +0300

    Create the temp folders for each file for thread format

--- ClangPowerTools/ClangPowerTools/MVVM/StyleFormatter.cs
+++ ClangPowerTools/ClangPowerTools/MVVM/StyleFormatter.cs
@@ -22,17 +22,28 @@ namespace ClangPowerTools
 
     public string FormatText(string textToFormat, List<IFormatOption> formatStyleOptions, EditorStyles selectedStyle)
     {
-      string filePath = Path.Combine(settingsPathBuilder.GetPath(""), "FormatTemp.cpp");
-      string formatFilePath = Path.Combine(settingsPathBuilder.GetPath(""), ".clang-format");
+      CreatePaths(out string filePath, out string formatFilePath, out string folderPath);
 
       FileSystem.WriteContentToFile(formatFilePath, FormatOptionFile.CreateOutput(formatStyleOptions, selectedStyle).ToString());
       FileSystem.WriteContentToFile(filePath, textToFormat);
 
       var content = FormatFileOutsideProject(settingsPathBuilder.GetPath(""), filePath);
 
-      FileSystem.DeleteFile(filePath);
-      FileSystem.DeleteFile(formatFilePath);
+      FileSystem.DeleteDirectory(folderPath);
       return content;
+    }
+
+
+    private void CreatePaths(out string filePath, out string formatFilePath, out string folderPath)
+    {
+      string parentFolder = Path.Combine(settingsPathBuilder.GetPath(""), "Format");
+      FileSystem.CreateDirectory(parentFolder);
+
+      folderPath = Path.Combine(parentFolder, (Directory.GetDirectories(parentFolder).Length + 1).ToString());
+      FileSystem.CreateDirectory(folderPath);
+
+      filePath = Path.Combine(folderPath.ToString(), "FormatTemp.cpp");
+      formatFilePath = Path.Combine(settingsPathBuilder.GetPath(""), folderPath.ToString(), ".clang-format");
     }
 
     private string FormatFileOutsideProject(string directoryPath, string filePath)
