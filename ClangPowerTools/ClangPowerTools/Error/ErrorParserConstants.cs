0a50ebc5040bf9933302c7e6259f60ce52ed4040
Enache-Ionut <enachestoianionut@gmail.com>
2020-08-24 18:26:32 +0300

    include new regex pattern for matching the path of a JSON file

--- ClangPowerTools/ClangPowerTools/Error/ErrorParserConstants.cs
+++ ClangPowerTools/ClangPowerTools/Error/ErrorParserConstants.cs
@@ -4,14 +4,15 @@
   {
     #region Constants
 
-    public const string kClangTag                     = "Clang Power Tools : ";
-    public const string kCompileClangMissingFromPath  = "'clang++.exe' is not recognized";
-    public const string kTidyClangMissingFromPath     = "'clang-tidy.exe' is not recognized";
-    public const string kMissingLlvmMessage           = "\n\nDid you forget to set-up LLVM?\n\nPlease follow these steps:\n- Go to Clang Power Tools Settings. \n- Select what LLVM version you want to use or install.";
-    public const string kErrorTag                     = "error";
-    public const string kWarningTag                   = "warning";
-    public const string kMessageTag                   = "message";
+    public const string kClangTag = "Clang Power Tools : ";
+    public const string kCompileClangMissingFromPath = "'clang++.exe' is not recognized";
+    public const string kTidyClangMissingFromPath = "'clang-tidy.exe' is not recognized";
+    public const string kMissingLlvmMessage = "\n\nDid you forget to set-up LLVM?\n\nPlease follow these steps:\n- Go to Clang Power Tools Settings. \n- Select what LLVM version you want to use or install.";
+    public const string kErrorTag = "error";
+    public const string kWarningTag = "warning";
+    public const string kMessageTag = "message";
     public const string kErrorMessageRegex = @"(.\:(\\|\/)[ \S+\\\/.]*[c|C|h|H|cpp|CPP|cc|CC|cxx|CXX|c++|C++|cp|CP])(\r\n|\r|\n| |:)*(\d+)(\r\n|\r|\n| |:)*(\d+)(\r\n|\r|\n| |:)*(error|note|warning)[^s](\r\n|\r|\n| |:)*(.*)";
+    public const string kJsonCompilationDbFilePathRegex = @"(.\:(\\|\/)[ \S+\\\/.]*json)";
 
     #endregion
   }
