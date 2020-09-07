d5e3f6bbe7dafa8a5c25ebd3bf0471bd85b8ba3e
Enache-Ionut <enachestoianionut@gmail.com>
2020-09-07 17:48:15 +0300

    new component for adding and removing a list of strings

---
+++ ClangPowerTools/ClangPowerTools/MVVM/Views/Components/InputList.xaml.cs
@@ -0,0 +1,100 @@
+﻿using ClangPowerTools.MVVM.Models;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace ClangPowerTools.MVVM.Views.Components
+{
+  /// <summary>
+  /// Interaction logic for InputList.xaml
+  /// </summary>
+  public partial class InputList : UserControl
+  {
+    #region Constructor
+
+    public InputList()
+    {
+      InitializeComponent();
+    }
+
+    #endregion
+
+
+    #region Properties
+
+    public string InputToAdd
+    {
+      get { return (string)GetValue(InputToAddProperty); }
+      set { SetValue(InputToAddProperty, value); }
+    }
+
+    public static readonly DependencyProperty InputToAddProperty =
+      DependencyProperty.Register("InputToAdd", typeof(string), typeof(InputList), new PropertyMetadata(null));
+
+
+    public ICommand AddCommand
+    {
+      get { return (ICommand)GetValue(AddCommandProperty); }
+      set { SetValue(AddCommandProperty, value); }
+    }
+
+    public static readonly DependencyProperty AddCommandProperty =
+      DependencyProperty.Register("AddCommand", typeof(ICommand), typeof(InputList), new PropertyMetadata(null));
+
+
+    public ObservableCollection<InputDataModel> Collection
+    {
+      get { return (ObservableCollection<InputDataModel>)GetValue(CollectionProperty); }
+      set { SetValue(CollectionProperty, value); }
+    }
+
+    public static readonly DependencyProperty CollectionProperty =
+      DependencyProperty.Register("Collection", typeof(ObservableCollection<InputDataModel>), typeof(InputList), new PropertyMetadata(null));
+
+
+    #region Properties on data model
+
+    public bool ReadOnlyItem
+    {
+      get { return (bool)GetValue(ReadOnlyProperty); }
+      set { SetValue(ReadOnlyProperty, value); }
+    }
+
+    public static readonly DependencyProperty ReadOnlyProperty =
+      DependencyProperty.Register("ReadOnlyItem", typeof(bool), typeof(InputList), new PropertyMetadata(null));
+
+
+    public string Item
+    {
+      get { return (string)GetValue(ItemProperty); }
+      set { SetValue(ItemProperty, value); }
+    }
+
+    public static readonly DependencyProperty ItemProperty =
+      DependencyProperty.Register("Item", typeof(string), typeof(InputList), new PropertyMetadata(null));
+
+    #endregion
+
+    #endregion
+
+
+    #region Methods
+
+    private readonly InputDataViewModel inputDataViewModel = new InputDataViewModel();
+
+    private void DeleteButton(object sender, RoutedEventArgs e)
+    {
+      var elementIndex = GetElementIndex(sender as FrameworkElement);
+      inputDataViewModel.DeleteInput(elementIndex);
+    }
+
+    private int GetElementIndex(FrameworkElement frameworkElement)
+    {
+      var element = frameworkElement.DataContext;
+      return InputsList.Items.IndexOf(element);
+    }
+
+    #endregion
+  }
+}
