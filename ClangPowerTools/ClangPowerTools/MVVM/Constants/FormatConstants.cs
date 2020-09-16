2fbbf02a42215149e8f4d24e47c2adc3fc570635
horatiu.prica <horatiu.prica@caphyon.com>
2020-09-16 15:55:15 +0300

    FontWeight constants

--- ClangPowerTools/ClangPowerTools/MVVM/Constants/FormatConstants.cs
+++ ClangPowerTools/ClangPowerTools/MVVM/Constants/FormatConstants.cs
@@ -3,5 +3,7 @@
   public static class FormatConstants
   {
     public const string FileExtensionsCodeFiles = "Code files (*.c;*.cpp;*.cxx;*.cc;*.tli;*.tlh;*.h;*.hh;*.hpp;*.hxx;)|*.c;*.cpp;*.cxx;*.cc;*.tli;*.tlh;*.h;*.hh;*.hpp;*.hxx";
+    public const string BoldFontWeight = "Bold";
+    public const string NormalFontWeight = "Normal";
   }
 }
