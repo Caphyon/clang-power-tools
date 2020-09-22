f29691ae238743e8d5df11a1e251d465ec39ac81
Enache-Ionut <enachestoianionut@gmail.com>
2020-09-22 14:18:25 +0300

    basic window for file size warning

---
+++ ClangPowerTools/ClangPowerTools/MVVM/Views/FileSizeWarningView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace ClangPowerTools.MVVM.Views
+{
+  /// <summary>
+  /// Interaction logic for FileSizeWarningView.xaml
+  /// </summary>
+  public partial class FileSizeWarningView : Window
+  {
+    public FileSizeWarningView()
+    {
+      InitializeComponent();
+    }
+  }
+}
