416032b65adaca75dacfc38e99839a6cf9f1e26b
Enache-Ionut <enachestoianionut@gmail.com>
2020-09-24 10:57:05 +0300

    basic window creation for format style detection info

---
+++ ClangPowerTools/ClangPowerTools/MVVM/Views/DetectedFormatStyleInfo.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace ClangPowerTools.MVVM.Views
+{
+  /// <summary>
+  /// Interaction logic for DetectedFormatStyleInfo.xaml
+  /// </summary>
+  public partial class DetectedFormatStyleInfo : Window
+  {
+    public DetectedFormatStyleInfo()
+    {
+      InitializeComponent();
+    }
+  }
+}
