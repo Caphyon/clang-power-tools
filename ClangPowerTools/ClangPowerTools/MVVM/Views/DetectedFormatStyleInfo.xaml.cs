0b1557a4c91360ba1efc5a57924c635bb2054503
Enache-Ionut <enachestoianionut@gmail.com>
2020-09-24 11:28:30 +0300

    disable the diff window when info window is displayed and enable diff window after the info window is closed

--- ClangPowerTools/ClangPowerTools/MVVM/Views/DetectedFormatStyleInfo.xaml.cs
+++ ClangPowerTools/ClangPowerTools/MVVM/Views/DetectedFormatStyleInfo.xaml.cs
@@ -7,14 +7,24 @@ namespace ClangPowerTools.MVVM.Views
   /// </summary>
   public partial class DetectedFormatStyleInfo : Window
   {
-    public DetectedFormatStyleInfo()
+    private readonly DiffWindow owner;
+
+    public DetectedFormatStyleInfo(DiffWindow view)
     {
       InitializeComponent();
+      owner = view;
+      owner.IsEnabled = false;
     }
 
     private void ContinueButton_Click(object sender, RoutedEventArgs e)
     {
+      owner.IsEnabled = true;
       this.Close();
+    }
+
+    private void Window_Closed(object sender, System.EventArgs e)
+    {
+      owner.IsEnabled = true;
     }
   }
 }
