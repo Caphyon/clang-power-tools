aa59c870f21b19dd77364636ca93fb3fee009fb1
horatiu.prica <horatiu.prica@caphyon.com>
2020-09-29 12:41:07 +0300

    Add the DetectedStyleInfoViewModel

---
+++ ClangPowerTools/ClangPowerTools/MVVM/ViewModels/DetectedStyleInfoViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+
+namespace ClangPowerTools
+{
+  public class DetectedStyleInfoViewModel : INotifyPropertyChanged
+  {
+    #region Members
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    #endregion
+
+
+    #region Properties
+
+    public string DetectedOptions { get; set; }
+
+    #endregion
+
+    #region Private Methods
+
+    private void OnPropertyChanged(string propertyName)
+    {
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    #endregion
+  }
+}
