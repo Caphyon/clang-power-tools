086900f55680283ed01445476e633b328e5c6dad
Enache-Ionut <enachestoianionut@gmail.com>
2020-09-30 16:42:38 +0300

    selected files model

---
+++ ClangPowerTools/ClangPowerTools/MVVM/Models/SelectedFileModel.cs
@@ -0,0 +1,64 @@
+﻿using System.ComponentModel;
+using System.IO;
+
+namespace ClangPowerTools.MVVM.Models
+{
+  public class SelectedFileModel : INotifyPropertyChanged
+  {
+    #region Members
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    private bool isReadOnly = false;
+    private string filePath = string.Empty;
+
+    #endregion
+
+
+    #region Constructor
+
+    public SelectedFileModel(string path)
+    {
+      if (!File.Exists(FilePath))
+        return;
+
+      FilePath = path;
+      FileSize = new FileInfo(FilePath).Length;
+    }
+
+    #endregion
+
+
+    #region Properties
+
+    public string FilePath
+    {
+      get
+      {
+        return filePath;
+      }
+      set
+      {
+        filePath = value;
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("FilePath"));
+      }
+    }
+
+    public long FileSize { get; private set; } = 0;
+
+    public bool IsReadOnly
+    {
+      get
+      {
+        return isReadOnly;
+      }
+      set
+      {
+        isReadOnly = value;
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("CanEdit"));
+      }
+    }
+
+
+    #endregion
+  }
+}
