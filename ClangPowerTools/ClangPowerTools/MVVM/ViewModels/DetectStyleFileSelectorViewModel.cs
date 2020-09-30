4ce7613234c82b3abf18cd00b0d1b186f9a94836
Enache-Ionut <enachestoianionut@gmail.com>
2020-09-30 16:46:31 +0300

    select files for style detection logic in view model

--- ClangPowerTools/ClangPowerTools/MVVM/ViewModels/DetectStyleFileSelectorViewModel.cs
+++ ClangPowerTools/ClangPowerTools/MVVM/ViewModels/DetectStyleFileSelectorViewModel.cs
@@ -1,278 +1,130 @@
-﻿//using ClangPowerTools.MVVM.Views;
-//using System.Collections.Generic;
-//using System.ComponentModel;
-//using System.Linq;
-//using System.Threading.Tasks;
+﻿using ClangPowerTools.MVVM.Commands;
+using ClangPowerTools.MVVM.Models;
+using ClangPowerTools.MVVM.Views;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
 
-//namespace ClangPowerTools.MVVM.ViewModels
-//{
-//  public class DetectStyleFileSelectorViewModel
-//  {
-//    #region Members
+namespace ClangPowerTools.MVVM.ViewModels
+{
+  public class DetectStyleFileSelectorViewModel : CommonSettingsFunctionality
+  {
+    #region Members
 
-//    public event PropertyChangedEventHandler PropertyChanged;
+    private readonly DetectStyleFileSelectorView view;
 
-//    private string inputToAdd;
-//    private string placeholder;
-//    private InputDataView inputDataView;
-//    private ICommand addCommand;
-//    private ICommand clearCommand;
-//    private ICommand detectFormatStyleCommand;
-//    private string warningText;
+    private ICommand browseCommand;
+    private ICommand detectFormatStyleCommand;
 
+    long totalFilesSize = 0;
+    private const long MAX_FILE_SIZE = 1500000; // 1.5 MB
 
-
-//    #endregion
-
-
-//    #region Constructor
-
-//    public InputDataViewModel(string content)
-//    {
-//      CreateInputsCollection(content);
-//    }
-
-//    public InputDataViewModel() { }
-
-//    public InputDataViewModel(DetectStyleFileSelectorView view, bool browse)
-//    {
-//      this.view = view;
-//      BrowseForFiles = browse;
-//      view.InputList.ClearButton.IsEnabled = false;
-//      view.DetectFormatStyleButton.IsEnabled = false;
-//    }
-
-//    #endregion
+    #endregion
 
 
-//    #region Properties
+    #region Constructor
 
-//    private bool BrowseForFiles { get; set; } = false;
+    public DetectStyleFileSelectorViewModel() { }
 
-//    public string InputToAdd
-//    {
-//      get
-//      {
-//        return inputToAdd;
-//      }
-//      set
-//      {
-//        inputToAdd = value;
-//        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("InputToAdd"));
-//      }
-//    }
+    public DetectStyleFileSelectorViewModel(DetectStyleFileSelectorView view)
+    {
+      this.view = view;
+      view.DetectFormatStyleButton.IsEnabled = false;
+    }
 
-//    public string Placeholder
-//    {
-//      get
-//      {
-//        return placeholder;
-//      }
-//      set
-//      {
-//        placeholder = value;
-//        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Placeholder"));
-//      }
-//    }
-
-//    public string WarningText
-//    {
-//      get
-//      {
-//        return warningText;
-//      }
-//      set
-//      {
-//        warningText = value;
-//        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("WarningText"));
-//      }
-//    }
-
-//    public ObservableCollection<InputDataModel> Inputs { get; set; } = new ObservableCollection<InputDataModel>();
-
-//    public ICommand AddCommand
-//    {
-//      get => addCommand ?? (addCommand = new RelayCommand(() => AddInput(), () => CanExecute));
-//    }
-//    public ICommand ClearCommand
-//    {
-//      get => clearCommand ?? (clearCommand = new RelayCommand(() => ClearList(), () => CanExecute));
-//    }
-
-//    public ICommand DetectFormatStyleCommand
-//    {
-//      get => detectFormatStyleCommand ?? (detectFormatStyleCommand = new RelayCommand(() => DetectFormatStyleAsync().SafeFireAndForget(), () => CanExecute));
-//    }
-
-//    public bool CanExecute
-//    {
-//      get
-//      {
-//        return true;
-//      }
-//    }
-
-//    #endregion
+    #endregion
 
 
-//    #region Methods
+    #region Properties
 
-//    public void ShowViewDialog()
-//    {
-//      inputDataView = new InputDataView(this);
-//      inputDataView.ShowDialog();
-//    }
+    public ObservableCollection<SelectedFileModel> SelectedFiles { get; set; } = new ObservableCollection<SelectedFileModel>();
 
-//    public void DeleteInput(int index)
-//    {
-//      if (index < 0 || index >= Inputs.Count)
-//        return;
+    public ICommand Browse
+    {
+      get => browseCommand ?? (browseCommand = new RelayCommand(() => BrowseForFiles(), () => CanExecute));
+    }
 
-//      var model = Inputs[index];
-//      totalFilesSize -= model.FileSize;
+    public ICommand DetectFormatStyleCommand
+    {
+      get => detectFormatStyleCommand ?? (detectFormatStyleCommand = new RelayCommand(() => DetectFormatStyleAsync().SafeFireAndForget(), () => CanExecute));
+    }
 
-//      Inputs.RemoveAt(index);
+    public bool CanExecute
+    {
+      get
+      {
+        return true;
+      }
+    }
 
-//      // Is not the last element update all the elements position from the index to the end
-//      if (index != Inputs.Count)
-//      {
-//        for (int position = index; position < Inputs.Count; ++position)
-//          Inputs[position].LineNumber = position + 1;
-//      }
-
-//      if (Inputs.Count == 0)
-//      {
-//        view.InputList.ClearButton.IsEnabled = false;
-//        view.DetectFormatStyleButton.IsEnabled = false;
-//      }
-
-//      if (view == null)
-//        return;
-
-//      if (totalFilesSize <= MAX_FILE_SIZE)
-//        view.WarningTextBox.Visibility = System.Windows.Visibility.Hidden;
-//    }
-
-//    private void AddInput()
-//    {
-//      if (BrowseForFiles && string.IsNullOrWhiteSpace(inputToAdd))
-//        AddBrowseFilePathsToCollection();
-//      else
-//        AddInputToCollection();
-
-//      if (Inputs.Count > 0)
-//      {
-//        view.InputList.ClearButton.IsEnabled = true;
-//        view.DetectFormatStyleButton.IsEnabled = true;
-//      }
-
-//      if (view == null)
-//        return;
-
-//      if (totalFilesSize > MAX_FILE_SIZE)
-//      {
-//        WarningText = MAX_FILE_WARNING;
-//        view.WarningTextBox.Foreground = Brushes.Orange;
-//        view.WarningTextBox.Visibility = System.Windows.Visibility.Visible;
-
-//        var warning = new FileSizeWarningView(view);
-//        warning.Show();
-//      }
-//    }
-
-//    private void AddBrowseFilePathsToCollection()
-//    {
-//      var filePaths = OpenFiles(string.Empty, ".cpp", ScriptConstants.FileExtensionsSelectFile);
-
-//      if (filePaths == null || filePaths.Length <= 0)
-//        return;
-
-//      for (int index = 0; index < filePaths.Length; ++index)
-//      {
-//        if (IsDuplicate(filePaths[index]))
-//          continue;
-
-//        int position = Inputs.Count == 0 ? 1 : Inputs.Last().LineNumber + 1;
-//        AddNewElement(filePaths[index], position);
-//      }
-
-//      if (view.ValidFilePathTextBlock.Visibility == System.Windows.Visibility.Visible)
-//      {
-//        view.ValidFilePathTextBlock.Visibility = System.Windows.Visibility.Hidden;
-//      }
-//    }
-
-//    private void AddInputToCollection()
-//    {
-//      if (string.IsNullOrWhiteSpace(inputToAdd))
-//        return;
-
-//      if (IsDuplicate(inputToAdd))
-//        return;
-
-//      if (!File.Exists(inputToAdd))
-//      {
-//        view.ValidFilePathTextBlock.Visibility = System.Windows.Visibility.Visible;
-//        return;
-//      }
-
-//      var extension = Path.GetExtension(inputToAdd);
-//      if (string.IsNullOrWhiteSpace(extension) || !ScriptConstants.kAcceptedFileExtensions.Contains(extension))
-//      {
-//        view.ValidFilePathTextBlock.Visibility = System.Windows.Visibility.Visible;
-//        return;
-//      }
-
-//      if (view.ValidFilePathTextBlock.Visibility == System.Windows.Visibility.Visible)
-//      {
-//        view.ValidFilePathTextBlock.Visibility = System.Windows.Visibility.Hidden;
-//      }
-
-//      int index = Inputs.Count == 0 ? 1 : Inputs.Last().LineNumber + 1;
-//      AddNewElement(inputToAdd, index);
-//      InputToAdd = string.Empty;
-//    }
-
-//    private bool IsDuplicate(string filePath) => Inputs.Where(model => model.InputData == filePath).Count() > 0;
-
-//    private void CreateInputsCollection(string content)
-//    {
-//      if (string.IsNullOrWhiteSpace(content))
-//        return;
-
-//      var splitContent = content.Split(';').ToList();
-//      for (int index = 0; index < splitContent.Count; ++index)
-//        AddNewElement(splitContent[index], index + 1);
-//    }
-
-//    private void AddNewElement(string filePath, int index)
-//    {
-//      var model = new InputDataModel(filePath, index);
-//      totalFilesSize += model.FileSize;
-//      Inputs.Add(model);
-//    }
-
-//    private void ClearList()
-//    {
-//      Inputs.Clear();
-
-//      view.InputList.ClearButton.IsEnabled = false;
-//      view.DetectFormatStyleButton.IsEnabled = false;
-
-//      if (view != null)
-//        view.WarningTextBox.Visibility = System.Windows.Visibility.Hidden;
-//    }
-
-//    private async Task DetectFormatStyleAsync()
-//    {
-//      var diffWin = new DiffWindow();
-//      List<string> filesPath = Inputs.Select(model => model.InputData).ToList();
-
-//      view.IsEnabled = false;
-//      await diffWin.ShowDiffAsync(filesPath, view);
-//      view.IsEnabled = true;
-//    }
+    #endregion
 
 
-//  }
-//}
+    #region Methods
+
+    private void BrowseForFiles()
+    {
+      string[] filePaths = OpenFiles(string.Empty, ".cpp", ScriptConstants.FileExtensionsSelectFile);
+
+      if (filePaths == null || filePaths.Length <= 0)
+        return;
+
+      for (int index = 0; index < filePaths.Length; ++index)
+      {
+        if (IsDuplicate(filePaths[index]))
+          continue;
+
+        AddNewElement(filePaths[index]);
+      }
+
+      view.DetectFormatStyleButton.IsEnabled = SelectedFiles.Count > 0;
+
+      if (totalFilesSize > MAX_FILE_SIZE)
+      {
+        view.WarningTextBox.Visibility = System.Windows.Visibility.Visible;
+        var warning = new FileSizeWarningView(view);
+        warning.Show();
+      }
+    }
+
+    private bool IsDuplicate(string filePath) => SelectedFiles.FirstOrDefault(model => model.FilePath == filePath) != null;
+
+    private void AddNewElement(string filePath)
+    {
+      var model = new SelectedFileModel(filePath);
+      totalFilesSize += model.FileSize;
+      SelectedFiles.Add(model);
+    }
+
+
+    public void DeleteInput(int index)
+    {
+      if (index < 0 || index >= SelectedFiles.Count)
+        return;
+
+      SelectedFileModel model = SelectedFiles[index];
+      totalFilesSize -= model.FileSize;
+      SelectedFiles.RemoveAt(index);
+
+      view.DetectFormatStyleButton.IsEnabled = SelectedFiles.Count == 0;
+
+      if (totalFilesSize <= MAX_FILE_SIZE)
+        view.WarningTextBox.Visibility = System.Windows.Visibility.Hidden;
+    }
+
+    private async Task DetectFormatStyleAsync()
+    {
+      var diffWin = new DiffWindow();
+      List<string> filesPath = SelectedFiles.Select(model => model.FilePath).ToList();
+
+      view.IsEnabled = false;
+      await diffWin.ShowDiffAsync(filesPath, view);
+      view.IsEnabled = true;
+    }
+
+    #endregion
+
+  }
+}
