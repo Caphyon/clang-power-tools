f79652c885ec93c61606786b3585c7310a3b60b9
horatiu.prica <horatiu.prica@caphyon.com>
2020-11-23 18:55:38 +0200

    Create the Feedback view and ViewModel

--- ClangPowerTools/ClangPowerTools/MVVM/ViewModels/FeedbackViewModel.cs
+++ ClangPowerTools/ClangPowerTools/MVVM/ViewModels/FeedbackViewModel.cs
@@ -1,13 +1,56 @@
-﻿using System.ComponentModel;
+﻿using ClangPowerTools.MVVM.Commands;
+using System.Diagnostics;
+using System.Windows.Input;
 
 namespace ClangPowerTools
 {
-  public class FeedbackViewModel : INotifyPropertyChanged
+  public class FeedbackViewModel
   {
 
     #region Members
 
-    public event PropertyChangedEventHandler PropertyChanged;
+    private ICommand githubCommand;
+    private ICommand websiteCommand;
+
+    #endregion
+
+    #region Properties
+
+    public bool CanExecute
+    {
+      get
+      {
+        return true;
+      }
+    }
+
+    #endregion
+
+    #region Commands
+
+    public ICommand GithubCommand
+    {
+      get => githubCommand ??= new RelayCommand(() => OpenGitHubFeedback(), () => CanExecute);
+    }
+
+    public ICommand WebsiteCommand
+    {
+      get => websiteCommand ??= new RelayCommand(() => OpenWebsiteFeedback(), () => CanExecute);
+    }
+
+    #endregion
+
+    #region Methods
+
+    private void OpenGitHubFeedback()
+    {
+      Process.Start(new ProcessStartInfo("https://github.com/Caphyon/clang-power-tools/issues/new"));
+    }
+
+    private void OpenWebsiteFeedback()
+    {
+      Process.Start(new ProcessStartInfo("https://clangpowertools.com/contact.html"));
+    }
 
     #endregion
   }
