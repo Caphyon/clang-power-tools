ef1777e622b3c64ccc5361cf688fbc159cd468dc
Enache-Ionut <enachestoianionut@gmail.com>
2020-11-23 19:51:59 +0200

    cmake warning for beta feture

---
+++ ClangPowerTools/ClangPowerTools/MVVM/Views/CMakeBetaWarning.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace ClangPowerTools.MVVM.Views
+{
+  /// <summary>
+  /// Interaction logic for CMakeBetaWarning.xaml
+  /// </summary>
+  public partial class CMakeBetaWarning : Window
+  {
+    public CMakeBetaWarning()
+    {
+      InitializeComponent();
+    }
+  }
+}
