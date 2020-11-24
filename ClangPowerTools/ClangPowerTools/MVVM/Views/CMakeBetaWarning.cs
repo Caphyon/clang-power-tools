6e58afab2964c1f05a3abfca14f114171d47b81c
Enache-Ionut <enachestoianionut@gmail.com>
2020-11-24 13:42:20 +0200

    include checkbox in the cmake warning window

--- ClangPowerTools/ClangPowerTools/MVVM/Views/CMakeBetaWarning.cs
+++ ClangPowerTools/ClangPowerTools/MVVM/Views/CMakeBetaWarning.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using ClangPowerTools.Helpers;
+using System.Windows;
 
 namespace ClangPowerTools.MVVM.Views
 {
@@ -7,6 +8,9 @@ namespace ClangPowerTools.MVVM.Views
   /// </summary>
   public partial class CMakeBetaWarning : Window
   {
+    private readonly string registryName = @"Software\Caphyon\Clang Power Tools";
+    private readonly string keyName = "CMakeBetaWarning";
+
     public CMakeBetaWarning()
     {
       InitializeComponent();
@@ -15,6 +19,18 @@ namespace ClangPowerTools.MVVM.Views
     private void ContinueButton_Click(object sender, RoutedEventArgs e)
     {
       this.Close();
+    }
+
+    private void CheckBox_Checked(object sender, RoutedEventArgs e)
+    {
+      var registryUtility = new RegistryUtility(registryName);
+      registryUtility.WriteCurrentUserKey(keyName, "DoNotShow");
+    }
+
+    private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+    {
+      var registryUtility = new RegistryUtility(registryName);
+      registryUtility.DeleteCurrentUserKey(keyName);
     }
   }
 }
