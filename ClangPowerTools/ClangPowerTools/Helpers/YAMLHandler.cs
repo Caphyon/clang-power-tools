62e37a9dbbfe30f0eb06d4112cc22c5eb3c07f05
horatiu.prica <horatiu.prica@caphyon.com>
2021-01-15 18:21:45 +0200

    Working monolith method to read YAML

--- ClangPowerTools/ClangPowerTools/Helpers/YAMLHandler.cs
+++ ClangPowerTools/ClangPowerTools/Helpers/YAMLHandler.cs
@@ -1,5 +1,7 @@
 ﻿using ClangPowerTools.MVVM.Models;
+using System;
 using System.IO;
+using System.Text;
 using System.Threading.Tasks;
 using YamlDotNet.RepresentationModel;
 
@@ -7,41 +9,78 @@ namespace ClangPowerTools
 {
   public class YAMLHandler
   {
+    //TODO parsing should be handled by a different class
     public async static Task ReadYAMLAsync(string path)
     {
       using var reader = new StreamReader(path);
       var input = await reader.ReadToEndAsync();
 
+      //TODO read directly in yaml LOAD
       var yaml = new YamlStream();
       yaml.Load(new StringReader(input));
-      var mapping = (YamlMappingNode)yaml.Documents[0].RootNode;
 
-      var formatOptions = FormatOptionsAllData.FormatOptions;
+      var mapping = (YamlMappingNode)yaml.Documents[0].RootNode;
       foreach (var entry in mapping.Children)
       {
-        var option = new FormatOptionModel();
-        if (formatOptions.TryGetValue(entry.Key.ToString(), out option))
+        if (FormatOptionsAllData.FormatOptions.TryGetValue(entry.Key.ToString(), out FormatOptionModel option))
         {
-          var test = option;
+          switch (option)
+          {
+            case FormatOptionToggleModel toggleModel:
+              Enum.TryParse(entry.Value.ToString(), out ToggleValues value);
+              toggleModel.BooleanCombobox = value;
+              break;
+
+            case FormatOptionInputModel inputModel:
+              inputModel.Input = entry.Value.ToString();
+              break;
+
+            case FormatOptionMultipleToggleModel multipleToggleModel:
+              var sequenceToggleNode = (YamlMappingNode)mapping.Children[new YamlScalarNode(option.Name)];
+              foreach (var node in sequenceToggleNode)
+              {
+                foreach (var item in multipleToggleModel.ToggleFlags)
+                {
+                  if (item.Name == node.Key.ToString())
+                  {
+                    Enum.TryParse(entry.Value.ToString(), out ToggleValues toogleValue);
+                    item.Value = toogleValue;
+                    break;
+                  }
+                }
+              }
+              break;
+
+            case FormatOptionMultipleInputModel multipleInputModel:
+              var sequenceInputNode = (YamlSequenceNode)mapping.Children[new YamlScalarNode(option.Name)];
+              var sb = new StringBuilder();
+              sb.AppendLine(string.Concat(option.Name, ":"));
+              foreach (var node in sequenceInputNode)
+              {
+                switch (node.NodeType)
+                {
+                  case YamlNodeType.Mapping:
+                    var mappingNode = (YamlMappingNode)node;
+                    sb.Append("  - ");
+                    foreach (var test in mappingNode.Children)
+                    {
+                      sb.AppendLine(string.Concat("    ", test.Key.ToString(), ": ", test.Value.ToString()));
+                    }
+                    break;
+                  case YamlNodeType.Scalar:
+                    sb.AppendLine(string.Concat("  - " + ((YamlScalarNode)node).Value));
+                    break;
+                  default:
+                    break;
+                }
+              }
+              multipleInputModel.MultipleInput = sb.ToString();
+              break;
+            default:
+              break;
+          }
         }
-
-        //output.WriteLine(((YamlScalarNode)entry.Key).Value);
       }
-
-      // List all the items
-      //var items = (YamlSequenceNode)mapping.Children[new YamlScalarNode("items")];
-      //foreach (YamlMappingNode item in items)
-      //{
-      //  output.WriteLine(
-      //      "{0}\t{1}",
-      //      item.Children[new YamlScalarNode("part_no")],
-      //      item.Children[new YamlScalarNode("descrip")]
-      //  );
-      //}
-
     }
-
-
-
   }
 }
