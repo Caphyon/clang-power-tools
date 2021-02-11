0fc07c352bb1c8e51a38fb99aed42968fbdf2b69
horatiu.prica <horatiu.prica@caphyon.com>
2021-02-11 17:26:28 +0200

    Remove unused constants from FormatEditorConstants

--- ClangPowerTools/ClangPowerTools/MVVM/Constants/FormatEditorConstants.cs
+++ ClangPowerTools/ClangPowerTools/MVVM/Constants/FormatEditorConstants.cs
@@ -4,10 +4,8 @@
   {
     public const string ExecutableName = "ClangFormatEditor.exe";
     public const string ClangFormatDetectorFolder = "ClangFormatEditor";
-    public const string CommandLineExe = "cmd.exe";
     public const string UpdaterParameter = " VisualStudio";
     public const string ClangFormatEditor = "Clang Format Editor";
-    public const string CommandLineArgument = "/C";
     public const string FrameworkdUrlDownload = @"https://dotnet.microsoft.com/download/dotnet/thank-you/runtime-desktop-5.0.2-windows-x86-installer";
     public const string FrameworkPath = @"C:\Program Files (x86)\dotnet\shared\Microsoft.WindowsDesktop.App";
     public const string ClangFormatExe = "clang-format.exe";
