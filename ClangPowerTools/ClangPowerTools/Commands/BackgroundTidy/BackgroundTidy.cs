7d2850f7226faee7d068c7508e312a03a41a0d8b
horatiu.prica <horatiu.prica@caphyon.com>
2021-06-28 12:15:45 +0300

    Add vs 2022 to the version list

--- ClangPowerTools/ClangPowerTools/Commands/BackgroundTidy/BackgroundTidy.cs
+++ ClangPowerTools/ClangPowerTools/Commands/BackgroundTidy/BackgroundTidy.cs
@@ -23,7 +23,8 @@ namespace ClangPowerTools.Commands.BackgroundTidy
       {"13.0", "2013"},
       {"14.0", "2015"},
       {"15.0", "2017"},
-      {"16.0", "2019"}
+      {"16.0", "2019"},
+      {"17.0", "2022"}
     };
 
     #endregion
