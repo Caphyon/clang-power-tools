3f928af3e8f47e8db491203d1e3e19e49a2afb1c
horatiu.prica <horatiu.prica@caphyon.com>
2021-07-23 19:47:39 +0300

    Add the Tooling folder to the shared project

---
+++ ClangPowerTools/ClangPowerToolsShared/Tooling/v1/psClang/~advinst.tests.ps1
@@ -0,0 +1,20 @@
+. "$PsScriptRoot\io.ps1"
+Describe "ai" {
+  It "Should build Advanced Installer" {
+
+   [string] $advinstRepo = $env:ADVINST
+
+   if ($advinstRepo)
+   {
+    Push-Location $advinstRepo
+
+    [string] $scriptLocation = Canonize-Path -base "$PSScriptRoot" -child "..\clang-build.ps1"
+    &"$scriptLocation" 2>&1 | Out-Default
+    [int] $exitCode = $LASTEXITCODE
+    Pop-Location
+
+    Write-Output "$PSScriptRoot"
+    $exitCode | Should -BeExactly 0
+   }
+  }
+}
