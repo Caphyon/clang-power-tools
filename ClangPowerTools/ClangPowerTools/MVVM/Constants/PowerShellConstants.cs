8cf444831ab1e3687526487439f5b54384f2a550
horatiu.prica <horatiu.prica@caphyon.com>
2021-07-29 16:52:01 +0300

    PowerShell constants and service

---
+++ ClangPowerTools/ClangPowerTools/MVVM/Constants/PowerShellConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace ClangPowerTools.MVVM.Constants
+{
+  public static class PowerShellConstants
+  {
+    public const string FolderName = "PowerShellScripts";
+    public const string ClangBuildScriptName = "clang-build.ps1";
+    public const string ClangBuildScriptUri = @"https://raw.githubusercontent.com/Caphyon/clang-power-tools/master/ClangPowerTools/ClangPowerTools/Tooling/v1/clang-build.ps1";
+  }
+}
---
+++ ClangPowerTools/ClangPowerTools/Services/PowerShellService.cs
@@ -0,0 +1,81 @@
+﻿using ClangPowerTools.MVVM.Constants;
+using System;
+using System.ComponentModel;
+using System.Net;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ClangPowerTools.Services
+{
+  public class PowerShellService
+  {
+    #region Members
+
+    public CancellationTokenSource downloadCancellationToken = new();
+
+    private readonly SettingsPathBuilder settingsPathBuilder = new();
+
+    #endregion
+
+
+    #region Public Methods
+
+    public void UpdateScripts()
+    {
+      DownloadScript();
+    }
+
+    #endregion
+
+
+    #region Private Methods
+
+    private void DownloadScript(string fileUri, string fileName)
+    {
+      var appDataFolder = settingsPathBuilder.GetPath("clang-build.ps1");
+      try
+      {
+        using WebClient client = new();
+        client.DownloadFileCompleted += DownloadCompleted;
+        downloadCancellationToken.Token.Register(client.CancelAsync);
+        client.DownloadFileAsync(new Uri(PowerShellConstants.ClangBuildScriptUri), appDataFolder);
+      }
+      catch (Exception)
+      {
+        DownloadCanceled();
+      }
+    }
+
+    private void DownloadCanceled()
+    {
+      //OnOperationCanceldEvent();
+
+      MessageBox.Show("The download process has stopped.", "PowerShell script udpate", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+    }
+
+    private void DownloadCompleted(object sender, AsyncCompletedEventArgs e)
+    {
+      if (downloadCancellationToken.IsCancellationRequested)
+      {
+        DownloadCanceled();
+      }
+      else
+      {
+        ReplaceScripts();
+      }
+    }
+
+
+    private void ReplaceScripts()
+    {
+
+    }
+
+    private void ClearFilesAfterReplace()
+    {
+
+    }
+
+    #endregion
+  }
+}
