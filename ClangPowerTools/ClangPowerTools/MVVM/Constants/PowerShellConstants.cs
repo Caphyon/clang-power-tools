4221df2a723d170fa262a3e406cc368da12ce75a
horatiu.prica <horatiu.prica@caphyon.com>
2021-07-30 16:43:44 +0300

    Rename PowerShell constants file

--- ClangPowerTools/ClangPowerTools/MVVM/Constants/PowerShellConstants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ClangPowerTools.MVVM.Constants
-{
-  public static class PowerShellConstants
-  {
-    public const string FolderName = "PowerShellScripts";
-    public const string ClangBuildScriptName = "clang-build.ps1";
-    public const string ClangBuildScriptUri = @"https://raw.githubusercontent.com/Caphyon/clang-power-tools/master/ClangPowerTools/ClangPowerTools/Tooling/v1/clang-build.ps1";
-  }
-}
