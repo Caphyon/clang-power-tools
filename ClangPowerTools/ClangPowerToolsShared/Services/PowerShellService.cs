995bff2ecd6eef71af201d4b082df677b1642f5b
horatiu.prica <horatiu.prica@caphyon.com>
2021-08-10 15:21:33 +0300

    Update files from the release branch

---
+++ ClangPowerTools/ClangPowerToolsShared/Services/PowerShellService.cs
@@ -0,0 +1,103 @@
+﻿using ClangPowerTools.Helpers;
+using ClangPowerTools.MVVM.Constants;
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClangPowerTools.Services
+{
+  public class PowerShellService
+  {
+    #region Members
+
+    public CancellationTokenSource downloadCancellationToken = new();
+
+    private readonly SettingsPathBuilder settingsPathBuilder = new();
+
+    #endregion
+
+
+    #region Public Methods
+
+    public async Task UpdateScriptsAsync()
+    {
+      string scriptUri = PsUpdaterConstants.GitHubUri + PsUpdaterConstants.ClangBuildScript;
+      await DownloadScriptAsync(scriptUri, PsUpdaterConstants.ClangBuildScript);
+
+      var sb = new StringBuilder();
+      foreach (string script in PsUpdaterConstants.ScriptsInPsClangFolder)
+      {
+        sb.Append(PsUpdaterConstants.GitHubUri).Append(PsUpdaterConstants.PsClangFolder).Append("/").Append(script);
+        await DownloadScriptAsync(sb.ToString(), script);
+        sb.Clear();
+      }
+
+      ReplaceScripts();
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    private async Task DownloadScriptAsync(string fileUri, string fileName)
+    {
+      string scriptsDirectory = settingsPathBuilder.GetPath(PsUpdaterConstants.PowerShellScriptsFolder);
+      string scriptFullName = Path.Combine(scriptsDirectory, fileName);
+      try
+      {
+        FileSystem.CreateDirectory(scriptsDirectory);
+        using WebClient client = new();
+        await client.DownloadFileTaskAsync(new Uri(fileUri), scriptFullName);
+      }
+      catch (Exception e)
+      {
+        MessageBox.Show(e.Message, "PowerShell Scripts", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+      }
+    }
+
+    private void ReplaceScripts()
+    {
+      string destFolder = Path.GetDirectoryName(settingsPathBuilder.GetAssemblyLocalPath());
+      destFolder = Path.Combine(destFolder, PsUpdaterConstants.ToolingFolder, PsUpdaterConstants.V1Folder);
+      string sourceFolder = settingsPathBuilder.GetPath(PsUpdaterConstants.PowerShellScriptsFolder);
+
+      try
+      {
+        FileSystem.MoveFile(Path.Combine(sourceFolder, PsUpdaterConstants.ClangBuildScript),
+                            Path.Combine(destFolder, PsUpdaterConstants.ClangBuildScript));
+
+        destFolder = Path.Combine(destFolder, PsUpdaterConstants.PsClangFolder);
+        foreach (string script in PsUpdaterConstants.ScriptsInPsClangFolder)
+        {
+          FileSystem.MoveFile(Path.Combine(sourceFolder, script), Path.Combine(destFolder, script));
+        }
+      }
+      catch (Exception e)
+      {
+        MessageBox.Show(e.Message, "PowerShell Scripts", MessageBoxButtons.OK, MessageBoxIcon.Error);
+      }
+
+      ClearFilesAfterReplace();
+      MessageBox.Show("PowerShell scripts were updated to the latest version.", "PowerShell Scripts", MessageBoxButtons.OK, MessageBoxIcon.Information);
+    }
+
+    private void ClearFilesAfterReplace()
+    {
+      string scriptsDirectory = settingsPathBuilder.GetPath(PsUpdaterConstants.PowerShellScriptsFolder);
+      try
+      {
+        FileSystem.DeleteDirectory(scriptsDirectory);
+      }
+      catch (Exception e)
+      {
+        MessageBox.Show(e.Message, "PowerShell Scripts", MessageBoxButtons.OK, MessageBoxIcon.Error);
+      }
+    }
+
+    #endregion
+  }
+}
