ed352fa75f5534374dd9bee7066d6a2f6cf7bc7c
horatiu.prica <horatiu.prica@caphyon.com>
2021-08-19 17:14:04 +0300

    BrowserView component

---
+++ ClangPowerTools/ClangPowerToolsShared/MVVM/Views/BrowserView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace ClangPowerTools.MVVM.Views
+{
+  /// <summary>
+  /// Interaction logic for Browser.xaml
+  /// </summary>
+  public partial class BrowserView : Window
+  {
+    public BrowserView()
+    {
+      InitializeComponent();
+      DataContext = new BrowserViewModel();
+    }
+  }
+}
