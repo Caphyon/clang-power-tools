75b27db65b99da456879384901fd949ca5bca3a8
DESKTOP-503D7S8\Marina Rusu <marina.rusu.99@gmail.com>
2021-09-03 18:55:25 +0300

    Add FolderExplorer (view) and FolderExplorerViewModel, make changes in FolderExplorer.xml, move FolderExplorer to view folder

---
+++ ClangPowerTools/ClangPowerToolsShared/MVVM/ViewModels/FolderExplorerViewModel.cs
@@ -0,0 +1,55 @@
+﻿using ClangPowerTools.MVVM.Command;
+using ClangPowerTools.MVVM.Views;
+using ClangPowerTools.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace ClangPowerToolsShared.MVVM.ViewModels
+{
+    public class FolderExplorerViewModel
+    {
+    #region Members
+    private string folderPath = string.Empty;
+    private FolderExplorer folderExplorerView;
+
+    private ICommand findFolderPathCommand;
+  //private ICommand folderTextPathCommand;
+    #endregion
+
+    #region Contructor 
+    public FolderExplorerViewModel(FolderExplorer view)
+    {
+      folderExplorerView = view;
+    }
+    #endregion
+
+    #region Commands
+    public ICommand FindFolderPathCommand
+    {
+      get => findFolderPathCommand ?? (findFolderPathCommand = new RelayCommand(() => GetFolderPath(), () => CanExecute));
+    }
+
+    //public ICommand InputToAdd
+    //{
+    //  get => folderTextPathCommand ?? (folderTextPathCommand = new RelayCommand(() => GetFolderPath(), () => CanExecute));
+    //}
+    #endregion
+
+    #region Public Methods
+    public void GetFolderPath()
+    {
+      //Implement get folder path
+    }
+
+    public bool CanExecute
+    {
+      get
+      {
+        return true;
+      }
+    }
+    #endregion
+  }
+}
---
+++ ClangPowerTools/ClangPowerToolsShared/MVVM/Views/FolderExplorer.xaml.cs
@@ -0,0 +1,27 @@
+﻿using ClangPowerTools.MVVM.Models;
+using ClangPowerToolsShared.MVVM.ViewModels;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace ClangPowerTools.MVVM.Views
+{
+  /// <summary>
+  /// Interaction logic for InputList.xaml
+  /// </summary>
+  public partial class FolderExplorer : Window
+  {
+    private readonly FolderExplorerViewModel viewModel;
+
+    #region Constructor
+    public FolderExplorer()
+    {
+      InitializeComponent();
+      viewModel= new FolderExplorerViewModel(this);
+      DataContext = viewModel;
+      Owner = SettingsProvider.SettingsView;
+    }
+    #endregion
+  }
+}
