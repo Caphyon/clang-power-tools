ad2d9bdcee60fe4279c59a356400237f814d2fef
horatiu.prica <horatiu.prica@caphyon.com>
2021-10-04 17:25:44 +0300

    Image and custom GUID for tool window

--- ClangPowerTools/ClangPowerToolsShared/MVVM/Views/ToolWindows/TidyToolWindow.cs
+++ ClangPowerTools/ClangPowerToolsShared/MVVM/Views/ToolWindows/TidyToolWindow.cs
@@ -1,15 +1,26 @@
 ﻿using ClangPowerTools.Views;
+using Microsoft.VisualStudio.Imaging;
 using Microsoft.VisualStudio.Shell;
+using System.Runtime.InteropServices;
 
 namespace ClangPowerToolsShared.MVVM.Views.ToolWindows
 {
+  [Guid(WindowGuidString)]
   public class TidyToolWindow : ToolWindowPane
   {
+    #region Members
+
+    public const string WindowGuidString = "7A1C87AD-8AFA-424E-9DCB-951BFFF050DC";
+    public const string Title = "Sample Tool Window";
+
+    #endregion
+
     #region Constructors
 
     public TidyToolWindow() : base()
     {
       Caption = "Tidy Tool window ";
+      BitmapImageMoniker = KnownMonikers.ImageIcon;
       Content = new TidyToolWindowView();
     }
 
