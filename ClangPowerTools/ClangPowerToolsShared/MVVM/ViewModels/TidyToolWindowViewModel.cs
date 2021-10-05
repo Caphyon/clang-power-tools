0a2afd2c0126f256a604547950e6f4044a79b67b
Marina Rusu <marina.rusu@caphyon.com>
2021-10-05 14:41:49 +0300

    Test display (test) list on tidy tool window

--- ClangPowerTools/ClangPowerToolsShared/MVVM/ViewModels/TidyToolWindowViewModel.cs
+++ ClangPowerTools/ClangPowerToolsShared/MVVM/ViewModels/TidyToolWindowViewModel.cs
@@ -1,10 +1,39 @@
-﻿using ClangPowerTools.Views;
+﻿using ClangPowerTools;
+using ClangPowerTools.MVVM.Models;
+using ClangPowerTools.Views;
 using Microsoft.VisualStudio.Shell;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 
 namespace ClangPowerToolsShared.MVVM.ViewModels
 {
-  public class TidyToolWindowViewModel
+  public class TidyToolWindowViewModel : CommonSettingsFunctionality, INotifyPropertyChanged
   {
+    public event PropertyChangedEventHandler PropertyChanged;
+    private List<FileModel> files = new List<FileModel> { new FileModel { FileName="testtttt" } };
+    private TidyToolWindowView tidyToolWindowView;
+    public TidyToolWindowViewModel(TidyToolWindowView tidyToolWindowView)
+    {
+      this.tidyToolWindowView = tidyToolWindowView;
+    }
 
+    #region Properties
+
+    public List<FileModel> Files
+    {
+      get
+      {
+        return files;
+      }
+      set
+      {
+        files = value;
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Files"));
+      }
+    }
+    //public ObservableCollection<SelectedFileModel> SelectedFiles { get; set; } = new ObservableCollection<SelectedFileModel>();
+
+    #endregion
   }
 }
--- ClangPowerTools/ClangPowerToolsShared/MVVM/Views/TidyToolWindowView.xaml.cs
+++ ClangPowerTools/ClangPowerToolsShared/MVVM/Views/TidyToolWindowView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using ClangPowerTools.MVVM.Interfaces;
 using ClangPowerToolsShared.MVVM.ViewModels;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -10,11 +11,26 @@ namespace ClangPowerTools.Views
   /// </summary>
   public partial class TidyToolWindowView : UserControl
   {
-    public TidyToolWindowView()
+    public  TidyToolWindowView()
     {
-      DataContext = new TidyToolWindowViewModel();
+      DataContext = new TidyToolWindowViewModel(this);
       InitializeComponent();
+      //List<User> items = new List<User>();
+      //items.Add(new User() { Name = "John Doe", Age = 42, Mail = "john@doe-family.com" });
+      //items.Add(new User() { Name = "Jane Doe", Age = 39, Mail = "jane@doe-family.com" });
+      //items.Add(new User() { Name = "Sammy Doe", Age = 7, Mail = "sammy.doe@gmail.com" });
+      //lvUsers.ItemsSource = items;
     }
+
+    public class User
+    {
+      public string Name { get; set; }
+
+      public int Age { get; set; }
+
+      public string Mail { get; set; }
+    }
+
     private void button1_Click(object sender, RoutedEventArgs e)
     {
       MessageBox.Show(
