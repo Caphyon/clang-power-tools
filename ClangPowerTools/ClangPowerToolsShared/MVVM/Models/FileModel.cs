95136abab3d8b7e1c8253a959c9fc06ec5bd909e
Marina Rusu <marina.rusu@caphyon.com>
2021-10-15 16:27:45 +0300

    Add idFixed prop for FileModel

--- ClangPowerTools/ClangPowerToolsShared/MVVM/Models/FileModel.cs
+++ ClangPowerTools/ClangPowerToolsShared/MVVM/Models/FileModel.cs
@@ -4,8 +4,22 @@ namespace ClangPowerTools.MVVM.Models
 {
   public class FileModel : INotifyPropertyChanged
   {
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    #region Public Members
+
     public string FileName { get; set; }
     public string FullFileName { get; set; }
+    public bool IsFixed
+    { 
+      get { return isFixed; } 
+      set 
+      { 
+        if(isFixed == value) return;
+        isFixed = value;
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsFixed"));
+      }
+    }
     public bool IsChecked
     {
       get { return isChecked; }
@@ -17,8 +31,14 @@ namespace ClangPowerTools.MVVM.Models
       }
     }
 
-    public event PropertyChangedEventHandler PropertyChanged;
+    #endregion
 
+    #region Private Members
+
+    private bool isFixed;
     private bool isChecked;
+
+    #endregion 
+
   }
 }
