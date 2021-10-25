4c9ba83b134564ea43104ec86ba6447cd0b9703b
Marina Rusu <marina.rusu@caphyon.com>
2021-10-25 12:46:36 +0300

    Add FileModel and UIElementsConstants, use constants (from UIElementsConstants) to set hidden and visible

---
+++ ClangPowerTools/ClangPowerToolsShared/MVVM/Constants/UIElementsConstants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClangPowerToolsShared.MVVM.Constants
+{
+    public static class UIElementsConstants
+    {
+      public static string Hidden { get; } = "Hidden";
+      public static string Visibile { get; } = "Visible";
+    }
+}
