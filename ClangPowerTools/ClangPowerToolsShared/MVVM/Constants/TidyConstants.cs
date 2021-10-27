2974ce3cde4c69c34a9c0455ad25e989f18cac75
Marina Rusu <marina.rusu@caphyon.com>
2021-10-27 12:20:26 +0300

    Add longPathPrefix constant, SolutionTempGuid, TempsFolderPath, replace with constants

--- ClangPowerTools/ClangPowerToolsShared/MVVM/Constants/TidyConstants.cs
+++ ClangPowerTools/ClangPowerToolsShared/MVVM/Constants/TidyConstants.cs
@@ -1,10 +1,17 @@
-﻿namespace ClangPowerTools
+﻿using System;
+using System.IO;
+
+namespace ClangPowerTools
 {
   public static class TidyConstants
   {
+
     public static string FlagsUri { get; } = @"https://clang.llvm.org/extra/clang-tidy/checks/";
     // This is the guid and id for the Tools.DiffFiles command
     public static string ToolsDiffFilesCmd { get; } = "{5D4C0442-C0A2-4BE8-9B4D-AB1C28450942}";
-    public static int ToolsDiffFilesId{ get; } = 256;
+    public static int ToolsDiffFilesId { get; } = 256;
+    public static string LongFilePrefix { get; } = @"\\?\";
+    public static string SolutionTempGuid { get; } = Guid.NewGuid().ToString();
+    public static string TempsFolderPath { get; } = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "ClangPowerTools", "Temp");
   }
 }
