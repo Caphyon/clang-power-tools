00a7851fb5bea45102bf9f1b415e023fbcc89300
Marina Rusu <marina.rusu@caphyon.com>
2021-10-28 11:04:32 +0300

    Display tooltip for fix, tidy, discard, remove

--- ClangPowerTools/ClangPowerToolsShared/MVVM/Constants/UIElementsConstants.cs
+++ ClangPowerTools/ClangPowerToolsShared/MVVM/Constants/UIElementsConstants.cs
@@ -6,6 +6,12 @@
     public static string Visibile { get; } = "Visible";
     public static string ObliqueStyleFont { get; } = "Oblique";
     public static string NormalStyleFont { get; } = "Normal";
+    public static string TidyTooltip { get; } = "Tidy ";
+    public static string RemoveTooltip { get; } = "Remove ";
+    public static string FixTooltip { get; } = "Fix ";
+    public static string DiscardTooltip { get; } = "Discard ";
+    public static string FilesTooltip { get; } = " files";
+    public static string FileTooltip { get; } = "file";
 
   }
 }
