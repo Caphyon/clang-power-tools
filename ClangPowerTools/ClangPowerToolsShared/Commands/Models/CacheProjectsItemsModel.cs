1ccfe3389799e97aef1cedba989ec5e236b7ae7f
Marina Rusu <marina.rusu@caphyon.com>
2021-11-06 18:43:09 +0200

    Collect projects and items in CacheProjectsItemsModel

--- ClangPowerTools/ClangPowerToolsShared/Commands/Models/CacheProjectsItemsModel.cs
+++ ClangPowerTools/ClangPowerToolsShared/Commands/Models/CacheProjectsItemsModel.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 
 namespace ClangPowerToolsShared.Commands.Models
 {
+  /// <summary>
+  /// Cache projects and Items, configuration, platform - used in script generating
+  /// </summary>
   public class CacheProjectsItemsModel
   {
-    public HashSet<Project> CacheProjects { get; set; } = new HashSet<Project>();
-    public HashSet<ProjectItem> CacheProjectItems { get; set; } = new HashSet<ProjectItem>();
+    public HashSet<Project> Projects { get; set; } = new HashSet<Project>();
+    public HashSet<ProjectItem> ProjectItems { get; set; } = new HashSet<ProjectItem>();
     public string Configuration { get; }
     public string Platform { get; }
   }
