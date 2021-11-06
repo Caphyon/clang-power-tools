dad2f5dd2a3e669485ba38e406b9402fedf92550
Marina Rusu <marina.rusu@caphyon.com>
2021-11-06 20:54:15 +0200

    Run script without the need to keep a collection of original items by path

--- ClangPowerTools/ClangPowerToolsShared/Commands/TidyCommand.cs
+++ ClangPowerTools/ClangPowerToolsShared/Commands/TidyCommand.cs
@@ -89,7 +89,7 @@ namespace ClangPowerTools.Commands
       cancellationToken: package.DisposalToken);
       var tidyToolWindow = (TidyToolWindow)window;
       FilePathCollector fileCollector = new FilePathCollector();
-      var paths = fileCollector.Collect(mItemsCollector.OriginalItems).ToList();
+      var paths = fileCollector.Collect(mItemsCollector.Items).ToList();
       if (tidyToolWindow != null)
         tidyToolWindow.OpenTidyToolWindow(paths);
     }
@@ -104,8 +104,8 @@ namespace ClangPowerTools.Commands
       cancellationToken: package.DisposalToken);
       var tidyToolWindow = (TidyToolWindow)window;
       FilePathCollector fileCollector = new FilePathCollector();
-      if (paths == null)
-        paths = fileCollector.Collect(mItemsCollector.OriginalItems).ToList();
+      //if (paths == null)
+      //  paths = fileCollector.Collect(mItemsCollector.OriginalItems).ToList();
       if (tidyToolWindow != null && paths != null)
         tidyToolWindow.UpdateToolWindow(paths);
     }
@@ -116,19 +116,6 @@ namespace ClangPowerTools.Commands
       {
         await PrepareCommmandAsync(commandUILocation);
         CacheProjectsFromItems();
-      }
-
-      if (paths != null)
-      {
-        mItemsCollector.Items = new List<IItem>();
-        foreach (var path in paths)
-        {
-          mItemsCollector.Items.Add(mItemsCollector.OriginalItems.Where(a => a.GetPath() == path).FirstOrDefault());
-        }
-      }
-      else
-      {
-        mItemsCollector.OriginalItems = new List<IItem>(mItemsCollector.Items);
       }
 
       if (CommandIds.kTidyToolWindowId == aCommandId || CommandIds.kTidyFixId == aCommandId)
