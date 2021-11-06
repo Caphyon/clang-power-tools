9e55943caa8cfcab1492ba70462bc02a6cb9376c
Marina Rusu <marina.rusu@caphyon.com>
2021-11-06 18:34:20 +0200

    Add CacheProjectsItems Model. Cache projects and Items, configuration, platform - will be used in script generating

---
+++ ClangPowerTools/ClangPowerToolsShared/Commands/Models/CacheProjectsItemsModel.cs
@@ -0,0 +1,13 @@
+﻿using EnvDTE;
+using System.Collections.Generic;
+
+namespace ClangPowerToolsShared.Commands.Models
+{
+  public class CacheProjectsItemsModel
+  {
+    public HashSet<Project> CacheProjects { get; set; } = new HashSet<Project>();
+    public HashSet<ProjectItem> CacheProjectItems { get; set; } = new HashSet<ProjectItem>();
+    public string Configuration { get; }
+    public string Platform { get; }
+  }
+}
