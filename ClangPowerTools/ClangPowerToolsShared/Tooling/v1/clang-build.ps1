cd2cc735186b0fdd6e70b4e59f9c0e61481641ac
Gabriel Diaconita <gabriel.diaconita@caphyon.com>
2021-11-07 19:31:03 +0200

    Remove debugging message accidentally committed

--- ClangPowerTools/ClangPowerToolsShared/Tooling/v1/clang-build.ps1
+++ ClangPowerTools/ClangPowerToolsShared/Tooling/v1/clang-build.ps1
@@ -1326,7 +1326,6 @@ Function Process-Project( [Parameter(Mandatory=$true)] [string]       $vcxprojPa
 
   if ($kCacheRepositorySaveIsNeeded)
   {
-    write-output "LK"
     Write-InformationTimed "Before serializing project"
     Save-ProjectToCacheRepo
     Write-InformationTimed "After serializing project"
