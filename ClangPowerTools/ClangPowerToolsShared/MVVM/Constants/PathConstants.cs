0b19986c5f56e8faaa505f40c4f7e619541d754b
Marina Rusu <marina.rusu@caphyon.com>
2021-11-09 10:32:09 +0200

    Add Path constants for cache repository and cpt

---
+++ ClangPowerTools/ClangPowerToolsShared/MVVM/Constants/PathConstants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.IO;
+
+namespace ClangPowerToolsShared.MVVM.Constants
+{
+  public static class PathConstants
+  {
+    public static string CPTPath { get; } = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "ClangPowerTools");
+    public static string CacheRepositoryPath { get; } = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "ClangPowerTools", "CacheRepository");
+  }
+}
