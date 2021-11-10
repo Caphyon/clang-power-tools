1acf2e01703c130294960c30f094b69e760d82a6
Marina Rusu <marina.rusu@caphyon.com>
2021-11-10 10:06:23 +0200

    Delete unused var from path constants

--- ClangPowerTools/ClangPowerToolsShared/MVVM/Constants/PathConstants.cs
+++ ClangPowerTools/ClangPowerToolsShared/MVVM/Constants/PathConstants.cs
@@ -5,7 +5,6 @@ namespace ClangPowerToolsShared.MVVM.Constants
 {
   public static class PathConstants
   {
-    public static string CPTPath { get; } = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "ClangPowerTools");
     public static string CacheRepositoryPath { get; } = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "ClangPowerTools", "CacheRepository");
   }
 }
