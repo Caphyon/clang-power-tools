976e02d5163f6c3745a148c1bfd1a7fca2ff76b9
Marina Rusu <marina.rusu@caphyon.com>
2021-11-12 10:05:59 +0200

    Delete unused code, add constants in vsThemeCommand

--- ClangPowerTools/ClangPowerToolsShared/MVVM/Commands/VSThemeCommand.cs
+++ ClangPowerTools/ClangPowerToolsShared/MVVM/Commands/VSThemeCommand.cs
@@ -9,14 +9,14 @@ namespace ClangPowerToolsShared.MVVM.Commands
   public static class VSThemeCommand
   {
     private const string darkTheme = "4293256677";
+    private const string textColorKey = "ToolWindowTextColorKey";
 
     public static VsThemes GetCurrentVsTheme()
     {
       try
       {
-
         var colorValues = VsColors.GetCurrentThemedColorValues();
-        var cvPair = colorValues.Where(a => a.Key.ToString() == "ToolWindowTextColorKey").FirstOrDefault();
+        var cvPair = colorValues.Where(a => a.Key.ToString() == textColorKey).FirstOrDefault();
         if (cvPair.Value.ToString() == darkTheme)
           return VsThemes.Dark;
       }
