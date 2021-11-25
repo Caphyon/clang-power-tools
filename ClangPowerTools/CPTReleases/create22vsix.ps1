a4903c3111b98500dc058c03d2628083e66bc443
Marina Rusu <marina.rusu@caphyon.com>
2021-11-25 09:50:38 +0200

    Replace caphyon id from manifest

--- ClangPowerTools/CPTReleases/create22vsix.ps1
+++ ClangPowerTools/CPTReleases/create22vsix.ps1
@@ -4,6 +4,7 @@ Function UpdateManifest()
 {
   $fileContent = Get-Content .\extension.vsixmanifest
   $fileContent = $fileContent -replace "<DisplayName>Clang Power Tools</DisplayName>", "<DisplayName>Clang Power Tools 2022</DisplayName>"
+  $fileContent = $fileContent -replace "<Identity Id=""Caphyon.705559db-5755-43fa-a023-41a3b14d2935""", "<Identity Id=""Caphyon.9ce239f2-d27a-432c-906c-1d55a123dbfd"""
   $fileContent > .\extension.vsixmanifest
 }
 
