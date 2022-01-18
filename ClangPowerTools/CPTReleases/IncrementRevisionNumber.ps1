415a2acfcf263c82c2d4e6cf657d3869654eb09e
mariru27 <marina.rusu@caphyon.com>
2022-01-18 14:55:50 +0200

    Refactor increment revision number script, catch invalid path exception

--- ClangPowerTools/CPTReleases/IncrementRevisionNumber.ps1
+++ ClangPowerTools/CPTReleases/IncrementRevisionNumber.ps1
@@ -1,20 +1,20 @@
 param([Parameter(Mandatory=$true, HelpMessage="Increments revision number in manifest file")][string] $loc)
 
-cd $loc
-cd ..\ClangPowerTools\
-
 #Get version from xml and increment revision value
-$fileName = "source.extension.vsixmanifest"
-[xml] $data = Get-Content $fileName
-$versionString = $data.PackageManifest.Metadata.Identity.Version
-$currentVersion = [Version]::new($versionString.ToString())
-$revision = $currentVersion.Revision -as [int]
-++$revision
-$nextVersion = [Version]::new($currentVersion.Major, $currentVersion.Minor, $currentVersion.Build, $revision)
+$filepath = "$loc\..\ClangPowerTools\source.extension.vsixmanifest"
+if(Test-Path $filepath)
+{
+    [xml] $data = Get-Content $filepath
+    $currentVersion = [Version]::new($data.PackageManifest.Metadata.Identity.Version.ToString())
+    $nextVersion = [Version]::new($currentVersion.Major, $currentVersion.Minor, $currentVersion.Build, $currentVersion.Revision + 1)
 
-#Replace old version with new one
-$data.PackageManifest.Metadata.Identity.Version = $nextVersion.ToString()
-$PSDefaultParameterValues['Out-File:Encoding'] = 'utf8' 
+    #Replace old version with new one
+    $data.PackageManifest.Metadata.Identity.Version = $nextVersion.ToString()
+    $data.Save("$filepath")
+}
+else 
+{
+    Write-Error "Invalid manifest file path"    
+}
 
 
-$data.Save("$pwd\$fileName")
