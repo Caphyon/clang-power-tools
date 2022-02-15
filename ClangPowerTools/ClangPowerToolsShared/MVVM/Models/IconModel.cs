638c917126270d3363bc6d7e18d0f2e0cc789b0d
mariru27 <marina.rusu@caphyon.com>
2022-02-15 14:57:52 +0200

    Create IconModel that will be used for icons management in tidy tool window

---
+++ ClangPowerTools/ClangPowerToolsShared/MVVM/Models/IconModel.cs
@@ -0,0 +1,20 @@
+﻿using ClangPowerToolsShared.MVVM.Constants;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClangPowerToolsShared.MVVM.Models
+{
+  public class IconModel
+  {
+    public string IconPath { get; set; }
+    public string Visibility { get; set; }
+    public bool IsEnabled { get; set; }
+
+    public IconModel(string iconPath, string visibility = "Hidden", bool isEnabled = false)
+    {
+      IconPath = iconPath;
+      Visibility = visibility;
+    }
+  }
+}
