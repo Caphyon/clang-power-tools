c4972daec09c625050e38b23400d86e4467188fc
mariru27 <marina.rusu@caphyon.com>
2022-02-16 12:03:55 +0200

    Add class CountStateFilesModel to store in all information about number of files on every state (ex. checked or unchecked)

---
+++ ClangPowerTools/ClangPowerToolsShared/MVVM/Models/TidyToolWindowModels/CountStateFilesModel.cs
@@ -0,0 +1,42 @@
+﻿using ClangPowerTools.MVVM.Models;
+using System.Collections.ObjectModel;
+
+namespace ClangPowerToolsShared.MVVM.Models.TidyToolWindowModels
+{
+  public class CountFilesModel
+  {
+    private int totalCheckedFixedFiles = 0;
+    private int totalCheckedFiles = 0;
+    private int totalCheckedHeaders = 0;
+    private int totalChecked = 0;
+
+    /// <summary>
+    /// Returns number of checked fixed files at this moment
+    /// </summary>
+    public int TotalCheckedFixedFiles { get { return totalCheckedFixedFiles;  } }
+
+    /// <summary>
+    /// Returns number of checked source files at this moment
+    /// </summary>
+    public int TotalCheckedFiles { get { return totalCheckedFiles; } }
+
+    /// <summary>
+    /// Returns number of checked headers at this moment
+    /// </summary>
+    public int TotalCheckedHeaders { get { return totalCheckedHeaders; } }
+
+    /// <summary>
+    /// Returns number of checked files at this moment
+    /// </summary>
+    public int TotalChecked { get { return totalChecked; } }
+
+    /// <summary>
+    /// Given a collection of files all CountFile properies will be updated. 
+    /// </summary>
+    /// <param name="Files"></param>
+    public void Update(ObservableCollection<FileModel> Files)
+    {
+
+    }
+  }
+}
