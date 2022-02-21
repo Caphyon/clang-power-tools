84514343e9aa18bb0f9b480ecdc0aa4497fa43a1
mariru27 <marina.rusu@caphyon.com>
2022-02-21 13:50:30 +0200

    Add tooltip for icons, from tidy tool window

--- ClangPowerTools/ClangPowerToolsShared/MVVM/Constants/UIElementsConstants.cs
+++ ClangPowerTools/ClangPowerToolsShared/MVVM/Constants/UIElementsConstants.cs
@@ -4,12 +4,5 @@
   {
     public static string Hidden { get; } = "Hidden";
     public static string Visibile { get; } = "Visible";
-    public static string TidyTooltip { get; } = "Tidy ";
-    public static string IgnoreTooltip { get; } = "Ignore ";
-    public static string FixTooltip { get; } = "Fix ";
-    public static string DiscardTooltip { get; } = "Discard ";
-    public static string FilesTooltip { get; } = " files";
-    public static string FileTooltip { get; } = "file";
-    public static string RefreshTooltip { get; } = " Refresh ";
   }
 }
