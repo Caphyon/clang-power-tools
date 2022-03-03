583b45e6c7d8ed25ed70644b4b4e0f1e716d1209
mariru27 <marina.rusu@caphyon.com>
2022-03-03 12:01:43 +0200

    Increment version in aip

--- ClangPowerTools/CPTReleases/IncrementRevisionNumber.ps1
+++ ClangPowerTools/CPTReleases/IncrementRevisionNumber.ps1
@@ -2,15 +2,24 @@ param([Parameter(Mandatory=$true, HelpMessage="Increments revision number in man
 
 #Get version from xml and increment revision value
 $filepath = "$loc\..\ClangPowerTools\source.extension.vsixmanifest"
-if(Test-Path $filepath)
+$filepathToAip = "$loc\..\ClangPowerTools.aip"
+if((Test-Path $filepath) -and (Test-Path $filepathToAip))
 {
+    #Get xml data from manifest file
     [xml] $data = Get-Content $filepath
     $currentVersion = [Version]::new($data.PackageManifest.Metadata.Identity.Version.ToString())
     $nextVersion = [Version]::new($currentVersion.Major, $currentVersion.Minor, $currentVersion.Build, $currentVersion.Revision + 1)
-
-    #Replace old version with new one
+    
+    #Get xml data from aip file
+    [xml] $aipData = Get-Content $filepathToAip
+		
+    #Replace old version with new one in manifest
     $data.PackageManifest.Metadata.Identity.Version = $nextVersion.ToString()
     $data.Save("$filepath")
+
+    #Replace old version with new one in aip file
+    $aipData.DOCUMENT.COMPONENT[0].ROW[5].Value = $nextVersion.ToString()
+    $aipData.Save("$filepathToAip")
 }
 else 
 {
