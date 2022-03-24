217c73e551be9c9dafdff88c69dc4a6b88952bfd
mariru27 <marina.rusu@caphyon.com>
2022-03-24 09:56:23 +0200

    Add llvm 14

--- ClangPowerTools/ClangPowerToolsShared/MVVM/Constants/LLVMVersionsAlternate.cs
+++ ClangPowerTools/ClangPowerToolsShared/MVVM/Constants/LLVMVersionsAlternate.cs
@@ -6,6 +6,7 @@ namespace ClangPowerTools
   {
     public static List<string> VersionAlternateUri { get; } = new List<string>
     {
+      "14.0.0",
       "13.0.1",
       "13.0.0",
       "12.0.1",
