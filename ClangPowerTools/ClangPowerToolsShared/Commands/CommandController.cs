3413b63e803b25b32ac5efd7f553f10dfc9ce90a
mariru27 <marina.rusu@caphyon.com>
2022-05-10 10:40:39 +0300

    Change tooltip, change to Tidy-Fix, delete unused code

--- ClangPowerTools/ClangPowerToolsShared/Commands/CommandController.cs
+++ ClangPowerTools/ClangPowerToolsShared/Commands/CommandController.cs
@@ -616,17 +616,13 @@ namespace ClangPowerTools
       //Change button name for tidy depending on settings property
       if (tidySettings.ApplyTidyFix && (command.CommandID.ID == CommandIds.kTidyId || command.CommandID.ID == CommandIds.kTidyToolbarId))
       {
-        command.Text = "Apply Tidy-Fix";
+        command.Text = "Tidy-Fix";
       }
       else if(command.CommandID.ID == CommandIds.kTidyId || command.CommandID.ID == CommandIds.kTidyToolbarId)
       {
         command.Text = "Tidy";
       }
 
-      if(command.CommandID.ID == CommandIds.kTidyId || command.CommandID.ID == CommandIds.kTidyToolbarId)
-      {
-        
-      }
       if (IsAToolbarCommand(command))
       {
         if (SolutionInfo.AreToolbarCommandsEnabled() == false)
