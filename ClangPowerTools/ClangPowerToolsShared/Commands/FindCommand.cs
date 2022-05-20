705139c9b773871a0fd42e756bb2f52cf9d733c9
mariru27 <marina.rusu@caphyon.com>
2022-05-20 13:56:25 +0300

    Add FindCommand

---
+++ ClangPowerTools/ClangPowerToolsShared/Commands/FindCommand.cs
@@ -0,0 +1,64 @@
+﻿using ClangPowerTools;
+using Microsoft.VisualStudio.Shell;
+using System;
+using System.ComponentModel.Design;
+using Task = System.Threading.Tasks.Task;
+
+namespace ClangPowerToolsShared.Commands
+{
+  public class FindCommand : ClangCommand
+  {
+    #region Properties
+
+    public static FindCommand Instance
+    {
+      get;
+      private set;
+    }
+
+    #endregion
+
+    #region Constructor
+
+    protected FindCommand(OleMenuCommandService aCommandService, CommandController aCommandController,
+      AsyncPackage aPackage, Guid aGuid, int aId)
+        : base(aPackage, aGuid, aId)
+    {
+      if (null != aCommandService)
+      {
+        var menuCommandID = new CommandID(CommandSet, Id);
+        var menuCommand = new OleMenuCommand(aCommandController.Execute, menuCommandID);
+        menuCommand.BeforeQueryStatus += aCommandController.OnBeforeClangCommand;
+        menuCommand.Enabled = true;
+        aCommandService.AddCommand(menuCommand);
+      }
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    /// <summary>
+    /// Initializes the singleton instance of the command.
+    /// </summary>
+    /// <param name="package">Owner package, not null.</param>
+    public static async Task InitializeAsync(CommandController aCommandController,
+      AsyncPackage aPackage, Guid aGuid, int aId)
+    {
+      // Switch to the main thread - the call to AddCommand in CompileCommand's constructor requires
+      // the UI thread.
+      await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(aPackage.DisposalToken);
+
+      OleMenuCommandService commandService = await aPackage.GetServiceAsync((typeof(IMenuCommandService))) as OleMenuCommandService;
+      Instance = new FindCommand(commandService, aCommandController, aPackage, aGuid, aId);
+    }
+
+    void Find()
+    {
+
+    }
+
+    #endregion
+
+  }
+}
