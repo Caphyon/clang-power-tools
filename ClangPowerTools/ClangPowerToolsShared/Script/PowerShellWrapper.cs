2973034807a48f769be49a9ced2c85692964c07c
mariru27 <marina.rusu@caphyon.com>
2022-05-26 16:26:35 +0300

    Run Sequential Commands passed to powershell

--- ClangPowerTools/ClangPowerToolsShared/Script/PowerShellWrapper.cs
+++ ClangPowerTools/ClangPowerToolsShared/Script/PowerShellWrapper.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 
@@ -65,6 +67,86 @@ namespace ClangPowerTools
         runningProcesses.Add(process);
 
         process.Start();
+
+        process.BeginErrorReadLine();
+        process.BeginOutputReadLine();
+
+        process.WaitForExit();
+      }
+      catch (Exception e)
+      {
+        process.EnableRaisingEvents = false;
+        process.ErrorDataReceived -= DataErrorHandler;
+        process.OutputDataReceived -= DataHandler;
+        process.Exited -= ExitedHandler;
+        process.Disposed -= ExitedHandler;
+
+        process.Close();
+
+        throw e;
+      }
+    }
+
+    public static void PassSequentialCommandsInvoke(string aScript, RunningProcesses runningProcesses,
+                                                    List<string> commands)
+    {
+      Process process = new Process();
+      try
+      {
+        process.StartInfo = new ProcessStartInfo()
+        {
+          FileName = $"{Environment.SystemDirectory}\\{ScriptConstants.kPowerShellPath}",
+          RedirectStandardError = true,
+          RedirectStandardOutput = true,
+          CreateNoWindow = true,
+          UseShellExecute = false,
+
+          /*
+          When we are dealing with file paths that contain single quotes, we are running into 
+          trouble because whey are messing up our script invocation text. The situation is further 
+          complicated by the fact that this invocation is imbricated (invoke inside invoke).
+          Explanation: we are invoking powershell.exe and telling it using -command what to invoke itself, 
+          which would be our very own clang-buils.ps1 script. 
+
+          All this script invocation command is enveloped in single quotes. One, quick to mind solution 
+          would be to use double quotes. However, this is not practical because it would lead to further 
+          issues down the road since those strings are interpolated (and the $ sign is valid in a Windows file path).
+
+          We have to keep using single quotes, but make sure that we double escape them when we find them.
+          IMPORTANT: there are single quotes which we should not escape. 
+          In order to precisely match the quotes that we need, we are exploiting the following detail:
+          file paths containing single quotes will never have spaces to the left or right of them, but the ones we 
+          are not interested in will have space either to the left or the right.
+           */
+          Arguments = Regex.Replace(aScript, @"([\w|\\])'([\w|\\])", "$1''''$2")
+        };
+        process.StartInfo.EnvironmentVariables["Path"] = CreatePathEnvironmentVariable();
+
+        var customTidyExecutable = GetCustomTidyPath();
+
+        if (string.IsNullOrWhiteSpace(customTidyExecutable) == false)
+          process.StartInfo.EnvironmentVariables[ScriptConstants.kEnvrionmentTidyPath] = customTidyExecutable;
+
+        process.EnableRaisingEvents = true;
+        process.ErrorDataReceived += DataErrorHandler;
+        process.OutputDataReceived += DataHandler;
+        process.Exited += ExitedHandler;
+        process.Disposed += ExitedHandler;
+
+        runningProcesses.Add(process);
+
+        process.Start();
+
+        using (StreamWriter sw = process.StandardInput)
+        {
+          if (sw.BaseStream.CanWrite)
+          {
+            foreach (var command in commands)
+            {
+              sw.WriteLine(command);
+            }
+          }
+        }
 
         process.BeginErrorReadLine();
         process.BeginOutputReadLine();
