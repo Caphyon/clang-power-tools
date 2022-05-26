cf17480e99cd6f52f875709e99b0ecf641af7696
mariru27 <marina.rusu@caphyon.com>
2022-05-26 10:01:46 +0300

    Rename TidyToolWindowModel to ToolWindowModel Add FindToolWindowModel

---
+++ ClangPowerTools/ClangPowerToolsShared/MVVM/Models/ToolWindowModels/DefaultArgsModel.cs
@@ -0,0 +1,17 @@
+﻿namespace ClangPowerToolsShared.MVVM.Models.ToolWindowModels
+{
+  public class DefaultArgsModel
+  {
+    private string functionName = string.Empty;
+    private int defaultArgsPosition = 0;
+
+    public string FunctionName
+    {
+      get { return functionName; }
+      set
+      {
+        functionName = value;
+      }
+    }
+  }
+}
---
+++ ClangPowerTools/ClangPowerToolsShared/MVVM/Models/ToolWindowModels/FindToolWindowModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+namespace ClangPowerToolsShared.MVVM.Models.ToolWindowModels
+{
+  public class FindToolWindowModel : INotifyPropertyChanged
+  {
+    public event PropertyChangedEventHandler PropertyChanged;
+    private bool isRunning = false;
+    public DefaultArgsModel DefaultArgs { get; set; }
+
+    public bool IsRunning
+    {
+      get
+      {
+        return isRunning;
+      }
+      set
+      {
+        isRunning = value;
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsRunning"));
+      }
+    }
+  }
+}
