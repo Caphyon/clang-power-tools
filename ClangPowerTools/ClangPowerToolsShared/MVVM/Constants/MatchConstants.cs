8020558274684944d740278efe67cda69e12e978
mariru27 <marina.rusu@caphyon.com>
2022-05-27 12:00:10 +0300

    Add constants AST matchers (first matcher CalledExprDefaultArg).

---
+++ ClangPowerTools/ClangPowerToolsShared/MVVM/Constants/MatchConstants.cs
@@ -0,0 +1,22 @@
+﻿namespace ClangPowerToolsShared.MVVM.Constants
+{
+  /// <summary>
+  /// AST Matchers
+  /// </summary>
+  public static class MatchConstants
+  {
+    /// <summary>
+    /// Match a called expersion with a default argument on a position.
+    /// Replace {0} with expersion name.
+    /// Replace {1} with position of default arg
+    /// </summary>
+    public static string CalledExprDefaultArg
+    {
+      get 
+      {
+        return "match callExpr(callee(functionDecl(hasName(\"{0}\"))), hasArgument({1}, cxxDefaultArgExpr()))";
+      }
+    }
+  }
+
+}
--- ClangPowerTools/ClangPowerToolsShared/MVVM/Controllers/FindController.cs
+++ ClangPowerTools/ClangPowerToolsShared/MVVM/Controllers/FindController.cs
@@ -1,6 +1,23 @@
-﻿namespace ClangPowerToolsShared.MVVM.Controllers
+﻿using ClangPowerToolsShared.Commands;
+
+namespace ClangPowerToolsShared.MVVM.Controllers
 {
   public class FindController
   {
+    private int currentCommand = 0;
+    
+    public void LaunchCommand(int commandId)
+    {
+      currentCommand = commandId;
+      switch (currentCommand)
+      {
+        case FindCommandIds.kDefaultArgs:
+        {
+          break;
+        }
+        default:
+          break;
+      }
+    }
   }
 }
