8217293d14c149defc5892ff8aede9037aee3fd3
mariru27 <marina.rusu@caphyon.com>
2022-06-02 16:29:01 +0300

    Add commands name and Ids

--- ClangPowerTools/ClangPowerToolsShared/Commands/FindCommandIds.cs
+++ ClangPowerTools/ClangPowerToolsShared/Commands/FindCommandIds.cs
@@ -1,7 +1,21 @@
-﻿namespace ClangPowerToolsShared.Commands
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ClangPowerToolsShared.Commands
 {
-  public class FindCommandIds
+  public static class FindCommandIds
   {
+    public static List<KeyValuePair<int, string>> matchers = new List<KeyValuePair<int, string>>()
+    {
+        new KeyValuePair<int, string>(1,"Default arguments"),
+    };
+
+    public static List<KeyValuePair<int, string>> Matchers { get { return matchers; } }
+
+    public static List<string> CommandsName { get { return matchers.Select(a => a.Value).ToList(); }  }
+    public static List<int> CommandIds { get { return matchers.Select(a => a.Key).ToList(); }  }
+
+
     public const int kDefaultArgs = 1;
   }
 }
