98def2b146be8bbbbb9c633f2615891427c55e1e
mariru27 <marina.rusu@caphyon.com>
2022-07-11 10:53:15 +0300

    Add CustomMatchesModel

---
+++ ClangPowerTools/ClangPowerToolsShared/MVVM/Models/ToolWindowModels/CustomMatchesModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace ClangPowerToolsShared.MVVM.Models.ToolWindowModels
+{
+  public class CustomMatchesModel : INotifyPropertyChanged
+  {
+    public event PropertyChangedEventHandler PropertyChanged;
+    private string matches = string.Empty;
+
+    public string Matches
+    {
+      get { return matches; }
+      set
+      {
+        matches = value;
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Matches"));
+      }
+    }
+
+    public string Details { get; } = "Details\n";
+  }
+}
