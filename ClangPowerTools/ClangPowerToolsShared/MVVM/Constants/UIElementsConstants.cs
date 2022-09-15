00e1e41e405c81cebcf653cc99f8890c1c251188
mariru27 <marina.rusu@caphyon.com>
2022-09-15 13:13:09 +0300

    Add tooltip on pin button, delete background color from find button

--- ClangPowerTools/ClangPowerToolsShared/MVVM/Constants/UIElementsConstants.cs
+++ ClangPowerTools/ClangPowerToolsShared/MVVM/Constants/UIElementsConstants.cs
@@ -4,5 +4,7 @@
   {
     public static string Hidden { get; } = "Hidden";
     public static string Visibile { get; } = "Visible";
+    public static string Pin { get; } = "Pin";
+    public static string Unpin { get; } = "Unpin";
   }
 }
