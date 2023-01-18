96db4784ae8f73b74156856c630986e4d72e76da
mariru27 <marina.rusu@caphyon.com>
2023-01-18 15:10:57 +0200

    Install Clang Format Editor if this is not already installed Replace ClangFormatEditor.exe with Clang Format Editor.msi

--- ClangPowerTools/ClangPowerToolsShared/MVVM/Controllers/FormatEditorController.cs
+++ ClangPowerTools/ClangPowerToolsShared/MVVM/Controllers/FormatEditorController.cs
@@ -1,5 +1,6 @@
 ﻿using ClangPowerTools.Helpers;
 using System;
+using System.ComponentModel.Design;
 using System.Diagnostics;
 using System.IO;
 using System.Windows;
@@ -33,7 +34,32 @@ namespace ClangPowerTools
       try
       {
         EditorProcess = new Process();
-        EditorProcess.StartInfo.FileName = Path.Combine(vsixPath, FormatEditorConstants.ExecutableName);
+        EditorProcess.StartInfo.FileName = Path.Combine(FormatEditorConstants.ClangFormatEditorPath,
+        FormatEditorConstants.ExecutableName);
+        EditorProcess.Start();
+      }
+      catch (Exception e)
+      {
+        MessageBox.Show(e.Message, FormatEditorConstants.ClangFormatEditor, MessageBoxButton.OK, MessageBoxImage.Error);
+      }
+    }
+
+    //Install Clang Format Editor if this is not already installed
+    public void InstallClangFormatEditorSilent()
+    {
+      if (File.Exists(Path.Combine(FormatEditorConstants.ClangFormatEditorPath,
+        FormatEditorConstants.ExecutableName)))
+        return;
+      try
+      {
+        var msiName = Path.Combine(vsixPath, FormatEditorConstants.ClangFormatMsi);
+        string Script = $"-ExecutionPolicy Unrestricted -NoProfile -Noninteractive -command \"& " +
+          $"msiexec.exe /i '{Path.Combine(vsixPath, FormatEditorConstants.ClangFormatMsi)}' /qn \" ";
+        EditorProcess = new Process();
+        EditorProcess.StartInfo.FileName = $"{Environment.SystemDirectory}\\{ScriptConstants.kPowerShellPath}";
+        EditorProcess.StartInfo.Arguments = Script;
+        EditorProcess.StartInfo.UseShellExecute = true;
+        EditorProcess.StartInfo.Verb = "runas";
         EditorProcess.Start();
       }
       catch (Exception e)
