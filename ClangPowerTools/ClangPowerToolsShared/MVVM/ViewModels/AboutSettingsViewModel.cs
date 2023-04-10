19e937c70fe15f32ba8232e2614060db074705ce
mariru27 <marina.rusu@caphyon.com>
2023-04-10 16:34:09 +0300

    Display product version and cpt logo in about tab

--- ClangPowerTools/ClangPowerToolsShared/MVVM/ViewModels/AboutSettingsViewModel.cs
+++ ClangPowerTools/ClangPowerToolsShared/MVVM/ViewModels/AboutSettingsViewModel.cs
@@ -1,9 +1,58 @@
 ﻿using ClangPowerTools;
+using ClangPowerTools.MVVM.Models;
+using System.ComponentModel;
 using System.Windows.Input;
 
 namespace ClangPowerToolsShared.MVVM.ViewModels
 {
   public class AboutSettingsViewModel
   {
+    #region Members
+
+    private GeneralSettingsModel generalModel;
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    #endregion
+
+
+    #region Constructor
+
+    public AboutSettingsViewModel()
+    {
+      generalModel = SettingsProvider.GeneralSettingsModel;
+    }
+
+    #endregion
+
+
+    #region Properties
+
+    public GeneralSettingsModel GeneralSettingsModel
+    {
+      get
+      {
+        return generalModel;
+      }
+      set
+      {
+        generalModel = value;
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("GeneralSettingsModel"));
+      }
+    }
+
+    public string DisplayMessage
+    {
+      get { return displayMessage; }
+      set
+      {
+        displayMessage = value;
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("DisplayMessage"));
+      }
+    }
+
+    private string displayMessage;
+
+    #endregion
   }
 }
