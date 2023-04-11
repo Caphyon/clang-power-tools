23a5c4fa94a68d3d639401b7da8ca47f39ef9775
mariru27 <marina.rusu@caphyon.com>
2023-04-11 11:56:24 +0300

    Add functionalities to export, import and reset settings

--- ClangPowerTools/ClangPowerToolsShared/MVVM/ViewModels/AboutSettingsViewModel.cs
+++ ClangPowerTools/ClangPowerToolsShared/MVVM/ViewModels/AboutSettingsViewModel.cs
@@ -1,13 +1,20 @@
 ﻿using ClangPowerTools;
+using ClangPowerTools.MVVM.Command;
 using ClangPowerTools.MVVM.Models;
 using System.ComponentModel;
+using System.Windows.Forms;
 using System.Windows.Input;
 
 namespace ClangPowerToolsShared.MVVM.ViewModels
 {
-  public class AboutSettingsViewModel
+  public class AboutSettingsViewModel : CommonSettingsFunctionality
   {
     #region Members
+    private readonly SettingsHandler settingsHandler = new SettingsHandler();
+
+    private ICommand exportSettingsCommand;
+    private ICommand importSettingsCommand;
+    private ICommand resetSettingsCommand;
 
     private GeneralSettingsModel generalModel;
 
@@ -50,9 +57,60 @@ namespace ClangPowerToolsShared.MVVM.ViewModels
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("DisplayMessage"));
       }
     }
+    public bool CanExecute
+    {
+      get
+      {
+        return true;
+      }
+    }
 
     private string displayMessage;
 
     #endregion
+
+    public ICommand ExportSettingsCommand
+    {
+      get => exportSettingsCommand ??= new RelayCommand(() => ExportSettings(), () => CanExecute);
+    }
+
+    public ICommand ImportSettingssCommand
+    {
+      get => importSettingsCommand ??= new RelayCommand(() => ImportSettings(), () => CanExecute);
+    }
+
+    public ICommand ResetSettingsCommand
+    {
+      get => resetSettingsCommand ??= new RelayCommand(() => ResetSettings(), () => CanExecute);
+    }
+
+    private void ExportSettings()
+    {
+      string path = SaveFile("settings", ".json", "Settings files (.json)|*.json");
+      if (string.IsNullOrEmpty(path) == false)
+      {
+        settingsHandler.SaveSettings(path);
+        MessageBox.Show("Settings exported.", "Clang Power Tools",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+      }
+    }
+
+    private void ImportSettings()
+    {
+      string path = OpenFile("settings", ".json", "Settings files (.json)|*.json");
+      if (string.IsNullOrEmpty(path) == false)
+      {
+        settingsHandler.LoadSettings(path);
+        MessageBox.Show("Settings imported.", "Clang Power Tools",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+      }
+    }
+
+    private void ResetSettings()
+    {
+      settingsHandler.ResetSettings();
+      MessageBox.Show("Settings were reset to their default values.", "Clang Power Tools",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+    }
   }
 }
