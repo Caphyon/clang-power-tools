6cccf2a28b64afb8e1c9b85958686d14849ee835
Marina Rusu <marina.rusu@caphyon.com>
2025-01-20 11:37:22 +0200

    Fix typo in githubLLVM url

--- ClangPowerTools/ClangPowerToolsShared/Tooling/v1/psClang/get-llvm-helper.ps1
+++ ClangPowerTools/ClangPowerToolsShared/Tooling/v1/psClang/get-llvm-helper.ps1
@@ -10,7 +10,7 @@ Set-Variable -name kLLVMInstallLocations    -value @("${Env:ProgramW6432}\LLVM\b
                                                     ,"${Env:ProgramFiles(x86)}\LLVM\bin"
                                                     )                   -option Constant
 #Url to assets (clang++ and clang-tidy) from previous release made by Clang Power Tools on github 
-Set-Variable -name kCptGithubLlvm -value "https://github.com/Caphyon/clang-power-tools/releases/tag/v2024.9.0" `
+Set-Variable -name kCptGithubLlvm -value "https://github.com/Caphyon/clang-power-tools/releases/download/v2024.9.0" `
                                   -option Constant
 Set-Variable -name kCptGithubLlvmVersion -value "19.1.7 (LLVM 19.1.7)" -Option Constant
 
